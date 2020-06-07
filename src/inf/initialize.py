ea7cc77d9db260b7d28a4f94e39baf093dcf28c8
Mohammad Nadji-Tehrani <m.nadji.tehrani@gmail.com>
2020-06-06 23:51:10 -0500

    main function structure

--- src/inf/initialize.py
+++ src/inf/initialize.py
@@ -5,19 +5,30 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def init_dataSources():
-    # logger.error('something something')
-    log.info("Logging is configured.")
-    return
-
-
 def init_parameters():
+    """To load all the key configuration parameters"""
+    from configparser import ConfigParser
+    feagi_config = ConfigParser()
+    feagi_config.read('./feagi_configuration.ini')
+    log.info("All parameters have been initialized.")
+    return feagi_config
+
+
+def init_data_sources():
+    """To validate and initialize all data sources and databases"""
+    log.info("All data sources have been initialized.")
     return
 
 
-def init_IPUs():
+def init_ipu():
+    """To validate and initialize all the Input Processing Units"""
+    log.info("All IPUs have been initialized.")
     return
 
 
-def init_OPUs():
+def init_opu():
+    """To validate and initialize all the Output Processing Units"""
+    log.info("All OPUs have been initialized.")
     return
+
+
--- src/main.py
+++ src/main.py
@@ -4,43 +4,50 @@ Copyright (c) 2019 Mohammad Nadji-Tehrani <m.nadji.tehrani@gmail.com>
 
 """
 
-from configparser import ConfigParser
-import logging
 import logging.config
 import json
+from art import text2art
 
-parser = ConfigParser()
-parser.read('../configuration.ini')
+logging_config_file = './logging_config.json'
 
-with open('./inf/logging_config.json', 'r') as data_file:
+with open(logging_config_file, 'r') as data_file:
     LOGGING_CONFIG = json.load(data_file)
+    logging.config.dictConfig(LOGGING_CONFIG)
 
-logging.config.dictConfig(LOGGING_CONFIG)
 
-if __name__ == '__main__':
-    from configparser import ConfigParser
-    from logging import Logger
-    from art import text2art
-    from shutil import copyfile
-    import sys
-    import os
-    from inf import initialize
-
+def splash_screen():
     # FEAGI Word Art
     print(text2art("FEAGI", font='block'))
 
 
-    initialize.init_parameters()
-    initialize.init_dataSources()
-    initialize.init_IPUs()
-    initialize.init_OPUs()
+if __name__ == '__main__':
+    from inf import initialize
+    from evo import neuroembryogenesis, death
+    from npu import consciousness
+    from life import adventures
+    from shutil import copyfile
+    import sys
+    import os
 
+    splash_screen()
 
+    # Initialize the environment
+    feagi_config = initialize.init_parameters()
+    # print(feagi_config.get('Logging', 'logging_config_file'))
+    initialize.init_data_sources()
+    initialize.init_ipu()
+    initialize.init_opu()
 
+    exit_condition = False
 
-
-
-
-
-
-
+    # This while loop simulates a single cycle of life for the artificial brain
+    while not exit_condition:
+        # Process of artificial neuroembryogenesis that leads to connectome development
+        neuroembryogenesis.develop_brain()
+        # All brain activities occur in between consciousness start and stop
+        consciousness.start()
+        # A set of experiences will be outlined under life adventures that leads to learning
+        adventures.tbd()
+        consciousness.stop()
+        # Death process eliminates the brain instance and captures associated performance details
+        death.register()
