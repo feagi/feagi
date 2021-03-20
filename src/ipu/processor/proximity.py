2b652a3abc72ce1da735a86126805c0f02ec3f9f
Eric Miller <ermiller@alumni.cmu.edu>
2021-03-19 20:37:40 -0400

    check in proximity IPU progress

--- src/ipu/processor/proximity.py
+++ src/ipu/processor/proximity.py
@@ -3,18 +3,14 @@ from evo.neuron import neuron_finder
 from inf import runtime_data
 
 
-def detections_to_coords(proximity_data, proximity_type):
+def detections_to_coords(proximity_data, proximity_type='LIDAR'):
     """ Converts coordinates from LIDAR detections to modified 
     Cartesian plane.
 
     :param proximity_data:
-    :param proximity_type:
     :return:
     """
     detection_locations = []
-    # can become expensive (O(n^2)) if high-res LIDAR (?)
-    # convert to list comprehension for modest gains? <-- could be ugly
-    # cythonize this?
     for sweep in proximity_data:
         for point in proximity_data[sweep][proximity_type]:
             h_angle = point[0]
@@ -39,14 +35,27 @@ def locations_to_neuron_ids(detection_locations, cortical_area):
     :return:
     """
     neuron_ids = []
-    for i in range(len(point_locations)):
-        found_neurons = neuron_finder(
-            cortical_area, 
-            point_locations[i], 
-            runtime_data.genome["location_tolerance"]
-        )    
-        if found_neurons:
-            for neuron in found_neurons:
-                neuron_ids.append(neuron)
-    
+    for i in range(len(detection_locations)):
+        # found_neurons = neuron_finder(
+        #     cortical_area, 
+        #     detection_locations[i], 
+        #     runtime_data.genome["location_tolerance"]
+        # )    
+        # if found_neurons:
+        #     for neuron in found_neurons:
+        #         neuron_ids.append(neuron)
+        block_ref = block_ref_builder(detection_locations[i])
+        if block_ref in runtime_data.block_dic[cortical_area]:
+            block_neurons = runtime_data.block_dic[cortical_area][block_ref]
+            for neuron in block_neurons:
+                if neuron is not None and neuron not in neuron_ids:
+                    neuron_ids.append(neuron)
+
     return neuron_ids
+
+
+def block_ref_builder(locations):
+    block_ref = ''
+    for loc in locations:
+        block_ref += str(round(loc))
+    return '-'.join(block_ref)
