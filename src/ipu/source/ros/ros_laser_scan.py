eb5b98495d9980ee2f159b81d20fc6b9f08563b0
m-nadji-tehrani <m.nadji.tehrani@gmail.com>
2021-03-21 22:44:06 -0500

    LIDAR source interface

--- src/ipu/source/ros/ros_laser_scan.py
+++ src/ipu/source/ros/ros_laser_scan.py
@@ -1,3 +1,29 @@
+
+"""
+This module needs to be compiled using colcon for ROS2 and is not directly run by FEAGI
+
+Todo: Need to implement an automated method to deploy and compile this method.
+1. mkdir -p ~/ros2_ws/src     # Create a ros2 workspace
+2. cd ~/ros2_ws/src
+3. ros2 pkg create --build-type ament_python py_topic    # Create a ros2 package
+4. cp /src/ipu/source/ros/ros_laser_scan.py ~/ros2_ws/src/py_topic/py_topic/
+5. edit ~/ros2_ws/src/py_topic/package.xml and add the following 3 lines after the license declaration line
+  <buildtool_depend>ament_python</buildtool_depend>
+  <exec_depend>rclpy</exec_depend>
+  <exec_depend>geometry_msgs</exec_depend>
+
+6. Edit ~/ros2_ws/src/py_topic/setup.py entry point as follows:
+    'console_scripts': ['py_laser_scan = py_topic.ros_laser_scan:main']
+
+7. cd ~/ros2_ws
+8. colcon build
+9. ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py  #in one terminal run Turtlebot3
+10.   # In another terminal, source your ros
+11. ros2 run py_topic ros_laser_scan
+
+
+Note: This module is a modified version of the code from OSRF
+
 # Copyright 2016 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,11 +37,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""
-This module needs to be compiled using colcon for ROS2.
-
-Todo: Need to implement an automated method to deploy and compile this method.
 
 """
 
@@ -32,7 +53,7 @@ from rclpy.qos import qos_profile_sensor_data #this is required to have a full d
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 
-# todo: move the binding port to the feagi_configuration.ini
+# todo: Figure a way to externalize the binding port. feagi_configuration.ini captures it on FEAGI side.
 socket.bind('tcp://127.0.0.1:2000')
 
 
