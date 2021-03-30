9f205c707397c47bc44eb8967a37c3a9393abf7a
eric <ermiller@alumni.cmu.edu>
2021-03-30 15:02:12 -0400

    wrap source lidar code in function to make callable

--- src/ipu/source/lidar.py
+++ src/ipu/source/lidar.py
@@ -7,36 +7,40 @@ import zmq
 from ipu.processor import proximity
 from inf import runtime_data
 
-socket_address = runtime_data.parameters["Sockets"]["lidar_socket"]
 
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-socket.connect(socket_address)
-socket.set(zmq.SUBSCRIBE, ''.encode('utf-8'))
+def get_and_translate():
+    socket_address = runtime_data.parameters["Sockets"]["lidar_socket"]
 
-listener = 0
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect(socket_address)
+    socket.set(zmq.SUBSCRIBE, ''.encode('utf-8'))
 
-message = socket.recv_pyobj()
-method_list = [method for method in dir(message) if method.startswith('_') is False]
+    listener = 0
 
-while True:
     message = socket.recv_pyobj()
+    method_list = [method for method in dir(message) if method.startswith('_') is False]
 
-    if message is not None:
-        # print("SLOT_TYPES", message.SLOT_TYPES)
-        print("angle_increment:", message.angle_increment)
-        print("angle_max:", message.angle_max)
-        print("angle_min:", message.angle_min)
-        print("get_fields_and_field_types:", message.get_fields_and_field_types)
-        print("header:", message.header)
-        print("intensities:", message.intensities)
-        print("range_max:", message.range_max)
-        print("range_min:", message.range_min)
-        print("ranges:", message.ranges)
-        print("scan_time:", message.scan_time)
-        print("time_increment:", message.time_increment)
-        print("-----")
+    while True:
+        message = socket.recv_pyobj()
 
-        detections = proximity.detections_to_coords(message.ranges)
-        neurons = coords_to_neuron_ids(detections, cortical_area='proximity')
-        runtime_data.fcl_queue.put(neurons)
+        if message is not None:
+            # print("SLOT_TYPES", message.SLOT_TYPES)
+            # print("angle_increment:", message.angle_increment)
+            # print("angle_max:", message.angle_max)
+            # print("angle_min:", message.angle_min)
+            # print("get_fields_and_field_types:", message.get_fields_and_field_types)
+            # print("header:", message.header)
+            # print("intensities:", message.intensities)
+            # print("range_max:", message.range_max)
+            # print("range_min:", message.range_min)
+            # print("ranges:", message.ranges)
+            # print("scan_time:", message.scan_time)
+            # print("time_increment:", message.time_increment)
+            # print("-----")
+
+            detections = proximity.detections_to_coords(message.ranges)
+            neurons = proximity.coords_to_neuron_ids(
+                detections, cortical_area='proximity'
+            )
+            runtime_data.fcl_queue.put({'proximity': set(neurons)})
