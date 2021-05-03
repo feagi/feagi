1abc20d38b3611f6d19706f934be73b2789ce4e9
eric <ermiller@alumni.cmu.edu>
2021-05-03 12:49:47 -0400

    remove gnome-terminal

--- src/ros/run_in_container.sh
+++ src/ros/run_in_container.sh
@@ -29,6 +29,6 @@ colcon build
 source ~/ros2_ws/install/setup.bash
 
 # run ros processes
-# gnome-terminal ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
-# gnome-terminal ros2 run py_topic py_laser_scan
+ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
+# ros2 run py_topic py_laser_scan
 # gnome-terminal ros2 run turtlebot3_teleop teleop_keyboard
