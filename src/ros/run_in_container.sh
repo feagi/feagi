1041b87868be72280be7196ad28dfdf0ed83cfb7
eric <ermiller@alumni.cmu.edu>
2021-05-04 00:07:45 -0400

    run from compose via cmd param

--- src/ros/run_in_container.sh
+++ src/ros/run_in_container.sh
@@ -6,12 +6,16 @@ cd turtlebot3_ws || exit
 wget https://raw.githubusercontent.com/ROBOTIS-GIT/turtlebot3/ros2/turtlebot3.repos
 vcs import ~/turtlebot3_ws/src < turtlebot3.repos
 source /opt/ros/foxy/setup.bash
-colcon build --symlink-install
-echo "source /install/setup.bash" >> /root/.bashrc
-echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models" >> /root/.bashrc
-echo "export TURTLEBOT3_MODEL=waffle_pi" >> /root/.bashrc
-echo "source /opt/ros/foxy/setup.bash" >> /root/.bashrc
-source /root/.bashrc
+colcon build --symlink-install --continue-on-error
+# echo "source /install/setup.bash" >> /root/.bashrc
+# echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models" >> /root/.bashrc
+# echo "export TURTLEBOT3_MODEL=burger" >> /root/.bashrc
+# echo "source /opt/ros/foxy/setup.bash" >> /root/.bashrc
+# source /root/.bashrc
+source install/setup.bash
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models
+export TURTLEBOT3_MODEL=burger
+source /opt/ros/foxy/setup.bash
 
 # ros workspace setup
 cd ~
@@ -26,9 +30,10 @@ sed '9i\  <buildtool_depend>ament_python</buildtool_depend>\n  <exec_depend>rclp
 sed '23i\             "ros_laser_scan = py_topic.ros_laser_scan:main",\n             "ros_teleop = py_topic.ros_teleop:main"' ~/ros2_ws/src/py_topic/setup.py > changed.txt && mv changed.txt ~/ros2_ws/src/py_topic/setup.py
 cd ~/ros2_ws || exit
 colcon build
-source ~/ros2_ws/install/setup.bash
+source /opt/ros/foxy/setup.bash
+source ros2_ws/install/setup.bash
 
 # run ros processes
-ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
+# ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
 # ros2 run py_topic py_laser_scan
 # gnome-terminal ros2 run turtlebot3_teleop teleop_keyboard
