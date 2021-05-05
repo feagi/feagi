06dbd672d0bfc3ff0f2a9c83cd1f019cd8a7dcc5
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-05-05 05:43:17 -0600

    Update to correct the output

--- src/ros/ardiunotopython/test.py
+++ src/ros/ardiunotopython/test.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import struct
 
 #ser = serial.Serial("/dev/ttyACM0", 9600)
 ser = serial.Serial(
@@ -9,7 +10,16 @@ ser = serial.Serial(
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS
     )
+
 while (1):
 	#h1=ser.readline()
-    print(int(ser.readline()))
+    if(ser.inWaiting()>0):
+        value = ser.readline()
+        #print(int('101',2))
+        data=ser.readline().decode('ascii').strip(' ')
+        print(int(data))
+        print(type(int(data)))
     time.sleep(0.1)
+
+
+
