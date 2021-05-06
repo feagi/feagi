6d6187825b9cb7db0973e38608c7e56754611f2f
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-05-06 08:17:54 -0600

    Adding for the properly set up for HC_SR04_Foxy.py as sonar_sensor instead of talker

---
+++ src/ros/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'py_topic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bwuk',
+    maintainer_email='kevin.a.araujo@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+             'listener = py_topic.ros_laser_scan:main',
+             'sonar_sensor = py_topic.HC_SR04_Foxy:main',
+		'py_laser_scan = py_topic.ros_laser_scan:main'
+        ],
+    },
+)
