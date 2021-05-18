afeeef2b91efcabab688b94f0770a1c397769a83
Eric Miller <51423770+erm156@users.noreply.github.com>
2021-05-17 22:10:08 -0400

    dockerize turtlebot <-> feagi system (#25)
    * check in progress dockerizing turtlebot system
    
    * restructure docker/shell script interaction
    
    * remove gnome-terminal
    
    * run from compose via cmd param
    
    * format cmd in compose yml
    
    * open terminal windows for ros cmds
    
    * use new ros docker image
    
    * change socket address for use in container
    
    * generalize socket address
    
    * update socket address in config
    
    * test socket address again
    
    * user container/service name as hostname
    
    * socket address test
    
    * test
    
    * revert socket address changes
    
    * input test
    
    * another input test
    
    * add missing import for test
    
    * yet another input test
    
    * revert changes used in local tests
    
    * comment out MNIST lines
    
    * remove zmq install (already in dev reqs)
    
    * update Dockerfile and compose yaml
    
    * remove ENV variable (didn't seem to work)
    
    * add instructions for deploying the interface
    
    * update reqs to match dev for docker builds

--- src/ros/setup_container.sh
+++ src/ros/setup_container.sh
@@ -1,6 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# turtlebot3 setup
+mkdir -p ~/turtlebot3_ws/src
+cd ~/turtlebot3_ws || exit
+wget https://raw.githubusercontent.com/ROBOTIS-GIT/turtlebot3/ros2/turtlebot3.repos
+vcs import ~/turtlebot3_ws/src < turtlebot3.repos
 source /opt/ros/foxy/setup.bash
+colcon build --symlink-install --continue-on-error
+source install/setup.bash
+echo 'export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models' >> ~/.bashrc
+echo 'export TURTLEBOT3_MODEL=burger' >> ~/.bashrc
+echo 'source /opt/ros/foxy/setup.bash' >> ~/.bashrc
+echo 'source ~/ros2_ws/install/setup.bash' >> ~/.bashrc
+
+# ros workspace setup
+cd ~
 mypath=`pwd`
+pip3 install zmq
+source /opt/ros/foxy/setup.bash
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws/src || exit
 ros2 pkg create --build-type ament_python py_topic
@@ -9,8 +26,5 @@ sed '9i\  <buildtool_depend>ament_python</buildtool_depend>\n  <exec_depend>rclp
 sed '23i\             "ros_laser_scan = py_topic.ros_laser_scan:main",\n             "ros_teleop = py_topic.ros_teleop:main"' ~/ros2_ws/src/py_topic/setup.py > changed.txt && mv changed.txt ~/ros2_ws/src/py_topic/setup.py
 cd ~/ros2_ws || exit
 colcon build
-echo 'sourcing...'
+source /opt/ros/foxy/setup.bash
 source ~/ros2_ws/install/setup.bash
-echo 'completed'
-#gnome-terminal -- ros2 run py_topic ros_laser_scan
-#gnome-terminal -- ros2 run py_topic ros_teleop
