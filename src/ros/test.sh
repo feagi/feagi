dbc8cda6154c3102d16ccd7066b4fe2682f7e716
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-06-03 11:58:07 -0600

    create a loop script for background to test. This is not the official script nor will be used in future

--- src/ros/test.sh
+++ src/ros/test.sh
@@ -3,14 +3,9 @@
 #sudo apt-get install xterm	
 name="FEAGI FOXY ARDIUNO TOPIC" #easier to remember which to run on.
 echo $name
+for name in $name
+do
 # opens terminal but then I can't control terminal afterwards
-
-if [[ "$name" == 'FEAGI' ]];then
-	echo $name && xterm -hold -e "'echo Hello My World'; cd; pwd'" &
-elif  [ $name == 'FOXY' ]
-then
-	xterm -hold -e "echo test"
-else
-	echo "else"
-fi
+echo "test"
+sleep 2
 done
