358817bfa625de6c87b832abebfb28b5453ef323
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-06-08 10:48:09 -0600

    Added the agent to be ready for container

--- src/ros/setup_container.sh
+++ src/ros/setup_container.sh
@@ -62,6 +62,20 @@ cp $mypath/ros_laser_scan.py $mypath/ros_teleop.py ~/ros2_ws/src/py_topic/py_top
 sed '9i\  <buildtool_depend>ament_python</buildtool_depend>\n  <exec_depend>rclpy</exec_depend>\n  <exec_depend>geometry_msgs</exec_depend>' ~/ros2_ws/src/py_topic/package.xml > changed.txt && mv changed.txt ~/ros2_ws/src/py_topic/package.xml
 sed '23i\             "ros_laser_scan = py_topic.ros_laser_scan:main",\n             "ros_teleop = py_topic.ros_teleop:main"' ~/ros2_ws/src/py_topic/setup.py > changed.txt && mv changed.txt ~/ros2_ws/src/py_topic/setup.py
 cd ~/ros2_ws || exit
+pip3 install zmq
 colcon build
 source /opt/ros/foxy/setup.bash
 source ~/ros2_ws/install/setup.bash
+
+#Install micro-ros
+cd ~
+mkdir micro_ros_arduino
+cd micro_ros_arduino
+source /opt/ros/$ROS_DISTRO/setup.bash
+git clone -b main https://github.com/micro-ROS/micro_ros_setup.git src/micro_ros_setup
+rosdep update && rosdep install --from-path src --ignore-src -y
+colcon build
+source install/local_setup.bash
+ros2 run micro_ros_setup create_agent_ws.sh
+ros2 run micro_ros_setup build_agent.sh
+source install/local_setup.sh
