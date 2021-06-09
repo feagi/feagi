f8f5700bd09459ad6cf8d7be30c6b569e24ffa5f
Eric Miller <ermiller@alumni.cmu.edu>
2021-06-08 23:57:26 -0400

    modify container launch script

--- src/ros/launch_in_container.sh
+++ src/ros/launch_in_container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source ~/ros2_ws/install/setup.bash
 ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py &
-ros2 run py_topic ros_laser_scan
+ros2 run py_topic ros_laser_scan &
+ros2 run turtlebot3_teleop teleop_keyboard
