b5fbecbdfa53f077b44ac077996ef621cea35dce
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-06-11 11:46:50 -0600

    adding sonar and ros2 files

--- src/ros/setup_container.sh
+++ src/ros/setup_container.sh
@@ -48,9 +48,10 @@ source /opt/ros/foxy/setup.bash
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws/src || exit
 ros2 pkg create --build-type ament_python py_topic
-cp $mypath/ros_laser_scan.py $mypath/ros_teleop.py ~/ros2_ws/src/py_topic/py_topic/
+cp $mypath/ros_laser_scan.py $mypath/HC_SR04_Foxy.py $mypath/Sonar_reader1.py $mypath/Sonar_reader.py $mypath/micro-ros_publisher/ $mypath/ardiunotopython/ $mypath/ros_teleop.py ~/ros2_ws/src/py_topic/py_topic/
 sed '9i\  <buildtool_depend>ament_python</buildtool_depend>\n  <exec_depend>rclpy</exec_depend>\n  <exec_depend>geometry_msgs</exec_depend>' ~/ros2_ws/src/py_topic/package.xml > changed.txt && mv changed.txt ~/ros2_ws/src/py_topic/package.xml
-sed '23i\             "ros_laser_scan = py_topic.ros_laser_scan:main",\n             "ros_teleop = py_topic.ros_teleop:main"' ~/ros2_ws/src/py_topic/setup.py > changed.txt && mv changed.txt ~/ros2_ws/src/py_topic/setup.py
+sed '23i\             "ros_laser_scan = py_topic.ros_laser_scan:main",\n             "ros_teleop = py_topic.ros_teleop:main"' ~/ros2_ws/src/py_topic/setup.py > changed.txt && mv changed.txt ~/ros2_ws/src/py_topic/setup.py 
+cp $mypath/setup.py ~/ros2_ws/src/py_topic/
 cd ~/ros2_ws/ || exit
 cp ~/feagi-core/src/ros/setup.py ~/ros2_ws/src/py_topic/
 cp ~/feagi-core/src/ros/* ~/ros2_ws/src/py_topic/py_topic/
