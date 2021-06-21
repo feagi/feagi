b97257d15b56d2cdbb9556d1a3682cb6e6de0bf5
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-06-21 09:20:16 -0600

    improve the script to upload file into arduino automatically

--- src/ros/test.sh
+++ src/ros/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd arduino-cli/
-export PATH=$PATH:/home/bwuk/arduino-cli/bin
+export PATH=$PATH:/home/ubuntu/arduino-cli/bin
+arduino-cli board attach serial:///dev/ttyACM0 micro-ros_publisher
 arduino-cli compile --port /dev/ttyACM0 micro-ros_publisher
 arduino-cli upload --port /dev/ttyACM0 micro-ros_publisher
