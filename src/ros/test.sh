3082e001c2b5bcd5a8b0b7a8e84d135a3139c14a
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-06-21 10:20:32 -0600

    complete to upload the board with one click

--- src/ros/test.sh
+++ src/ros/test.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+DIRECTORY="$HOME/Arduino/libraries/micro_ros_arduino/"
+
+if [ -d "$DIRECTORY" ]
+then
+	export PATH=$PATH:/home/ubuntu/arduino-cli/bin
+	arduino-cli lib search newping
+	arduino-cli lib install newping
+	cd ~
+	cd Arduino/libraries/
+	git clone -b foxy https://github.com/micro-ROS/micro_ros_arduino.git
+else
+	echo "Micro ros exists"
+fi
 
 cd arduino-cli/
 export PATH=$PATH:/home/ubuntu/arduino-cli/bin
