ec6c832c7b845e48fa302ccb0c978a97159f84be
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-06-23 11:58:13 -0600

    test.sh

---
+++ src/ros/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "First time on the board only."
+echo "This will erase everything in your board."
+echo "This will start in 5 seconds. "
+echo "To cancel this process, press ctrl C"
+sleep 5
+export PATH=$PATH:/home/ubuntu/arduino-cli/bin
+mv ardiunotopython/ arduino-cli/
+arduino-cli core install arduino:avr
+arduino-cli lib search newping
+arduino-cli lib install newping
+cd ~
+cd Arduino/libraries/
+git clone -b foxy https://github.com/micro-ROS/micro_ros_arduino.git
+cd ~
+cd arduino-cli/
+export PATH=$PATH:/home/ubuntu/arduino-cli/bin
+arduino-cli board attach serial:///dev/ttyACM0 ardiunotopython
+arduino-cli compile --port /dev/ttyACM0 ardiunotopython
+arduino-cli upload --port /dev/ttyACM0 ardiunotopython
+./linux_py2arduino.sh
