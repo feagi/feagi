ba3a6ae1ab3268f28df64dd16c5a7aad096eee14
Eric Miller <51423770+erm156@users.noreply.github.com>
2021-07-02 11:58:11 -0400

    functional checkpoint for container development (#40)
    * check in updates to dockerizing turtlebot
    
    * run turtlebot as background process
    
    * map host USB to container USB
    
    * use non-VNC ROS docker image
    
    * make ROS container privileged to access host USB
    
    * modify container launch script
    
    * test container connectivity
    
    * update laser scan socket address
    
    * remove unneeded imports
    
    * update host addresses for connecting containers
    
    * remove leftover merge conflict dialog
    
    * debugging FEAGI/ROS socket connection
    
    * fix bug
    
    * revert changes and debugging
    
    * try different port for FEAGI container
    
    * revert debugging changes
    
    * add tools/changes for troubleshooting container connectivity
    
    * convert incoming sonar data to int
    
    * debugging
    
    * update container hostname with static IP
    
    * update FEAGI lidar socket subscription
    
    * fix hostname typo
    
    * revert hostname changes for debugging
    
    * listen on all interfaces for mvmt OPU
    
    * update lidar address port
    
    * check in troubleshooting changes
    
    * add talker/listener for debugging
    
    * test service name as hostname on FEAGI side
    
    * update docker-compose file
    
    * remove packages installed for debugging
    
    * update LIDAR interface
    
    * differentiate between and process LIDAR/SONAR data
    
    * A fully functional checkpoint
    
    * remove unneeded tools from Dockerfiles
    
    * revert interface change
    
    * resolve interface for LIDAR data acquisition
    
    * revert interface resolving on FEAGI side for now
    
    * remove branch checkout
    
    * remove zmq debugging app
    
    * update teleop interface for running in container
    
    Co-authored-by: Mohammad Nadji-Tehrani <m.nadji.tehrani@gmail.com>

--- src/ros/ros_laser_scan.py
+++ src/ros/ros_laser_scan.py
@@ -39,16 +39,13 @@ Note: This module is a modified version of the code from OSRF
 # limitations under the License.
 
 """
-
-import sensor_msgs.msg #this is needed to read lidar or any related to lidar.
 import rclpy
 import zmq
 
-from time import sleep
 from rclpy.node import Node
-from sensor_msgs.msg import LaserScan #to call laserscan so it can convert the data or provide the data
+from sensor_msgs.msg import LaserScan
 from rclpy.qos import QoSProfile
-from rclpy.qos import qos_profile_sensor_data #this is required to have a full data
+from rclpy.qos import qos_profile_sensor_data
 
 print("Starting FEAGI-ROS Laser Scan Interface...")
 
@@ -76,10 +73,12 @@ class MinimalSubscriber(Node):
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        # self.get_logger().info("I heard: {}".format(msg)) #put .format(msg) to display the data
-        self.get_logger().info("angle_max: {}".format(msg.angle_max)) #put .format(msg) to display the data
-
-        socket.send_pyobj(msg)
+        try:
+            ranges = msg.ranges
+            socket.send_pyobj(ranges)
+        except AttributeError:
+            socket.send_pyobj(msg)
+            self.get_logger().info("angle_max: {}".format(msg.angle_max))
 
 
 def main(args=None):
