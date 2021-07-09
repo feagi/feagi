9e78492c9d0cf35c509412338724798962808593
Eric Miller <51423770+erm156@users.noreply.github.com>
2021-07-09 17:33:12 -0400

    separate builds for turtlebot/sonar (#45)
    * separate turtlebot from container setup scripts
    
    * complete separation of turtlebot/micro-ros services in docker builds
    
    * update docker-compose files to reflect docker image changes
    
    * restructure so subdirectories contain needed all dependencies
    
    * delete unneeded files, etc.

--- 3rd_party/ros/Micro-ROS/start_sonar.sh
+++ 3rd_party/ros/Micro-ROS/start_sonar.sh
@@ -4,12 +4,12 @@ echo "This will erase everything in your board."
 echo "This will start in 5 seconds. "
 echo "To cancel this process, press ctrl C"
 sleep 5
-/bin/bash ./setup_container.sh
-/bin/bash ./ws_setup.sh
+# /bin/bash ./setup_container.sh
+# /bin/bash ./ws_setup.sh
 /bin/bash ./sonar_setup.sh
-export PATH=$PATH:/home/$USER/arduino-cli/bin
+export PATH=$PATH:/root/$USER/arduino-cli/bin
 mkdir ~/arduino-cli/ardiunotopython/
-cp ~/feagi-core/src/arduino/ardiunotopython/ardiunotopython.ino ~/arduino-cli/ardiunotopython/
+cp ~/ardiunotopython.ino ~/arduino-cli/ardiunotopython/
 arduino-cli core install arduino:avr
 arduino-cli lib search newping
 arduino-cli lib install newping
@@ -18,9 +18,10 @@ cd Arduino/libraries/
 git clone -b foxy https://github.com/micro-ROS/micro_ros_arduino.git
 cd ~
 cd arduino-cli/
-export PATH=$PATH:/home/$USER/arduino-cli/bin
+export PATH=$PATH:/root/$USER/arduino-cli/bin
 arduino-cli board attach serial:///dev/ttyACM0 ardiunotopython
 arduino-cli compile --port /dev/ttyACM0 ardiunotopython
 arduino-cli upload --port /dev/ttyACM0 ardiunotopython
 #/bin/bash ~/linux_py2arduino.sh
+source /opt/ros/foxy/setup.bash
 cd ~/ros2_ws && source install/setup.bash && ros2 run py_topic sonar_sensor
