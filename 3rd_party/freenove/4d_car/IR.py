55469d3066be6d90f51383d73097dc70c2538721
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-07-22 15:11:14 -0600

    changed it into a function

--- 3rd_party/freenove/4d_car/IR.py
+++ 3rd_party/freenove/4d_car/IR.py
@@ -1,4 +1,4 @@
-# Class of Line_Tracking is made by Freenove. 
+# Class of Line_Tracking is made by Freenove.
 # The run definition is heavily modified and the motors are not in this code.
 # This responds to contrast of light and dark.
 
@@ -13,20 +13,23 @@ class Line_Tracking:
         GPIO.setup(self.IR01,GPIO.IN)
         GPIO.setup(self.IR02,GPIO.IN)
         GPIO.setup(self.IR03,GPIO.IN)
-    def run(self):
-        while True:
-            if GPIO.input(self.IR01)==True:
-                print("Left")
-            if GPIO.input(self.IR02)==True:
-                print("Middle")
-            if GPIO.input(self.IR03)==True:
-                print("Right")
 
-infrared=Line_Tracking()
+    def infrared(self,position):
+        if position == 1:
+            if GPIO.input(self.IR01) == True:
+                print("Left has been detected on bright")
+        elif position == 2:
+            if GPIO.input(self.IR02) == True:
+                print("Middle has been detected on bright")
+        elif position == 3:
+            if GPIO.input(self.IR03) == True:
+                print("Right has been detected on bright")
+        else:
+            print("Nothing has been detected.")
+
+IR_read=Line_Tracking()
 # Main program logic follows:
 if __name__ == '__main__':
-    print ('Program is starting ... ')
-    infrared.run()
 
-
-
+    while True:
+        IR_read.infrared(2)
