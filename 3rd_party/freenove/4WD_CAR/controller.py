902c5c9866d3ffb3a7cc4c75df14fb92a43d224d
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2021-07-26 11:35:02 -0400

    Bug Fix

--- 3rd_party/freenove/4WD_CAR/controller.py
+++ 3rd_party/freenove/4WD_CAR/controller.py
@@ -112,30 +112,31 @@ class Servo:
         self.PwmServo.setPWMFreq(50)
         self.PwmServo.setServoPulse(8,1500)
         self.PwmServo.setServoPulse(9,1500)
+
     def setServoPwm(self,channel,angle,error=10):
-        angle=int(angle)
-        if channel=='0':
+        angle = int(angle)
+        if channel == '0':
             self.PwmServo.setServoPulse(8,2500-int((angle+error)/0.09))
-        elif channel=='1':
+        elif channel == '1':
             self.PwmServo.setServoPulse(9,500+int((angle+error)/0.09))
-        elif channel=='2':
+        elif channel == '2':
             self.PwmServo.setServoPulse(10,500+int((angle+error)/0.09))
-        elif channel=='3':
+        elif channel == '3':
             self.PwmServo.setServoPulse(11,500+int((angle+error)/0.09))
-        elif channel=='4':
+        elif channel == '4':
             self.PwmServo.setServoPulse(12,500+int((angle+error)/0.09))
-        elif channel=='5':
+        elif channel == '5':
             self.PwmServo.setServoPulse(13,500+int((angle+error)/0.09))
-        elif channel=='6':
+        elif channel == '6':
             self.PwmServo.setServoPulse(14,500+int((angle+error)/0.09))
-        elif channel=='7':
+        elif channel == '7':
             self.PwmServo.setServoPulse(15,500+int((angle+error)/0.09))
 
     def head_UP_DOWN(self, num):
-        pwm.setServoPwm('1', num) #90 to 0 degree is turn the head down. 90 to 180 is to turn the head up
+        self.setServoPwm('1', num) #90 to 0 degree is turn the head down. 90 to 180 is to turn the head up
 
     def head_RIGHT_LEFT(self, num):
-        pwm.setServoPwm('0', num) #90 to 0 degree is turn the head left. 90 to 180 is to turn the head right
+        self.setServoPwm('0', num) #90 to 0 degree is turn the head left. 90 to 180 is to turn the head right
 
 
 class Motor:
