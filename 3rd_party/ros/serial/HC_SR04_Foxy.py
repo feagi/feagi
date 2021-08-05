c2e4568ca1b478aea32b972911d448d290fea252
Eric Miller <51423770+erm156@users.noreply.github.com>
2021-08-05 10:39:22 -0400

    fix proximity processor and seed genome issues  (#48)
    * reconnect ROS publisher/subscriber
    
    * update FEAGI docker image being pulled
    
    * change logic for sonar-based neuron activation
    
    * add print statements to view outputs
    
    * more debugging
    
    * add biased rounding to value mapping
    
    * more debugging of proximity processing
    
    * remove some print statements
    
    * increase number of proximity neurons
    
    * examine block dictionary for proximity cortical area
    
    * check length of proximity block dictionary
    
    * reduce number of proximity neurons
    
    * adjust proximity area geometry to test neuron activation
    
    * adjust parameters for sonar data processing
    
    * more proximity processory debugging
    
    * increase proximity area z dimension size
    
    * proximity processor debugging via container
    
    * fix block referencing
    
    * view proximity block dictionary during runtime
    
    * more proximity i/o debugging
    
    * more print statements for debugging
    
    * remove IDE-generated auto-import
    
    * update proximity memory cortical area params
    
    * view proximity block dictionary output
    
    * increase proximity area neuron count
    
    * increase proximity memory neuron count
    
    * reduce cortical neuron counts
    
    * reduce proximity cortical neuron count for testing on pi
    
    * reduce prox cortical neuron count even further
    
    * reduce cortical neuron count
    
    * remove print statements
    
    * remove thresholding
    
    * adjust Z_MAX
    
    * add more debugging of block dict key errors
    
    * debugging
    
    * revert Z_MAX
    
    * check that mapped value falls within appropriate range
    
    * use pre-existing function
    
    * round mapped value
    
    * resize proximity blocks
    
    * avoid hard-coding x/y vals
    
    * avoid hard-coding LIDAR detection coords
    
    * remove unused functions
    
    * reduce proximity neuron counts
    
    * fix SyntaxWarning(s)
    
    * fix publisher sonar value handling
    
    * remove pub/sub sonar logging
    
    * test cortical area config for connecting prox to prox memory
    
    * fix another SyntaxWarning
    
    * remove print statements
    
    * remove branch checkout

--- 3rd_party/ros/serial/HC_SR04_Foxy.py
+++ 3rd_party/ros/serial/HC_SR04_Foxy.py
@@ -2,12 +2,11 @@ import rclpy
 import serial
 import time
 import std_msgs
-import zmq
 
 from example_interfaces.msg import Int64
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
-from rclpy.qos import qos_profile_sensor_data  # this is required to have a full data
+from rclpy.qos import qos_profile_sensor_data
 
 ser = serial.Serial(
     port="/dev/ttyACM0",
@@ -15,15 +14,8 @@ ser = serial.Serial(
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
-)  # connect to ardiuno port.
-# serialcomm.timeout = 1
-print("Found the ardiuno board.")
-print("Creating the /scan topic..")
+)
 
-socket_address = "tcp://0.0.0.0:2000"
-context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.bind(socket_address)
 
 class MinimalPublisher(Node):
 
@@ -34,28 +26,13 @@ class MinimalPublisher(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
-    def timer_callback(self):  # this is the part where we need to get it keep running
-        # check = ser.readline()
-        # if check == ' ':  # this if statement is to skip string id. It doesn't seem like it works
-        #     print("Skipped the ' '")  # in #44 line, it kept recieving a string ' '
-        # else:
-        #     sensorvalue = float(ser.readline())  # posts the value
-        # msg = Int64()
-        # msg.data= int(sensorvalue)
-        # print(msg)
-        # self.get_logger().info("distance: {}".format(sensorvalue))
-        # print(type(msg))  # this is to verify the type of the value. It should be float only
-        # self.publisher_.publish(msg)  # this is to publish the data to topic 'scann'. It can change to 'scan' in #34 line
-        # self.i += 1
-        bytes = ser.readline()
-        data = bytes.decode(encoding="utf-8").strip("\r\n")
-        if data is not None and data != '':
-            if data[:4] == 'Ping':
-                data = data[5:]
-                data = data[:-2]
-            distance = int(float(data))
-            # self.get_logger().info(str(distance))
-            socket.send_pyobj(distance)
+    def timer_callback(self):
+        sensor_val = ser.readline()
+        if sensor_val != ' ':
+            msg = Int64()
+            msg.data= int(sensor_val)
+            # self.get_logger().info("PUBLISHER: {}".format(msg.data))
+            self.publisher_.publish(msg)
 
 
 def main(args=None):
@@ -70,9 +47,8 @@ def main(args=None):
     # when the garbage collector destroys the node object)
     minimal_publisher.destroy_node()
     rclpy.shutdown()
-    serialcomm.close()
+    ser.close()
 
 
 if __name__ == '__main__':
     main()
-
