b91968cf9e30cc1110a410f038c082958247f3cf
Eric Miller <ermiller@alumni.cmu.edu>
2021-08-26 20:37:33 +0000

    add ultrasonic class to smart_car controller

--- third_party/freenove/smart_car/controller.py
+++ third_party/freenove/smart_car/controller.py
@@ -384,6 +384,7 @@ class Motor:
         time.sleep(3)
         self.stop()
 
+
 class Photoresistor:
     """
     Photoresistor has two photoresistors. 0 is the left photoresistor and 1 is the right photoresistor on the board.
@@ -409,3 +410,42 @@ class Battery:
         Power = adc.recvADC(2) * 3
         # print(Power)
         return Power
+
+
+class Ultrasonic:
+    def __init__(self):
+        GPIO.setwarnings(False)
+        self.trigger_pin = 27
+        self.echo_pin = 22
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.trigger_pin,GPIO.OUT)
+        GPIO.setup(self.echo_pin,GPIO.IN)
+
+    def send_trigger_pulse(self):
+        GPIO.output(self.trigger_pin,True)
+        time.sleep(0.00015)
+        GPIO.output(self.trigger_pin,False)
+
+    def wait_for_echo(self,value,timeout):
+        count = timeout
+        while GPIO.input(self.echo_pin) != value and count>0:
+            count = count-1
+
+    def getDistance(self):
+        """
+
+        Returns distance_cm in INT value
+        -------
+
+        """
+        distance_cm=[0,0,0]
+        for i in range(3):
+            self.send_trigger_pulse()
+            self.wait_for_echo(True,10000)
+            start = time.time()
+            self.wait_for_echo(False,10000)
+            finish = time.time()
+            pulse_len = finish-start
+            distance_cm[i] = pulse_len/0.000058
+        distance_cm=sorted(distance_cm)
+        return int(distance_cm[1])
