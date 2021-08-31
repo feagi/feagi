680938604c48c3e826cdd1e33b9556bc4cbbd234
Eric Miller <ermiller@alumni.cmu.edu>
2021-08-31 15:48:27 +0000

    update Pi local GPIO setup script

--- third_party/Raspberry_PI/setup_GPIO.sh
+++ third_party/Raspberry_PI/setup_GPIO.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install python3-smbus
-sudo apt-get install i2c-tools
-sudo apt-get install python3-dev python3-rpi.gpio
-sudo apt install python3-gpiozero
-
+pip3 install RPi.GPIO==0.7.0
+pip3 install smbus==1.1.post2
+pip3 install rpi-ws281x==4.3.0
