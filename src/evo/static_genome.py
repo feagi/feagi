04f88567c481a3638b93457d7f660deb8834e5dc
Eric Miller <ermiller@alumni.cmu.edu>
2021-08-31 16:08:16 +0000

    add base line-tracking/IR cortical area

--- src/evo/static_genome.py
+++ src/evo/static_genome.py
@@ -3788,6 +3788,58 @@ genome = {
                 ]
             }
         }
+    },
+    "line_tracking": {
+        "growth_path": "",
+        "group_id": "IPU",
+        "sub_group_id": "IPU_line_tracking",
+        "plot_index": 1,
+        "orientation_selectivity_pattern": "",
+        "location": "",
+        "kernel_size": 7,
+        "cortical_neuron_count": 3,
+        "location_generation_type": "sequential",
+        "synapse_attractivity": 100,
+        "init_synapse_needed": False,
+        "postsynaptic_current": 501,
+        "plasticity_constant": 0.05,
+        "postsynaptic_current_max": 501,
+        "neighbor_locator_rule_id": "rule_0",
+        "neighbor_locator_rule_param_id": "param_1",
+        "cortical_mapping_dst": {},
+        "neuron_params": {
+            "activation_function_id": "",
+            "orientation_selectivity_id": "",
+            "depolarization_threshold": 5,
+            "firing_threshold": 1,
+            "firing_pattern_id": "",
+            "refractory_period": 0,
+            "axon_avg_length": "",
+            "leak_coefficient": 10,
+            "axon_avg_connections": "",
+            "axon_orientation function": "",
+            "consecutive_fire_cnt_max": 3,
+            "snooze_length": 0,
+            "block_boundaries": [
+                3,
+                1,
+                1
+            ],
+            "geometric_boundaries": {
+                "x": [
+                    0,
+                    3
+                ],
+                "y": [
+                    0,
+                    1
+                ],
+                "z": [
+                    0,
+                    1
+                ]
+            }
+        }
     }
   }
 }
