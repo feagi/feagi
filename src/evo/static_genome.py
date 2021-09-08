d933f10f59189c976507f6f25accbb0c478b8898
Eric Miller <ermiller@alumni.cmu.edu>
2021-09-08 21:42:02 +0000

    remove extra motor thalamus area in genome and rewire ir_ipu

--- src/evo/static_genome.py
+++ src/evo/static_genome.py
@@ -3357,7 +3357,7 @@ genome = {
         "location_generation_type" : "random",
         "synapse_attractivity" : 100,
         "init_synapse_needed" : False,
-        "postsynaptic_current" : -5000,
+        "postsynaptic_current" : -500,
         "plasticity_constant" : 0.05,
         "postsynaptic_current_max" : 3501,
         "neighbor_locator_rule_id" : "rule_5",
@@ -3516,63 +3516,6 @@ genome = {
                 ]
             }
         }
-      },
-      "motor_thalamus": {
-          "growth_path": "",
-          "group_id": "MOTOR",
-          "sub_group_id": "Motor_Cortex",
-          "plot_index": 1,
-          "orientation_selectivity_pattern": "",
-          "location": "",
-          "kernel_size": 7,
-          "cortical_neuron_count": 1000,
-          "location_generation_type": "random",
-          "synapse_attractivity": 100,
-          "init_synapse_needed": False,
-          "postsynaptic_current": 51,
-          "plasticity_constant": 0.05,
-          "postsynaptic_current_max": 1,
-          "neighbor_locator_rule_id": "rule_1",
-          "neighbor_locator_rule_param_id": "param_1",
-          "cortical_mapping_dst": {
-              "motor_thalamus": {
-                  "neighbor_locator_rule_id": "rule_6",
-                  "neighbor_locator_rule_param_id": "param_2"
-              }
-          },
-          "neuron_params": {
-              "activation_function_id": "",
-              "orientation_selectivity_id": "",
-              "depolarization_threshold": 20,
-              "firing_threshold": 1,
-              "firing_pattern_id": "",
-              "refractory_period": 0,
-              "axon_avg_length": "",
-              "leak_coefficient": 1,
-              "axon_avg_connections": "",
-              "axon_orientation function": "",
-              "consecutive_fire_cnt_max": 1,
-              "snooze_length": 0,
-              "block_boundaries": [
-                  5,
-                  5,
-                  20
-              ],
-              "geometric_boundaries": {
-                  "x": [
-                      0,
-                      5
-                  ],
-                  "y": [
-                      0,
-                      5
-                  ],
-                  "z": [
-                      0,
-                      20
-                  ]
-              }
-          }
       },
       "motor_memory": {
           "growth_path": "",
@@ -3753,12 +3696,20 @@ genome = {
         "neighbor_locator_rule_id": "rule_0",
         "neighbor_locator_rule_param_id": "param_1",
         "cortical_mapping_dst": {
-            "ir_mapper": {
-                "neighbor_locator_rule_id" : "rule_7",
-                "neighbor_locator_rule_param_id" : "param_1"
-            },
-            "motor_ipu": {
-                "neighbor_locator_rule_id": "rule_6",
+            # "ir_mapper": {
+            #     "neighbor_locator_rule_id" : "rule_7",
+            #     "neighbor_locator_rule_param_id" : "param_1"
+            # },
+            # "motor_ipu": {
+            #     "neighbor_locator_rule_id": "rule_6",
+            #     "neighbor_locator_rule_param_id": "param_1"
+            # }
+            # "motor_opu": {
+            #     "neighbor_locator_rule_id": "rule_6",
+            #     "neighbor_locator_rule_param_id": "param_1"
+            # }
+            "motor_opu": {
+                "neighbor_locator_rule_id": "rule_7",
                 "neighbor_locator_rule_param_id": "param_1"
             }
         },
