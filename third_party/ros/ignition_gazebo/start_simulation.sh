46df75926419079a3b6b635a9e8d87eb6c025e08
Eric Miller <ermiller@alumni.cmu.edu>
2021-09-13 18:08:21 -0400

    check in container dev in working state

--- third_party/ros/ignition_gazebo/start_simulation.sh
+++ third_party/ros/ignition_gazebo/start_simulation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source /opt/ros/foxy/setup.bash
-colcon build --symlink-install
+colcon build
 source install/setup.bash
-ros2 launch freenove_4wd_car_description diff_drive.launch.py
+# ros2 launch freenove_4wd_car_description diff_drive.launch.py
+# ros2 run freenove_4wd_car_description range.py
