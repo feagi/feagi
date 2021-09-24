585ef10970c46894be8cc4bebbc58bc58ad39e54
Eric Miller <ermiller@alumni.cmu.edu>
2021-09-24 18:59:32 -0400

    run main gazebo gui and ultrasonic via script

--- third_party/gazebo/smart_car/setup_simulation.sh
+++ third_party/gazebo/smart_car/setup_simulation.sh
@@ -9,4 +9,4 @@ cd /opt/source-code/freenove_4wd_car_description/
 colcon build
 source install/setup.bash
 ros2 launch freenove_4wd_car_description freenove_smart_car.launch.py &
-xterm -hold -e "echo -e '\033]2;'$foxy_title'\007' && cd /opt/source-code/freenove_4wd_car_description/ && bash ./start_ultrasonic.sh " &
+xterm -hold -e "echo -e '\033]2;'$foxy_title'\007' && cd /opt/source-code/freenove_4wd_car_description/ && bash ./start_ultrasonic.sh" &
