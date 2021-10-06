710f0c4756f794656aaefaee2269b4475efad571
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2021-10-06 17:37:05 -0400

    Debug

--- src/opu/destination/motor.py
+++ src/opu/destination/motor.py
@@ -11,7 +11,7 @@ def motor_operator(motor_id, speed, power=None):
     try:
         # todo: Generalize the following section. using specifics for test only
         runtime_data.opu_pub.send(message=message)
-        print(f">>>>>>>>>>>>>>>>>>>>>>>> {motor_id} activation command sent to router with speed {speed}")
+        print(f">>>>>>>>>>>>>>>>>>>>>>>> {motor_id} activation command sent to router with speed {speed}", message)
 
     except Exception as e:
         print("ERROR at Motor module:", e)
