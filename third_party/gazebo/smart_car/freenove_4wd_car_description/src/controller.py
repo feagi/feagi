12754c2c7d66eb85be36a27361ae0bf985a1fc01
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2021-10-07 15:32:26 -0400

    Motor tuning

--- third_party/gazebo/smart_car/freenove_4wd_car_description/src/controller.py
+++ third_party/gazebo/smart_car/freenove_4wd_car_description/src/controller.py
@@ -131,8 +131,7 @@ class Motor:
                 model_properties['motor']['motor_statuses'][motor_index] = 0
 
             motor_current_position = model_properties['motor']['motor_statuses'][motor_index]
-            motor_position.data = float(((speed * router_settings['feagi_burst_speed']) / 100) +
-                                        motor_current_position)
+            motor_position.data = float((speed * router_settings['feagi_burst_speed']) + motor_current_position)
 
             model_properties['motor']['motor_statuses'][motor_index] = motor_position.data
             print("Motor index + position = ", motor_index, motor_position.data)
