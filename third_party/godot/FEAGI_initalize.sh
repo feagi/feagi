126bf5fd13c590fe2905cbb0e2067c39c80b5534
Kakcalu13 <kevin.a.araujo@gmail.com>
2021-12-16 09:10:16 -0700

    push two different folders for godot

--- third_party/godot/FEAGI_initalize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# FEAGI static_genome.py
-#git clone -b feature-publish_brain_activities https://github.com/feagi/feagi-core
-#cp ~/feagi-core/src/evo/static_genome.py .
-./FEAGI_activity.x86_64 &
-xterm -hold -e "echo -e '\033]2;'FEAGI_activity'\007' && python3 bridge_godot_python.py" &
-
