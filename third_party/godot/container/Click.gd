d2e847133e3c33de0ebc6c536af3523e4d3f916e
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-01-18 21:34:15 -0500

    push latest and current

--- third_party/godot/container/Click.gd
+++ third_party/godot/container/Click.gd
@@ -2,8 +2,8 @@ extends GridMap
 
 onready var selected =  preload("res://selected.meshlib")
 onready var deselected = preload("res://Cortical_area_box.meshlib")
-onready var block_outline = $BlockOutline
-onready var full = $MeshInstance/Area/CollisionShape
+onready var block_outline = preload("res://outline.meshlib")
+
 
 var location = Vector3()
 var Gx = 0
@@ -14,15 +14,7 @@ var Gz = 0
 func _on_Area_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == BUTTON_LEFT and event.pressed == true:
-			print("Clicked")
-			location =get_used_cells()
-			print(location)
 			mesh_library = selected
+			print(get_node("."))
 	else:
 		mesh_library = deselected
-		
-
-
-#func _on_Area_mouse_entered():
-#	block_outline.visible = true
-#	block_outline.translation = Vector3(x,y,z)
