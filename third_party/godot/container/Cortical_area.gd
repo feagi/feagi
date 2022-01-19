c40f133c635e6eb1e2dfc1aac79c3b463d9717e1
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-01-19 15:06:10 -0500

    This is a script to allow a single cube to control, display a specific location, show the ID and change the cube's appearance

--- third_party/godot/container/Cortical_area.gd
+++ third_party/godot/container/Cortical_area.gd
@@ -1,14 +1,38 @@
-extends CSGMesh
+extends CSGBox
+
+onready var white = preload("res://white.material")
+onready var deselected = preload("res://cortical_area.material")
+onready var selected = preload("res://selected.material")
 
 
 var location = Vector3()
 var Gx = 0
 var Gy = 0
 var Gz = 0
+var flag = false
 
 
 func _on_Area_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == BUTTON_LEFT and event.pressed == true:
+		if event.button_index == BUTTON_LEFT and material == selected and event.pressed == true:
+			material = deselected
+			#print(material)
+		elif event.button_index == BUTTON_LEFT and event.pressed == true:
 			print(get_node("."))
-			print("clicked")
+			print("X: ", transform.origin.x, "Y: ", transform.origin.y, "Z: ", transform.origin.z)
+			material = selected
+			
+			
+
+
+func _on_Area_mouse_entered():
+	if material == selected:
+		material = selected
+	else:
+		material = white
+
+func _on_Area_mouse_exited():
+	if material == selected:
+		material = selected
+	else:
+		material = deselected
