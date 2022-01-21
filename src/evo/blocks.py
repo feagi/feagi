172d4bb4e135eaadada6a85010b2cba6817fe704
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2022-01-21 17:17:23 -0500

    Debugging the stimulator

--- src/evo/blocks.py
+++ src/evo/blocks.py
@@ -64,9 +64,10 @@ def neurons_in_the_block(cortical_area, block_ref):
     block_id to be entered as [x,y,z]
     """
     try:
+        print("block dict for ", cortical_area, block_ref, runtime_data.block_dic[cortical_area])
         return runtime_data.block_dic[cortical_area][block_ref]
-    except KeyError:
-        print("Error while processing --neurons_in_the_block-- function")
+    except Exception as e:
+        print("Error while processing --neurons_in_the_block-- function:\n", e)
         return []
 
 
