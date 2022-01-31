e4757657653a14881bcd193bd45e3610392f1e63
Kakcalu13 <65916520+Kakcalu13@users.noreply.github.com>
2022-01-31 13:25:38 -0500

    Able to load godot through dockerfile now (#48)

--- third_party/godot/container/FEAGI_initalize.sh
+++ third_party/godot/container/FEAGI_initalize.sh
@@ -4,6 +4,6 @@
 #mkdir -p ./.local/share/godot/templates/3.2.stable/linux_x11_64_release/
 #wget https://downloads.tuxfamily.org/godotengine/3.2.2/Godot_v3.2.stable_export_templates.tpz
 #mv Godot_v3.2-stable_export_templates ./.local/share/godot/templates/3.2.stable/linux_x11_64_release/
-xterm -hold -e "echo -e '\033]2;'FEAGI_activity'\007' && godot3" &
+xterm -hold -e "echo -e '\033]2;'FEAGI_activity'\007' && ./Godot_v3.4.2-stable_x11.64" &
 xterm -hold -e "echo -e '\033]2;'FEAGI_bridge'\007' && python3 bridge_godot_python.py" &
 #./FEAGI_activity_container
