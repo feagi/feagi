bcb77f46c3607caaaa6b8c60a4188b3e0fb5a1b6
Eric Miller <51423770+erm156@users.noreply.github.com>
2022-02-03 10:29:03 -0500

    use private repo in FEAGI Docker image (#50)
    * update feagi Dockerfile
    
    * testing private repo via docker-compose
    
    * build local docker images individually prior to docker-compose deployment
    
    * update build script
    
    * revert changes since building separately
    
    * remove branch checkout
    
    * simplify Dockerfile
    
    * add repo switch using build arg
    
    * rename/update build script
    
    * revert feagi build param changes
    
    * add feagi service config override
    
    * revert changes to FEAGI Dockerfile arg
    
    * update build script
    
    * remove hardcoded ssh url
    
    * bring back environment variable

---
+++ docker/run_private.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# == 0 ]; then
+    echo ">>> USAGE: $0 private_repo_ssh_url"
+    echo ">>> private_repo_ssh_url: SSH URL of private FEAGI repo"
+    exit 1
+else
+    export DOCKER_BUILDKIT=1
+    export PRIVATE_FEAGI_REPO=$1
+    docker build --ssh default --build-arg REPO=$1 .
+    docker build ../third_party/grafana
+    docker build ../third_party/godot/container
+    docker-compose -f feagi.yml -f feagi.private.yml up
+fi
