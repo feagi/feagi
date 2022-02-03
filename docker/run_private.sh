3f34d73b79acea8f56f67fccae3fa1527e4e6fa5
Eric Miller <51423770+erm156@users.noreply.github.com>
2022-02-03 16:44:56 -0500

    public/private FEAGI Dockerfiles (#59)
    * public/private Dockerfiles
    
    * update private repo build context

--- docker/run_private.sh
+++ docker/run_private.sh
@@ -7,7 +7,9 @@ if [ $# == 0 ]; then
 else
     export DOCKER_BUILDKIT=1
     export PRIVATE_FEAGI_REPO=$1
-    docker build --ssh default --build-arg REPO=$1 .
+    docker build --ssh default \
+                 --build-arg REPO=$1 \
+                 -f Dockerfile.private .
     docker build ../third_party/grafana
     docker build ../third_party/godot/container
     docker-compose -f feagi.yml -f feagi.private.yml up
