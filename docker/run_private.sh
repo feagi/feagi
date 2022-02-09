13b758fb1d3d82bed09a2003349c1d897264dd1f
Eric Miller <51423770+erm156@users.noreply.github.com>
2022-02-09 15:37:47 -0500

    update build script to fix weird caching and add private key to ssh-agent (#70)

--- docker/run_private.sh
+++ docker/run_private.sh
@@ -5,11 +5,12 @@ if [ $# == 0 ]; then
     echo ">>> private_repo_ssh_url: SSH URL of private FEAGI repo"
     exit 1
 else
-    export DOCKER_BUILDKIT=1
+    ssh-add -A
     export PRIVATE_FEAGI_REPO=$1
-    docker build --ssh default \
-                 --build-arg REPO=$1 \
-                 -f Dockerfile.private . --no-cache
+    docker image rm -f docker_feagi
+    DOCKER_BUILDKIT=1 docker build --ssh default \
+                                   --build-arg REPO=$1 \
+                                   -f Dockerfile.private .
     docker build ../third_party/grafana
     docker build ../third_party/godot/container
     docker-compose -f feagi.yml -f feagi.private.yml up
