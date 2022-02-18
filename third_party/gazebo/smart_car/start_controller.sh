7284de86737e304830dc7913bfac04eb3bef0c42
Kakcalu13 <65916520+Kakcalu13@users.noreply.github.com>
2022-02-18 17:18:50 -0500

    added --symlink-install to speed things (#81)

--- third_party/gazebo/smart_car/start_controller.sh
+++ third_party/gazebo/smart_car/start_controller.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xterm -hold -e "cd /opt/source-code/freenove_4wd_car_description/ && source /opt/ros/foxy/setup.bash && cd .. && sudo chmod 777 freenove_4wd_car_description/ && cd freenove_4wd_car_description/ && colcon build && source install/setup.bash && (ros2 launch freenove_4wd_car_description freenove_smart_car.launch.py &) && ros2 run freenove_4wd_car_description controller.py" &
+xterm -hold -e "cd /opt/source-code/freenove_4wd_car_description/ && source /opt/ros/foxy/setup.bash && cd .. && sudo chmod 777 freenove_4wd_car_description/ && cd freenove_4wd_car_description/ && colcon build --symlink-install && source install/setup.bash && (ros2 launch freenove_4wd_car_description freenove_smart_car.launch.py &) && ros2 run freenove_4wd_car_description controller.py" &
 
