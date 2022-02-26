2e1a23058376953beceb526ca95b3fbd4dbeabea
Mohammad Nadji-Tehrani <m.nadji.tehrani@gmail.com>
2022-02-26 18:44:41 -0500

    Projection Visualizer Base

--- src/evo/syn_viz.py
+++ src/evo/syn_viz.py
@@ -1,52 +1,106 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig = plt.figure()
-ax = fig.add_subplot(projection="polar", facecolor="lightgoldenrodyellow")
 
-r = np.linspace(0, 3, 301)
-theta = 2 * np.pi * r
-ax.plot(theta, r, color="tab:orange", lw=3, label="a line")
-ax.plot(0.5 * theta, r, color="tab:blue", ls="--", lw=3, label="another line")
-ax.tick_params(grid_color="palegoldenrod")
-# For polar axes, it may be useful to move the legend slightly away from the
-# axes center, to avoid overlap between the legend and the axes.  The following
-# snippet places the legend's lower left corner just outside of the polar axes
-# at an angle of 67.5 degrees in polar coordinates.
-angle = np.deg2rad(67.5)
-ax.legend(loc="lower left",
-          bbox_to_anchor=(.5 + np.cos(angle)/2, .5 + np.sin(angle)/2))
+# This import registers the 3D projection, but is otherwise unused.
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+from mpl_toolkits.mplot3d.proj3d import proj_transform
+from matplotlib.text import Annotation
+from matplotlib.patches import FancyArrowPatch
+mpl.use('macosx')
+
+class Annotation3D(Annotation):
+
+    def __init__(self, text, xyz, *args, **kwargs):
+        super().__init__(text, xy=(0, 0), *args, **kwargs)
+        self._xyz = xyz
+
+    def draw(self, renderer):
+        x2, y2, z2 = proj_transform(*self._xyz, self.axes.M)
+        self.xy = (x2, y2)
+        super().draw(renderer)
+
+def _annotate3D(ax, text, xyz, *args, **kwargs):
+    '''Add anotation `text` to an `Axes3d` instance.'''
+
+    annotation = Annotation3D(text, xyz, *args, **kwargs)
+    ax.add_artist(annotation)
+
+setattr(Axes3D, 'annotate3D', _annotate3D)
+
+
+class Arrow3D(FancyArrowPatch):
+
+    def __init__(self, x, y, z, dx, dy, dz, *args, **kwargs):
+        super().__init__((0, 0), (0, 0), *args, **kwargs)
+        self._xyz = (x, y, z)
+        self._dxdydz = (dx, dy, dz)
+
+    def draw(self, renderer):
+        x1, y1, z1 = self._xyz
+        dx, dy, dz = self._dxdydz
+        x2, y2, z2 = (x1 + dx, y1 + dy, z1 + dz)
+
+        xs, ys, zs = proj_transform((x1, x2), (y1, y2), (z1, z2), self.axes.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        super().draw(renderer)
+
+
+def _arrow3D(ax, x, y, z, dx, dy, dz, *args, **kwargs):
+    '''Add an 3d arrow to an `Axes3D` instance.'''
+
+    arrow = Arrow3D(x, y, z, dx, dy, dz, *args, **kwargs)
+    ax.add_artist(arrow)
+
+
+setattr(Axes3D, 'arrow3D', _arrow3D)
+
+
+
+# prepare some coordinates
+x, y, z = np.indices((8, 8, 8))
+
+# draw cuboids in the top left and bottom right corners, and a link between them
+cube1 = (x < 3) & (y < 3) & (z < 3)
+cube2 = (x >= 5) & (y >= 5) & (z >= 5)
+# link = abs(x - y) + abs(y - z) + abs(z - x) <= 2
+
+# combine the objects into a single boolean array
+voxels = cube1 | cube2
+
+# set the colors of each object
+colors = np.empty(voxels.shape, dtype=object)
+# colors[link] = 'red'
+colors[cube1] = 'blue'
+colors[cube2] = 'green'
+#
+
+fig = plt.figure()
+
+# ax = fig.gca(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
+ax.voxels(voxels, facecolors=colors, edgecolor='k')
+
+
+
+# ax.set_xlim(0,2)
+# ax.arrow3D(0,0,0,
+#            7,7,7,
+#            mutation_scale=20,
+#            arrowstyle="-|>",
+#            linestyle='dashed')
+ax.arrow3D(1,1,0,
+           2,6,8,
+           mutation_scale=20,
+           ec ='green',
+           fc='red')
+# ax.set_title('3D Arrows Demo')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+
+# fig.tight_layout()
+
 
 plt.show()
-
-
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-# Compute areas and colors
-N = 150
-r = 2 * np.random.rand(N)
-theta = 2 * np.pi * np.random.rand(N)
-area = 200 * r**2
-colors = theta
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='polar')
-c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
-
-
-def godot_data(input):
-    """
-    Simply clean the list and remove all unnecessary special characters and deliver with name, xyz only
-    """
-    print("OLD dict: ", input)
-    data = ast.literal_eval(input)
-    dict_holder_with_old_name = dict(data)
-
-    print(dict_holder_with_old_name)
-
-    for key in dict_holder_with_old_name["data"]["direct_stimulation"]:
-
-        data["data"]["direct_stimulation"].pop(key, name_to_id(key))
-    print("NEW dict: " , data)
-    return data
