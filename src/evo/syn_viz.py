4af71ef54d150c9502c1dca950cf13b0e8d2777b
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2022-02-28 10:11:24 -0500

    Tweaks

--- src/evo/syn_viz.py
+++ src/evo/syn_viz.py
@@ -57,45 +57,44 @@ def _arrow3D(ax, x, y, z, dx, dy, dz, *args, **kwargs):
 setattr(Axes3D, 'arrow3D', _arrow3D)
 
 # Extract the actual synapse data from connectome
-__src_cortical_area = "i__inf"
-__dst_cortical_area = "i__inf"
+__src_cortical_area = "i__pro"
+__dst_cortical_area = "t__pro"
 
 cortical_areas = [__src_cortical_area, __dst_cortical_area]
 
-connectome_path = '/var/folders/gy/4ydq5w4n76jg9zlpjs6mfrlm0000gn/T/2022-02-26_22-01-31_298037_K6UIT1_R/connectome/'
+connectome_path = '/var/folders/16/7xn3q7792q30rf7yqfvm24sh0000gn/T/2022-02-28_09-29-12_614138_DLW9JN_R/connectome/'
 genome_path = connectome_path[:-11] + 'genome_tmp.json'
 
 with open(genome_path, "r") as genome_file:
     genome = json.load(genome_file)
 
+print("Genome:", genome)
 connectome = load_brain_in_memory(connectome_path, [__src_cortical_area, __dst_cortical_area])
 
 cortical_data = {}
-
-
-padding = 5
-src_dim = list()
-dst_dim = list()
-
+cortical_dims = list()
 
 # Extract Cortical dimensions from Genome
-for key in genome['blueprint']:
-    if key[9:15] == __src_cortical_area:
-        if key[19:24] == '___bb':
-            if key[25] =='x':
-                src
+for cortical_area in cortical_areas:
+    cortical_dims.append(genome["blueprint"][cortical_area]["neuron_params"]["block_boundaries"])
+
+print("original cortical dimensions=", cortical_dims)
+
+# Swapping y and z to match Godot axis
+tmp = cortical_dims[0][2]
+cortical_dims[0][2] = cortical_dims[0][1]
+cortical_dims[0][1] = tmp
+
+tmp = cortical_dims[1][2]
+cortical_dims[1][2] = cortical_dims[1][1]
+cortical_dims[1][1] = tmp
 
 
+max_x = max(cortical_dims[0][0], cortical_dims[1][0])
+max_y = max(cortical_dims[0][1], cortical_dims[1][1])
+max_z = max(cortical_dims[0][2], cortical_dims[1][2])
 
-
-
-src_dim = genome[][__src_cortical_area][3, 1, 1]
-dst_dim = [3, 1, 1]
-
-
-max_x = max(src_dim[0], dst_dim[0])
-max_y = max(src_dim[1], dst_dim[1])
-max_z = max(src_dim[2], dst_dim[2])
+padding = max_x * 2
 
 max_max = max(2 * max_x + padding, max_y, max_z)
 
@@ -107,18 +106,17 @@ vectors = []
 # prepare some coordinates
 x, y, z = np.indices((max_max, max_max, max_max))
 
-# draw cuboids in the top left and bottom right corners, and a link between them
-cube1 = (x < src_dim[0]) & (y < src_dim[2]) & (z < src_dim[1])
-cube2 = (x >= padding + dst_dim[0]) & (y < dst_dim[2]) & (z < dst_dim[1])
+cortical_area_1 = (x < cortical_dims[0][0]) & (y < cortical_dims[0][1]) & (z < cortical_dims[0][2])
+cortical_area_2 = (padding + cortical_dims[0][0] <= x) & (x < padding + cortical_dims[0][0] + cortical_dims[1][0]) & (y < cortical_dims[1][1]) & (z < cortical_dims[1][2])
 
 # combine the objects into a single boolean array
-voxels = cube1 | cube2
+voxels = cortical_area_1 | cortical_area_2
 
 # set the colors of each object
 colors = np.empty(voxels.shape, dtype=object)
 
-colors[cube1] = 'blue'
-colors[cube2] = 'green'
+colors[cortical_area_1] = 'blue'
+colors[cortical_area_2] = 'green'
 
 fig = plt.figure()
 
@@ -143,7 +141,7 @@ for neuron in connectome[cortical_area]:
                 dst_neuron_location = connectome[__dst_cortical_area][dst_neuron]["soma_location"]
                 vectors.append(src_neuron_location + dst_neuron_location)
 
-print(vectors)
+print("vectors", vectors)
 
 for vector in vectors:
     sx, sy, sz, dx, dy, dz = vector
