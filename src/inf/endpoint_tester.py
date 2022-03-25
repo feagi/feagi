a0d9a817c602497682bc99a09d68cc1f487b6742
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2022-03-25 18:17:27 -0400

    FEAGI API

--- src/inf/endpoint_tester.py
+++ src/inf/endpoint_tester.py
@@ -1,7 +1,10 @@
 import requests
+import contextlib
 
 url = "http://127.0.0.1:7000/v1/feagi"
 request_obj = {'begin': True}
 
 x = requests.post(url, data=request_obj)
 print(x.text)
+
+test = Config()
