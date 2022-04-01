a13a3b0eeaa7f24297e5e5f0a665d63a9f52fb88
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2022-04-01 15:31:17 -0400

    Burst duration

--- src/api/static/main.js
+++ src/api/static/main.js
@@ -24,3 +24,16 @@ function start_feagi() {
     let data = JSON.stringify({"existing_connectome": document.getElementById('existing_connectome').value});
     xhttp.send(data)
 }
+
+function set_burst_duration() {
+    let url = "http://127.0.0.1:8000/v1/feagi/feagi/burst_engine";
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open("POST", url, true);
+
+    // Set the request header i.e. which type of content you are sending
+    xhttp.setRequestHeader("Content-Type", "application/json");
+
+    let data = JSON.stringify({"burst_duration": document.getElementById('burst_duration').value});
+    xhttp.send(data)
+}
