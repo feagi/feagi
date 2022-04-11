2c8e283f7754f1c53c566c04a3925ff2e0232938
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-04-11 18:39:02 -0400

    Updated to clear red voxels

--- third_party/godot/container/clear_voxel_timer.gd
+++ third_party/godot/container/clear_voxel_timer.gd
@@ -1,17 +1,16 @@
-extends GridMap
+extends Spatial
 
 
 var timer = false
-
+var timer_string = ""
 
 # This will clear red voxels automatically without halt the process of godot
 func time_to_clear():
 	yield(get_tree().create_timer(1), "timeout")
-	clear()
-	timer = false
+	timer_string = "clear_now"
 
 func _process(delta):##We will need to createa something to make this more scalable later.
 	if timer:
 		time_to_clear()
 	else:
-		pass
+		timer_string =""
