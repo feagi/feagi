aa7359b7048bcf10966867b2ed9648e51fdaae7d
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-04-14 13:22:12 -0400

    revert ip

--- third_party/godot/src/configuration.py
+++ third_party/godot/src/configuration.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 network_settings = {
-    "feagi_ip": "127.0.0.1",
+    "feagi_ip": "feagi",
     "feagi_outbound_port": "30000",
     "feagi_inbound_port_godot": "30001",
     'TTL': 2,
