9fe5ff8cd159c4986de9fb76c68e94a1ce1a18d6
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-04-14 11:59:36 -0400

    Updated the script

--- third_party/godot/feagi_initalize.sh
+++ third_party/godot/feagi_initalize.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-FILE=./csv_data.csv
-xterm -hold -e "echo -e '\033]2;'FEAGI_bridge'\007' && python3 bridge_godot_python.py" &
-python3 -m http.server
-#FEAGI_monitor.html
+
+python3 bridge_godot_python.py &
+http-server -p 8002
