511b1ba62e89a86b5bebbbb38a204386ba277371
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-04-19 09:34:43 -0400

    completed to maximize the gazebo screen inside the vnc

--- third_party/gazebo/smart_car/start_controller.sh
+++ third_party/gazebo/smart_car/start_controller.sh
@@ -2,6 +2,5 @@
 cd /opt/source-code/freenove_4wd_car_description/ && source /opt/ros/foxy/setup.bash && cd .. && sudo chmod 777 freenove_4wd_car_description/ && cd freenove_4wd_car_description/ && colcon build --symlink-install &
 wait -n
 xterm -hold -e "cd /opt/source-code/freenove_4wd_car_description/ && source install/setup.bash && ros2 run freenove_4wd_car_description controller.py" & 
-xterm -e "sleep 5 && wmctrl -r 'Gazebo' -b toggle,fullscreen" &
-cd /opt/source-code/freenove_4wd_car_description/ && source install/setup.bash && ros2 launch freenove_4wd_car_description freenove_smart_car.launch.py 
-
+sleep 7 && wmctrl -r Gazebo -b toggle,fullscreen &
+cd /opt/source-code/freenove_4wd_car_description/ && source install/setup.bash && ros2 launch freenove_4wd_car_description freenove_smart_car.launch.py
