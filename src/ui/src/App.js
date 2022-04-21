f31132e8f0ae5bc78eaaed5fbda13d038cdaefea
Eric Miller <ermiller@alumni.cmu.edu>
2022-04-21 18:19:35 -0400

    update routes

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import ChooseEnv from "./routes/ChooseEnv";
+import ChooseEnvironment from "./routes/ChooseEnvironment";
 import CreateGenome from "./routes/CreateGenome";
+import ChooseSensory from "./routes/ChooseSensory";
 import CorticalMapping from "./routes/CorticalMapping";
-
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
 import ProgressStepper from "./components/ProgressStepper";
 
@@ -10,12 +10,13 @@ function App() {
   return (
     <>
       <ResponsiveAppBar />
-      <ProgressStepper />
       <Routes>
-        <Route path="/environment" element={<ChooseEnv />} />
+        <Route path="/environment" element={<ChooseEnvironment />} />
         <Route path="/genome" element={<CreateGenome />} />
         <Route path="/brain/mapping" element={<CorticalMapping />} />
+        <Route path="/genome/sensory" element={<ChooseSensory />} />
       </Routes>
+      <ProgressStepper />
     </>
   );
 }
