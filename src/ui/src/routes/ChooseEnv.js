0c0aa36e8987cd20bc17c9f9767c5e32f227975a
Eric Miller <ermiller@alumni.cmu.edu>
2022-04-21 14:09:51 -0400

    refactor components to use stack for layout

--- src/ui/src/routes/ChooseEnv.js
+++ src/ui/src/routes/ChooseEnv.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Item from "../components/Item";
 import MenuCard from "../components/MenuCard";
 import { GiVintageRobot } from "react-icons/gi";
@@ -9,38 +8,31 @@ import { Link } from "react-router-dom";
 
 const ChooseEnv = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid
-        container
-        spacing={0}
-        direction="row"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Grid item xs={4}>
-          <Item>
-            <Link to={"/genome"}>
-              <MenuCard
-                image={<GiVintageRobot size="xl" />}
-                label="Physical Robot"
-                info="A physical device with basic I/O and processing power capable of running or interacting with FEAGI"
-              />
-            </Link>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-            <Link to={"/genome"}>
-              <MenuCard
-                image={<AiOutlineRobot size="xl" />}
-                label="Virtual Robot"
-                info="A virtual robot existing in a virtual environment (ex: Gazebo) with a defined physics engine capable of interfacing with FEAGI"
-              />
-            </Link>
-          </Item>
-        </Grid>
-      </Grid>
-    </Box>
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      spacing={2}
+    >
+      <Item>
+        <Link to={"/genome"}>
+          <MenuCard
+            image={<GiVintageRobot size="xl" />}
+            label="Physical Robot"
+            info="A physical device with basic I/O and processing power capable of running or interacting with FEAGI"
+          />
+        </Link>
+      </Item>
+      <Item>
+        <Link to={"/genome"}>
+          <MenuCard
+            image={<AiOutlineRobot size="xl" />}
+            label="Virtual Robot"
+            info="A virtual robot existing in a virtual environment (ex: Gazebo) with a defined physics engine capable of interfacing with FEAGI"
+          />
+        </Link>
+      </Item>
+    </Stack>
   );
 };
 
