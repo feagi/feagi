383f4c7641e5a8797b656db97b19a4cd470215f4
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-04-22 11:17:26 -0400

    new configuration.py

--- third_party/elecfreaks/configuration.py
+++ third_party/elecfreaks/configuration.py
@@ -16,20 +16,19 @@ limitations under the License.
 """
 #!/usr/bin/env python3
 
+app_name = 'gazebo'
+
 network_settings = {
-    "feagi_ip": "feagi",
+    "feagi_host": "feagi",
+    "feagi_api_port": "8000",
     "feagi_outbound_port": "30000",
     "feagi_inbound_port_gazebo": "30002",
     'TTL': 2,
     'last_message': 0,
-    'feagi_burst_speed':  1,
-    "host_name": "",
-    "ip_address": "",
-    "microbit_mac_address": "xx:xx:xx:xx:xx:xx",
-    "primary_mac_address": "yy:yy:yy:yy:yy:yy"
-}
+    "microbit_mac_address": "F2:1B:92:0A:89:28",
+    "primary_mac_address": "14:BD:61:F0:EE:D1"
+    }
 
-capabilities = {
-}
+capabilities = {}
 
 message_to_feagi = {"data": {}}
