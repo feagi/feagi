64aec4c29950404728d6dd8cecd18e7a7831b9eb
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2022-04-25 17:51:25 -0400

    Sync Routers

--- third_party/gazebo/smart_car/freenove_4wd_car_description/src/router.py
+++ third_party/gazebo/smart_car/freenove_4wd_car_description/src/router.py
@@ -83,6 +83,9 @@ def register_with_feagi(app_name, feagi_host, api_port, app_capabilities, app_ho
     registration_complete = False
 
     while not registration_complete:
+
+        print("Registration data:", registration_data)
+
         feagi_registration_result = requests.post(api_address + registration_endpoint, data=registration_data)
 
         print("FEAGI registration results: ", feagi_registration_result)
@@ -103,7 +106,7 @@ def register_with_feagi(app_name, feagi_host, api_port, app_capabilities, app_ho
         if feagi_settings and feagi_settings['burst_duration'] and feagi_settings['burst_counter']:
             print("\n\n\n\nRegistration is complete....")
             registration_complete = True
-            sleep(1)
+        sleep(1)
 
     # Transmit Controller Capabilities
     pub_address = "tcp://0.0.0.0:" + feagi_settings[app_port_id]
