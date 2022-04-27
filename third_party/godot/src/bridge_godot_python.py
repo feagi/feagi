3248e508b2de7dae05558f8bbd4c8083bb51a171
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-04-27 18:23:52 -0400

    updated to work with csv

--- third_party/godot/src/bridge_godot_python.py
+++ third_party/godot/src/bridge_godot_python.py
@@ -47,7 +47,7 @@ def csv_writer(cortical_dimensions):
     print("Generating CSV...")
     cwd = os.getcwd()
     print("Current path is:", cwd)
-    f = open('../html/csv_data.csv', 'w', newline='')
+    f = open('../godot_source/csv_data.gdc', 'w', newline='')
     writer = csv.writer(f)
     godot_cortical_dimensions = list()
     for cortical_area in cortical_dimensions:
