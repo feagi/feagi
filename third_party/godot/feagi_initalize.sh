899ba7314cfb17f189128a326a75362252f3dd38
kakcalu13 <kevin.a.araujo@gmail.com>
2022-04-28 12:58:58 -0400

    changed the format

--- third_party/godot/feagi_initalize.sh
+++ third_party/godot/feagi_initalize.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-FILE=./csv_data.gdc
+FILE=./csv_data.gd
 rm /root/html/*
-rm /root/godot_source/csv_data.gdc
+rm /root/godot_source/csv_data.gd
 python3 bridge_godot_python.py &
 
 
 cd ../godot_source/
 
-while [ ! -s csv_data.gdc ]; do
+while [ ! -s csv_data.gd ]; do
   echo "Waiting on csv_data being generated..."
   sleep 2
 done
