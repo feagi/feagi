5769d26e79f87315f1801bf05c9076a0187003f4
Eric Miller <ermiller@alumni.cmu.edu>
2022-04-28 15:19:32 -0400

    functional checkpoint for gazebo/godot monitoring

--- src/ui/src/routes/MonitoringDashboard.js
+++ src/ui/src/routes/MonitoringDashboard.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Stack from "@mui/material/Stack";
+import Iframe from "react-iframe";
 
 const MonitoringDashboard = () => {
-  return <div>MonitoringDashboard</div>;
+  const [frameHeight, setFrameHeight] = useState("");
+
+  useEffect(() => {
+    const clientHeight = document.documentElement.scrollHeight;
+    setTimeout(() => {
+      setFrameHeight(clientHeight + "px");
+    }, 100);
+  }, []);
+
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      spacing={6}
+      sx={{ m: 4 }}
+    >
+      <Iframe
+        id="godotFrame"
+        url="http://localhost:6080"
+        width="100%"
+        height={frameHeight}
+      />
+      <Iframe
+        id="gazeboFrame"
+        url="http://localhost:6080"
+        width="100%"
+        height={frameHeight}
+      />
+    </Stack>
+  );
 };
 
 export default MonitoringDashboard;
