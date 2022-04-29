2c56b4feb3486ffbee17eaf043cefc23aa096e1a
Eric Miller <ermiller@alumni.cmu.edu>
2022-04-29 18:24:39 -0400

    feagi API module

--- src/ui/src/services/FeagiAPI.js
+++ src/ui/src/services/FeagiAPI.js
@@ -1,1 +1,40 @@
-const FeagiAPI = {};
+import axios from "axios";
+
+const FEAGI_URL = "http://localhost:8000/v1";
+
+const FeagiAPI = {
+  async getBaselineSensory() {
+    const response = await axios.get(
+      `${FEAGI_URL}/feagi/feagi/gui_baseline/ipu`,
+      {
+        headers: {},
+        params: {},
+      }
+    );
+    return response.data;
+  },
+
+  async getBaselineMotor() {
+    const response = await axios.get(
+      `${FEAGI_URL}/feagi/feagi/gui_baseline/opu`,
+      {
+        headers: {},
+        params: {},
+      }
+    );
+    return response.data;
+  },
+
+  async getBaselineMorphology() {
+    const response = await axios.get(
+      `${FEAGI_URL}/feagi/feagi/gui_baseline/morphology`,
+      {
+        headers: {},
+        params: {},
+      }
+    );
+    return response.data;
+  },
+};
+
+export default FeagiAPI;
