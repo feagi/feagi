53ebae64dee94a3207cf049b48950937d989619d
Eric Miller <ermiller@alumni.cmu.edu>
2022-05-02 11:30:05 -0400

    add error handling to existing api methods

--- src/ui/src/services/FeagiAPI.js
+++ src/ui/src/services/FeagiAPI.js
@@ -4,35 +4,41 @@ const FEAGI_URL = "http://localhost:8000/v1";
 
 const FeagiAPI = {
   async getBaselineSensory() {
-    const response = await axios.get(
-      `${FEAGI_URL}/feagi/feagi/gui_baseline/ipu`,
-      {
+    const response = await axios
+      .get(`${FEAGI_URL}/feagi/feagi/gui_baseline/ipu`, {
         headers: {},
         params: {},
-      }
-    );
+      })
+      .catch((error) => {
+        console.error(error);
+        throw error;
+      });
     return response.data;
   },
 
   async getBaselineMotor() {
-    const response = await axios.get(
-      `${FEAGI_URL}/feagi/feagi/gui_baseline/opu`,
-      {
+    const response = await axios
+      .get(`${FEAGI_URL}/feagi/feagi/gui_baseline/opu`, {
         headers: {},
         params: {},
-      }
-    );
+      })
+      .catch((error) => {
+        console.error(error);
+        throw error;
+      });
     return response.data;
   },
 
   async getBaselineMorphology() {
-    const response = await axios.get(
-      `${FEAGI_URL}/feagi/feagi/gui_baseline/morphology`,
-      {
+    const response = await axios
+      .get(`${FEAGI_URL}/feagi/feagi/gui_baseline/morphology`, {
         headers: {},
         params: {},
-      }
-    );
+      })
+      .catch((error) => {
+        console.error(error);
+        throw error;
+      });
     return response.data;
   },
 };
