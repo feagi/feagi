e8cff26a99ebee29188d6aa9b537ef058a118d9d
Eric Miller <ermiller@alumni.cmu.edu>
2022-05-05 19:08:10 -0400

    check in progress refactoring mapping form

--- src/gui/src/components/CorticalAreaMapForm.js
+++ src/gui/src/components/CorticalAreaMapForm.js
@@ -1,33 +1,32 @@
 import React, { useState, useEffect } from "react";
 import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Divider from "@mui/material/Divider";
 import Fab from "@mui/material/Fab";
 import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
 import InputLabel from "@mui/material/InputLabel";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 import MenuItem from "@mui/material/MenuItem";
+import SaveIcon from "@mui/icons-material/Save";
 import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
+import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 import FeagiAPI from "../services/FeagiAPI";
 
 const CorticalAreaMapForm = (props) => {
-  const [destinationCount, setDestinationCount] = useState(1);
   const [sensoryAreas, setSensoryAreas] = useState([]);
   const [motorAreas, setMotorAreas] = useState([]);
   const [predefinedSynapseRules, setPredefinedSynapseRules] = useState({});
-
-  const [selectedCorticalArea, setSelectedCorticalArea] = useState("");
+  const [selectedArea, setSelectedArea] = useState("");
   const [selectedRule, setSelectedRule] = useState("");
-
-  /* 
-
-    - state might need to be an object with keys pertaining to the mapping #
-      for the respective cortical area
-
-    - use idx value from main .map to access unique keys in state object and
-      update their values
-  
-  */
+  const [mappedAreas, setMappedAreas] = useState([]);
+  const [definedMappings, setDefinedMappings] = useState([]);
 
   useEffect(() => {
     FeagiAPI.getBaselineMotor().then((items) => setMotorAreas(items));
@@ -38,7 +37,7 @@ const CorticalAreaMapForm = (props) => {
   }, []);
 
   const handleAreaChange = (event) => {
-    setSelectedCorticalArea(event.target.value);
+    setSelectedArea(event.target.value);
   };
 
   const handleRuleChange = (event) => {
@@ -46,90 +45,158 @@ const CorticalAreaMapForm = (props) => {
   };
 
   const handleAdd = () => {
-    setDestinationCount(destinationCount + 1);
+    setMappedAreas([
+      ...mappedAreas,
+      { dstArea: selectedArea, rule: selectedRule },
+    ]);
+    setSelectedArea("");
+    setSelectedRule("");
   };
 
-  const handleRemove = () => {
-    if (destinationCount > 0) {
-      setDestinationCount(destinationCount - 1);
-    } else {
-      setDestinationCount(0);
-    }
+  const handleMappingDelete = (index) => {
+    let updatedMappings = mappedAreas;
+    setMappedAreas(Array.from(updatedMappings.splice(index, 1)));
+  };
+
+  const handleSave = (event) => {
+    console.log(event);
   };
 
   return (
     <>
-      <Typography gutterBottom variant="h5" component="div" sx={{ mb: 3 }}>
-        Cortical Mapping for {props.corticalArea}
+      <Typography gutterBottom variant="h5" component="div" sx={{ mb: 4 }}>
+        {props.corticalArea} Area Cortical Mapping
       </Typography>
-      {Array.from(Array(destinationCount)).map((item, idx) => {
-        return (
-          <div key={`${item}-${idx}`}>
-            <Typography variant="h6" component="div" sx={{ mt: 2 }}>
-              Destination {idx + 1}
-            </Typography>
-            <FormControl sx={{ m: 2, minWidth: 120 }}>
-              <InputLabel id={`dest-select-label-${idx}`}>
-                Cortical Area
-              </InputLabel>
-              <Select
-                labelId={`dest-select-label-${idx}`}
-                id={`dest-select-${idx}`}
-                value={selectedCorticalArea}
-                label="Cortical Area"
-                onChange={handleAreaChange}
-                sx={{ width: "250px" }}
-              >
-                {sensoryAreas.concat(motorAreas).map((area) => {
-                  return (
-                    <MenuItem key={area} value={area}>
-                      {area}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </FormControl>
-            <FormControl sx={{ m: 2, minWidth: 120 }}>
-              <InputLabel id={`rule-select-label-${idx}`}>
-                Pre-defined Rule
-              </InputLabel>
-              <Select
-                labelId={`rule-select-label-${idx}`}
-                id={`rule-select-${idx}`}
-                value={selectedRule}
-                label="Pre-defined Rule"
-                onChange={handleRuleChange}
-                sx={{ width: "250px" }}
-              >
-                {Object.keys(predefinedSynapseRules).map((rule) => {
-                  return (
-                    <MenuItem key={rule} value={rule}>
-                      {rule}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </FormControl>
-            <FormControl sx={{ m: 2, minWidth: 200 }}>
-              <TextField
-                id="rule-def-field"
-                label="Rule Definition"
-                variant="outlined"
-                sx={{ width: "250px" }}
-              >
-                {console.log(typeof predefinedSynapseRules[selectedRule])}
-              </TextField>
-            </FormControl>
-          </div>
-        );
-      })}
       <div>
-        <Fab size="small" color="primary" aria-label="add" sx={{ m: 1 }}>
-          <AddIcon onClick={handleAdd} />
-        </Fab>
-        <Fab size="small" color="primary" aria-label="add" sx={{ m: 1 }}>
-          <RemoveIcon onClick={handleRemove} />
-        </Fab>
+        <Typography variant="h6" component="div" sx={{ mt: 2, mb: 1 }}>
+          Select mapping destination and type
+        </Typography>
+        <Divider />
+        <FormControl sx={{ m: 2, minWidth: 120 }}>
+          <InputLabel id="dest-select-label">Cortical Area</InputLabel>
+          <Select
+            labelId="dest-select-label"
+            id="dest-select"
+            value={selectedArea}
+            label="Cortical Area"
+            onChange={handleAreaChange}
+            sx={{ width: "250px" }}
+          >
+            {sensoryAreas.concat(motorAreas).map((area) => {
+              return (
+                <MenuItem key={area} value={area}>
+                  {area}
+                </MenuItem>
+              );
+            })}
+          </Select>
+          <FormHelperText>Required</FormHelperText>
+        </FormControl>
+        <FormControl sx={{ m: 2, minWidth: 120 }}>
+          <InputLabel id="rule-select-label">Pre-defined Rule</InputLabel>
+          <Select
+            labelId="rule-select-label"
+            id="rule-select"
+            value={selectedRule}
+            label="Pre-defined Rule"
+            onChange={handleRuleChange}
+            sx={{ width: "250px" }}
+          >
+            {Object.keys(predefinedSynapseRules).map((rule) => {
+              return (
+                <MenuItem key={rule} value={rule}>
+                  {rule}
+                </MenuItem>
+              );
+            })}
+          </Select>
+          <FormHelperText>Required</FormHelperText>
+        </FormControl>
+        <FormControl sx={{ m: 2, minWidth: 200 }}>
+          <TextField
+            disabled
+            id="rule-def-field"
+            label={
+              selectedRule
+                ? JSON.stringify(predefinedSynapseRules[selectedRule])
+                : "Rule info"
+            }
+            variant="outlined"
+            sx={{ width: "250px" }}
+          />
+        </FormControl>
+      </div>
+      <div>
+        <Tooltip
+          title={
+            !(selectedArea && selectedRule)
+              ? "Select area/rule first..."
+              : "Add mapping..."
+          }
+        >
+          <span>
+            <Fab
+              size="small"
+              color="primary"
+              aria-label="add"
+              sx={{ m: 1 }}
+              disabled={!(selectedArea && selectedRule)}
+            >
+              <AddIcon onClick={handleAdd} />
+            </Fab>
+          </span>
+        </Tooltip>
+        <Tooltip
+          title={
+            mappedAreas.length > 0 ? "Save mappings" : "Add mapping first..."
+          }
+        >
+          <span>
+            <Fab
+              size="small"
+              color="primary"
+              aria-label="add"
+              sx={{ m: 1 }}
+              disabled={!mappedAreas.length > 0}
+            >
+              <SaveIcon onClick={handleSave} />
+            </Fab>
+          </span>
+        </Tooltip>
+      </div>
+      <div>
+        <Typography gutterBottom variant="h6" component="div" sx={{ mt: 4 }}>
+          Defined Mappings for {props.corticalArea}
+        </Typography>
+        <Divider />
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={8}>
+            <List>
+              {mappedAreas.map((value, index) => {
+                console.log(mappedAreas);
+                return (
+                  <ListItem
+                    divider
+                    key={index}
+                    secondaryAction={
+                      <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={() => handleMappingDelete(index)}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    }
+                  >
+                    <ListItemText primary={index + 1} />
+                    <ListItemText primary={value.dstArea} />
+                    <ListItemText primary={value.rule} />
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Grid>
+        </Grid>
       </div>
     </>
   );
