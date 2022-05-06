0ecc5b5c1b42d4c123748b1d91b79e95b07894fd
Eric Miller <ermiller@alumni.cmu.edu>
2022-05-06 15:18:11 -0400

    conditionally render based on upload status

--- src/gui/src/components/GenomeUploadCard.js
+++ src/gui/src/components/GenomeUploadCard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -11,10 +12,17 @@ const Input = styled("input")({
 });
 
 const GenomeUploadCard = (props) => {
+  const [fileUploaded, setFileUploaded] = useState(false);
+
+  const sendToMonitoringDashboard = () => {
+    return <Navigate to="/monitoring" />;
+  };
+
   const handleFileUpload = (event) => {
     try {
       let fileData = { file: event.target.files[0] };
       FeagiAPI.postGenomeFile(fileData);
+      setFileUploaded(true);
     } catch (error) {
       console.error(error);
       throw error;
@@ -22,28 +30,34 @@ const GenomeUploadCard = (props) => {
   };
 
   return (
-    <Card sx={{ width: 345, height: 330 }}>
-      {props.image}
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.label}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {props.info}
-        </Typography>
-        <label htmlFor="contained-button-file">
-          <Input
-            accept="*.py"
-            id="contained-button-file"
-            type="file"
-            onChange={handleFileUpload}
-          />
-          <Button variant="contained" component="span">
-            Upload
-          </Button>
-        </label>
-      </CardContent>
-    </Card>
+    <>
+      {fileUploaded ? (
+        sendToMonitoringDashboard()
+      ) : (
+        <Card sx={{ width: 345, height: 330 }}>
+          {props.image}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {props.label}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {props.info}
+            </Typography>
+            <label htmlFor="contained-button-file">
+              <Input
+                accept="*.py"
+                id="contained-button-file"
+                type="file"
+                onChange={handleFileUpload}
+              />
+              <Button variant="contained" component="span">
+                Upload
+              </Button>
+            </label>
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 };
 
