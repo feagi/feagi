7a5b75b8f88b38c386bc3f588ceaec7fa182e36e
Eric Miller <ermiller@alumni.cmu.edu>
2022-05-10 12:06:07 -0400

    add option for using default genome

--- src/gui/src/routes/Genome.js
+++ src/gui/src/routes/Genome.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Stack from "@mui/material/Stack";
+import { BiDna } from "react-icons/bi";
 import { GiDna2 } from "react-icons/gi";
 import { MdUploadFile } from "react-icons/md";
 import { Link } from "react-router-dom";
 import Item from "../components/Item";
 import MenuCard from "../components/MenuCard";
 import GenomeUploadCard from "../components/GenomeUploadCard";
+import FeagiAPI from "../services/FeagiAPI";
 
 const CreateGenome = () => {
+  const handleDefaultClick = () => {
+    FeagiAPI.postDefaultGenome();
+  };
+
   return (
     <>
       <Stack
@@ -18,12 +24,12 @@ const CreateGenome = () => {
         sx={{ m: 6 }}
       >
         <Item>
-          <Link to="/brain">
+          <Link to="/monitoring">
             <label htmlFor="genome-card">
               <MenuCard
-                image={<GiDna2 size={150} />}
-                label="Create Genome"
-                // info="Create a seed genome by defining desired sensorimotor capabilities, corresponding cortical areas and their connections."
+                image={<BiDna size={150} />}
+                label="Default Genome"
+                onClick={handleDefaultClick}
               />
             </label>
           </Link>
@@ -35,6 +41,17 @@ const CreateGenome = () => {
               label="Upload Genome"
             />
           </label>
+        </Item>
+        <Item>
+          <Link to="/brain/sensory">
+            <label htmlFor="genome-card">
+              <MenuCard
+                image={<GiDna2 size={150} />}
+                label="Create Genome"
+                // info="Create a seed genome by defining desired sensorimotor capabilities, corresponding cortical areas and their connections."
+              />
+            </label>
+          </Link>
         </Item>
       </Stack>
     </>
