54a16f13729c15392c821a3cbd8090215f3e46c0
Kakcalu13 <65916520+Kakcalu13@users.noreply.github.com>
2022-05-11 15:07:36 -0400

    added program to deny double load (#169)

--- third_party/gazebo/smart_car/start_controller.sh
+++ third_party/gazebo/smart_car/start_controller.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-cd /opt/source-code/freenove_4wd_car_description/ && source /opt/ros/foxy/setup.bash && cd .. && sudo chmod 777 freenove_4wd_car_description/ && cd freenove_4wd_car_description/ && colcon build --symlink-install &
-wait -n
-xterm -hold -e "cd /opt/source-code/freenove_4wd_car_description/ && source install/setup.bash && ros2 run freenove_4wd_car_description controller.py" & 
-sleep 7 && wmctrl -r Gazebo -b toggle,fullscreen &
-cd /opt/source-code/freenove_4wd_car_description/ && source install/setup.bash && ros2 launch freenove_4wd_car_description freenove_smart_car.launch.py
+GAZEBO="ruby"
+ROS2="ros2"
+
+if pgrep -x "$ROS2" >/dev/null
+then
+    echo "$ROS2 is already running"
+else
+    cd /opt/source-code/freenove_4wd_car_description/ && source /opt/ros/foxy/setup.bash && cd .. && sudo chmod 777 freenove_4wd_car_description/ && cd freenove_4wd_car_description/ && colcon build --symlink-install &
+    wait -n
+    xterm -hold -e "cd /opt/source-code/freenove_4wd_car_description/ && source install/setup.bash && ros2 run freenove_4wd_car_description controller.py" &
+    sleep 7 && wmctrl -r Gazebo -b toggle,fullscreen &
+fi
+
+if pgrep -x "$GAZEBO" >/dev/null
+then
+    echo "$GAZEBO is already running"
+else
+    cd /opt/source-code/freenove_4wd_car_description/ && source install/setup.bash && ros2 launch freenove_4wd_car_description freenove_smart_car.launch.py
+fi 
