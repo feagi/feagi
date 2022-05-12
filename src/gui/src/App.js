dc6db63dd416242d1e3dde3d50664ca18426ee41
Eric Miller <ermiller@alumni.cmu.edu>
2022-05-12 18:29:52 -0400

    pass props to child components

--- src/gui/src/App.js
+++ src/gui/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Environment from "./routes/Environment";
 import Genome from "./routes/Genome";
@@ -9,6 +10,13 @@ import ResponsiveAppBar from "./components/ResponsiveAppBar";
 import ProgressStepper from "./components/ProgressStepper";
 
 function App() {
+  const [definedMotor, setDefinedMotor] = useState([]);
+  const [definedSensory, setDefinedSensory] = useState([]);
+  const [definedMappings, setDefinedMappings] = useState([]);
+
+  console.log(definedSensory);
+  console.log(definedMotor);
+
   return (
     <>
       <ResponsiveAppBar />
@@ -16,9 +24,35 @@ function App() {
         <Route path="/" element={<Navigate replace to="/environment" />} />
         <Route path="/environment" element={<Environment />} />
         <Route path="/genome" element={<Genome />} />
-        <Route path="/brain/mapping" element={<Mapping />} />
-        <Route path="/brain/motor" element={<Motor />} />
-        <Route path="/brain/sensory" element={<Sensory />} />
+        <Route
+          path="/brain/mapping"
+          element={
+            <Mapping
+              definedMappings={definedMappings}
+              setDefinedMappings={setDefinedMappings}
+              definedSensory={definedSensory}
+              definedMotor={definedMotor}
+            />
+          }
+        />
+        <Route
+          path="/brain/motor"
+          element={
+            <Motor
+              definedMotor={definedMotor}
+              setDefinedMotor={setDefinedMotor}
+            />
+          }
+        />
+        <Route
+          path="/brain/sensory"
+          element={
+            <Sensory
+              definedSensory={definedSensory}
+              setDefinedSensory={setDefinedSensory}
+            />
+          }
+        />
         <Route path="/monitoring" element={<MonitoringDashboard />} />
       </Routes>
       <ProgressStepper />
