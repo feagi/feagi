27abf57dc28474e98de5a6cd299a25f4a4db9ff9
Eric Miller <ermiller@alumni.cmu.edu>
2022-05-16 17:36:27 -0400

    check in progress generating genome (need to update/refactor)

--- src/gui/src/routes/GenomeAssembler.js
+++ src/gui/src/routes/GenomeAssembler.js
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import FeagiAPI from "../services/FeagiAPI";
 
 const GenomeAssembler = (props) => {
-  console.log(props);
+  // don't forget to insert additional genome data (version, morphologies, etc.)
+
+  // default values for post-synaptic current multiplier + plasticity flag?
+  // or add user inputs for these in the cortical mapping form?
+
+  // add title, button, some sort of progress/loading indicator
+
+  const [genomeString, setGenomeString] = useState({});
+
+  console.log(props.definedSensory);
+  console.log(props.definedMotor);
+
+  Object.keys(props.definedMappings).forEach((key) => {
+    if (key in props.definedSensory && key in props.definedMotor) {
+      console.log("IT IS IN BOTH!!!");
+    } else if (key in props.definedSensory && !(key in props.definedMotor)) {
+      console.log("IT IS ONLY IN SENSORY!!!");
+
+      // create a function for performing this operation
+      Object.keys(props.definedSensory[key]).forEach((subKey) => {
+        if (subKey.includes("-dstmap-d")) {
+          for (let i = 0; i < props.definedMappings[key].length; i++) {
+            props.definedSensory[key][subKey] = {
+              [props.definedMappings[key][i]["dstArea"]]: [
+                [
+                  props.definedMappings[key][i]["rule"],
+                  props.definedMappings[key][i]["info"][
+                    Object.keys(props.definedMappings[key][i]["info"])[0]
+                  ],
+                  1,
+                  false,
+                ],
+              ],
+            };
+          }
+        }
+      });
+    } else if (key in props.definedMotor && !(key in props.definedSensory)) {
+      console.log("IT IS ONLY IN MOTOR!!!");
+
+      // create a function for performing this operation
+      Object.keys(props.definedMotor[key]).forEach((subKey) => {
+        if (subKey.includes("-dstmap-d")) {
+          for (let i = 0; i < props.definedMappings[key].length; i++) {
+            props.definedMotor[key][subKey] = {
+              [props.definedMappings[key][i]["dstArea"]]: [
+                [
+                  props.definedMappings[key][i]["rule"],
+                  props.definedMappings[key][i]["info"][
+                    Object.keys(props.definedMappings[key][i]["info"])[0]
+                  ],
+                  1,
+                  false,
+                ],
+              ],
+            };
+          }
+        }
+      });
+    }
+  });
+
+  console.log(props.definedSensory);
+  console.log(props.definedMotor);
+
+  const insertMappingData = (mappingData, sensoryData, motorData) => {};
+
+  const generateGenome = (sensoryData, motorData) => {};
+
   return <div>GenomeAssembler</div>;
 };
 
