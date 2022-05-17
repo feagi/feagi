e0b4d7f524f72e050a4b44e385bbe800d719ee14
Eric Miller <ermiller@alumni.cmu.edu>
2022-05-17 18:06:49 -0400

    refactor GenomeAssembler (WIP)

--- src/gui/src/routes/GenomeAssembler.js
+++ src/gui/src/routes/GenomeAssembler.js
@@ -4,105 +4,118 @@ import Typography from "@mui/material/Typography";
 import FeagiAPI from "../services/FeagiAPI";
 
 const GenomeAssembler = (props) => {
-  // don't forget to insert additional genome data (version, morphologies, etc.)
-
-  // default values for post-synaptic current multiplier + plasticity flag?
-  // or add user inputs for these in the cortical mapping form?
-
   // add title, button, some sort of progress/loading indicator
+  console.log(props);
 
-  const [genomeString, setGenomeString] = useState({});
-
-  console.log(props.definedSensory);
-  console.log(props.definedMotor);
-
-  Object.keys(props.definedMappings).forEach((key) => {
-    if (key in props.definedSensory && key in props.definedMotor) {
-      console.log("IN BOTH!!!");
-
-      insertMappingData(props.definedMappings, key, props.definedSensory);
-      insertMappingData(props.definedMappings, key, props.definedMotor);
-    } else if (key in props.definedSensory && !(key in props.definedMotor)) {
-      console.log("ONLY IN SENSORY!!!");
-
-      insertMappingData(props.definedMappings, key, props.definedSensory);
-
-      // create a function for performing this operation
-      // Object.keys(props.definedSensory[key]).forEach((subKey) => {
-      //   if (subKey.includes("-dstmap-d")) {
-      //     for (let i = 0; i < props.definedMappings[key].length; i++) {
-      //       props.definedSensory[key][subKey] = {
-      //         [props.definedMappings[key][i]["dstArea"]]: [
-      //           [
-      //             props.definedMappings[key][i]["rule"],
-      //             props.definedMappings[key][i]["info"][
-      //               Object.keys(props.definedMappings[key][i]["info"])[0]
-      //             ],
-      //             1,
-      //             false,
-      //           ],
-      //         ],
-      //       };
-      //     }
-      //   }
-      // });
-    } else if (key in props.definedMotor && !(key in props.definedSensory)) {
-      console.log("ONLY IN MOTOR!!!");
-
-      insertMappingData(props.definedMappings, key, props.definedMotor);
-
-      // create a function for performing this operation
-      // Object.keys(props.definedMotor[key]).forEach((subKey) => {
-      //   if (subKey.includes("-dstmap-d")) {
-      //     for (let i = 0; i < props.definedMappings[key].length; i++) {
-      //       props.definedMotor[key][subKey] = {
-      //         [props.definedMappings[key][i]["dstArea"]]: [
-      //           [
-      //             props.definedMappings[key][i]["rule"],
-      //             props.definedMappings[key][i]["info"][
-      //               Object.keys(props.definedMappings[key][i]["info"])[0]
-      //             ],
-      //             1,
-      //             false,
-      //           ],
-      //         ],
-      //       };
-      //     }
-      //   }
-      // });
-    }
-  });
-
-  console.log(props.definedSensory);
-  console.log(props.definedMotor);
+  let genomeBase = {
+    version: "2.0",
+    max_burst_count: 3,
+    burst_delay: 0.1,
+    evolution_burst_count: 50,
+    ipu_idle_threshold: 1000,
+    neuron_morphologies: {
+      placeholder: {},
+      block_to_block: {
+        vectors: [[0, 0, 0]],
+      },
+      decrease_filter_diagonal: {
+        vectors: [[0, 1, 1]],
+      },
+      decrease_filter_diagonal2: {
+        vectors: [[0, 2, 1]],
+      },
+      increase_filter_diagonal: {
+        vectors: [[0, 1, -1]],
+      },
+      increase_filter_diagonal2: {
+        vectors: [[0, 2, -1]],
+      },
+      y_consolidator: {
+        patterns: [
+          ["*", "*", "*"],
+          ["*", "?", "*"],
+        ],
+      },
+      "lateral_+x": {
+        vectors: [[1, 0, 0]],
+      },
+      "lateral_-x": {
+        vectors: [[-1, 0, 0]],
+      },
+      "lateral_+y": {
+        vectors: [[0, 1, 0]],
+      },
+      "lateral_-y": {
+        vectors: [[0, -1, 0]],
+      },
+      "lateral_+z": {
+        vectors: [[0, 0, 1]],
+      },
+      "lateral_-z": {
+        vectors: [[0, 0, -1]],
+      },
+      one_to_all: {
+        patterns: [
+          [1, 1, 1],
+          ["*", "*", "*"],
+        ],
+      },
+      all_to_one: {
+        patterns: [
+          ["*", "*", "*"],
+          [1, 1, 1],
+        ],
+      },
+      "to_block_[5, 7, 4]": {
+        patterns: [
+          ["*", "*", "*"],
+          [5, 7, 4],
+        ],
+      },
+      expander_x: {
+        functions: true,
+      },
+      reducer_x: {
+        functions: true,
+      },
+      randomizer: {
+        functions: true,
+      },
+      lateral_pairs_x: {
+        functions: true,
+      },
+    },
+    species: {
+      parents: {},
+      species_id: "",
+      class: "toy",
+      brand: "gazebo",
+      model: "smart_car",
+    },
+    blueprint: {},
+  };
 
   const matchMappings = (mappingData, sensoryData, motorData) => {
     Object.keys(mappingData).forEach((key) => {
-      if (key in sensoryData && key in motorData) {
+      if (key in sensoryData && !(key in motorData)) {
+        console.log("ONLY IN SENSORY!!!");
         insertMappingData(mappingData, key, sensoryData);
-        insertMappingData(mappingData, key, motorData);
-      } else if (key in sensoryData && !(key in motorData)) {
-        insertMappingData(mappingData, key, sensoryData);
+        console.log(sensoryData);
       } else if (key in motorData && !(key in sensoryData)) {
+        console.log("ONLY IN MOTOR!!!");
         insertMappingData(mappingData, key, motorData);
+        console.log(motorData);
       }
     });
   };
 
   const insertMappingData = (mappingData, targetKey, corticalAreaData) => {
-    Object.keys(mappingData[targetKey]).forEach((subKey) => {
+    Object.keys(corticalAreaData[targetKey]).forEach((subKey) => {
       if (subKey.includes("-dstmap-d")) {
         for (let i = 0; i < mappingData[targetKey].length; i++) {
           corticalAreaData[targetKey][subKey] = {
             [mappingData[targetKey][i]["dstArea"]]: [
-              [
-                mappingData[targetKey][i]["rule"],
-                mappingData[targetKey][i]["info"][
-                  Object.keys(mappingData[targetKey][i]["info"])[0]
-                ],
-                1,
-                false,
-              ],
+              [mappingData[targetKey][i]["rule"], [1, 1, 1], 1, false],
             ],
           };
         }
@@ -110,7 +123,19 @@ const GenomeAssembler = (props) => {
     });
   };
 
-  const generateGenome = (sensoryData, motorData) => {};
+  const generateGenome = (sensoryData, motorData, genomeBase) => {
+    let blueprintShell = {};
+    let combinedDataVals = Object.values({ ...sensoryData, ...motorData });
+    // loop through combined values and add them to blueprintShell
+    // then genomeBase["blueprint"] = blueprintShell
+  };
+
+  let mappingDataCopy = structuredClone(props.definedMappings);
+  let sensoryDataCopy = structuredClone(props.definedSensory);
+  let motorDataCopy = structuredClone(props.definedMotor);
+
+  matchMappings(mappingDataCopy, sensoryDataCopy, motorDataCopy);
+  generateGenome(sensoryDataCopy, motorDataCopy, genomeBase);
 
   return <div>GenomeAssembler</div>;
 };
