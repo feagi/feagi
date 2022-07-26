ffa38986cbb9ed4686a1e145bdffa28557541013
Sabra <ssisler7@gmail.com>
2022-07-26 09:24:23 -0400

    ensured shock was working

--- src/trn/shock.py
+++ src/trn/shock.py
@@ -87,6 +87,7 @@ def shock_scenario_2():
     TBD
     """
     print("Shock scenario 2 has been triggered!")
+    shock_level = 0
     for neuron in runtime_data.fire_candidate_list["ii_inf"]:
         shock_level += 3
 
