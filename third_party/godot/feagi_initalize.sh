2b0e55ee9bb198ccdbb4d3b89210c991c073e785
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-07-29 13:37:55 -0400

    updated to work with FEAGI completely

--- third_party/godot/feagi_initalize.sh
+++ third_party/godot/feagi_initalize.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+FLAG=true
 FILE=./csv_data.gdc
 rm /root/godot_source/csv_data.gdc
 echo "% pre-bridge launch"
@@ -8,21 +9,29 @@ echo "% post-bridge launch"
 
 cd ../html
 
-live-server &
+#live-server &
 #http-server -c-1 &
 
 cd ../godot_source/
 
 while true; do
-  while [ ! -s csv_data.gdc ]; do
-    sleep 1
+  while [ ! -f csv_data.gdc ]; do
+    if [ -f reset.txt ]; then
+      if $FLAG; then
+        FLAG=false
+        echo "WORKING!"
+        rm ../html/*
+        cp ../html_backup/* ../html/
+      fi
+    fi
   done
   echo "\n\n\n ### ### ### ### ### ### ### ### New CSV has been detected \n\n\n"
 
-  rm ../html/*
-  cp ../html_backup/* ../html/
-
+#  rm ../html/*
+#  cp ../html_backup/* ../html/
   ./Godot_v3.4.4-stable_linux_headless.64 --export "HTML5"
   rm ./csv_data.gdc
+  rm ./reset.txt
+  FLAG=true
 
 done
