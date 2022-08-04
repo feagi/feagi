45e0ee1bfc4acb5b61b0be7ac0ba014ea7a2bd28
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2022-08-04 15:51:27 -0400

    VNC performance tuning

--- third_party/gazebo/smart_car/start_controller.sh
+++ third_party/gazebo/smart_car/start_controller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Xvfb :1 -ac -noreset -core -screen 0 1280x1024x24 &
+Xvfb :1 -ac -noreset -core -screen 0 1280x768x12 &
 export DISPLAY=:1.0
 export RENDER_ENGINE_VALUES=ogre2
 export MESA_GL_VERSION_OVERRIDE=3.3
