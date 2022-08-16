964eb5a2f9223d94378098ae0b6c79e05047809d
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-08-16 10:06:14 -0400

    added accelerator section

--- src/pns/stimuli_translator.py
+++ src/pns/stimuli_translator.py
@@ -291,3 +291,28 @@ def gyro_translator(gyroscope_data):
                 # runtime_data.fcl_queue.put({cortical_area: set(neurons)})
     else:
         print("Warning! Cortical stimulation received but genome missing", cortical_area)
+
+def accelerator_translator(accelerator_data):
+    """
+    Translate the lidar messages based on its type.
+
+    todo: add details here about the message format and expectations
+    """
+    cortical_area = 'i__acc'
+    if cortical_area_in_genome(cortical_area):
+        if accelerator_data is not None:
+            x = accelerator_data['0']
+            y = accelerator_data['1']
+            z = accelerator_data['2']
+            holder_position = 0
+            for i in x, y, z:
+                detections = stimuli_processor.accelerator_to_coords(i, holder_position)
+                holder_position+=1
+                neurons = stimuli_processor.coords_to_neuron_ids(detections, cortical_area=cortical_area)
+                if 'i__acc' not in runtime_data.fire_candidate_list:
+                    runtime_data.fire_candidate_list['i__acc'] = set()
+                for neuron in neurons:
+                    runtime_data.fire_candidate_list['i__acc'].add(neuron)
+                # runtime_data.fcl_queue.put({cortical_area: set(neurons)})
+    else:
+        print("Warning! Cortical stimulation received but genome missing", cortical_area)
