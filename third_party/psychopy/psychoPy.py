9a08ea0117946502dc50d18437fdb7baa11009d1
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-09-09 16:26:57 -0400

    halfway checkpoint

--- third_party/psychopy/psychoPy.py
+++ third_party/psychopy/psychoPy.py
@@ -5,8 +5,12 @@
 Demo of dot kinematogram
 """
 
-from psychopy import visual, event, core
 import numpy as np
+import feagi_interface as FEAGI
+from time import sleep
+from configuration import *
+from datetime import datetime
+from psychopy import visual, event, core
 
 
 def ndarray_to_list(array):
@@ -15,10 +19,10 @@ def ndarray_to_list(array):
     return new_list
 
 
-def get_rgb(frame):
+def get_rgb(frame, w, h):
     vision_dict = dict()
-    frame_row_count = 600
-    frame_col_count = 600
+    frame_row_count = w
+    frame_col_count = h
 
     x_vision = 0  # row counter
     y_vision = 0  # col counter
@@ -36,9 +40,9 @@ def get_rgb(frame):
                 if previous_frame[index] != frame[index]:
                     if (abs((previous_frame[index] - frame[index])) / 100) > \
                             0:
-                        print("CHANGED FRAME: ", frame[index])
+                        # print("CHANGED FRAME: ", frame[index])
                         dict_key = str(x_vision) + '-' + str(y_vision) + '-' + str(z_vision)
-                        print("dict: ", dict_key)
+                        # print("dict: ", dict_key)
                         vision_dict[dict_key] = frame[index]  # save the value for the changed index to the dict
                 z_vision += 1
                 if z_vision == 3:
@@ -56,39 +60,80 @@ def get_rgb(frame):
     return {'vision': vision_dict}
 
 
-win = visual.Window((600, 600), allowGUI=False, winType='pyglet')
+if __name__ == "__main__":
+    # Generate runtime dictionary
+    runtime_data = {"cortical_data": {}, "current_burst_id": None, "stimulation_period": None, "feagi_state": None,
+                    "feagi_network": None}
 
-# Initialize some stimuli
-dotPatch = visual.DotStim(win, color=(1.0, 1.0, 1.0), dir=270,
-                          nDots=1, fieldShape='circle', fieldPos=(0.0, 0.0), fieldSize=1,
-                          dotLife=5,  # number of frames for each dot to be drawn
-                          signalDots='same',  # are signal dots 'same' on each frame? (see Scase et al)
-                          noiseDots='position',  # do the noise dots follow random- 'walk', 'direction', or 'position'
-                          speed=0.0001, coherence=0.9)
+    # FEAGI section start
+    print("Connecting to FEAGI resources...")
 
-print(dotPatch)
+    # address = 'tcp://' + network_settings['feagi_host'] + ':' + network_settings['feagi_outbound_port']
 
-message = visual.TextStim(win, text='Any key to quit', pos=(0, -0.5))
-trialClock = core.Clock()
-previous_frame_data = dict()
+    feagi_host, api_port = FEAGI.feagi_setting_for_registration()
+    api_address = FEAGI.feagi_gui_address(feagi_host, api_port)
 
-while not event.getKeys():
-    pixels = win._getFrame()
-    pixels = np.array(pixels)
-    print("BEFORE RESIZE: ", np.shape(pixels))
-    pixels.resize(50, 50)
-    print("AFTER RESIZE: ", np.shape(pixels))
-    # pixels = ndarray_to_list(pixels)
-    # pixels_changed = get_rgb(pixels)
-    # print(pixels_changed)
-    dotPatch.draw()
-    message.draw()
-    win.flip()  # make the drawn things visible
+    stimulation_period_endpoint = FEAGI.feagi_api_burst_engine()
+    burst_counter_endpoint = FEAGI.feagi_api_burst_counter()
 
-    event.clearEvents('mouse')  # only really needed for pygame windows
+    runtime_data["feagi_state"] = FEAGI.feagi_registration(feagi_host=feagi_host, api_port=api_port)
 
-print(win.fps())
-win.close()
-core.quit()
+    print("** **", runtime_data["feagi_state"])
+    network_settings['feagi_burst_speed'] = float(runtime_data["feagi_state"]['burst_duration'])
 
-# The contents of this file are in the public domain.
+    # todo: to obtain this info directly from FEAGI as part of registration
+    ipu_channel_address = FEAGI.feagi_inbound(runtime_data["feagi_state"]['feagi_inbound_port_gazebo'])
+    print("IPU_channel_address=", ipu_channel_address)
+    opu_channel_address = FEAGI.feagi_outbound(network_settings['feagi_host'],
+                                               runtime_data["feagi_state"]['feagi_outbound_port'])
+
+    feagi_ipu_channel = FEAGI.pub_initializer(ipu_channel_address)
+    feagi_opu_channel = FEAGI.sub_initializer(opu_address=opu_channel_address)
+    # FEAGI section ends
+
+    win = visual.Window((600, 600), allowGUI=False, winType='pyglet')
+
+    # Initialize some stimuli
+    dotPatch = visual.DotStim(win, color=(1.0, 1.0, 1.0), dir=270,
+                              nDots=500, fieldShape='circle', fieldPos=(0.0, 0.0), fieldSize=3,
+                              dotLife=5,  # number of frames for each dot to be drawn
+                              signalDots='same',  # are signal dots 'same' on each frame? (see Scase et al)
+                              noiseDots='position',
+                              # do the noise dots follow random- 'walk', 'direction', or 'position'
+                              speed=0.0001, coherence=0.9)
+
+    print(dotPatch)
+
+    message = visual.TextStim(win, text='Any key to quit', pos=(0, -0.5))
+    trialClock = core.Clock()
+    previous_frame_data = dict()
+    msg_counter = runtime_data["feagi_state"]['burst_counter']
+
+    while not event.getKeys():
+        message_from_feagi = feagi_opu_channel.receive()
+        pixels = win._getFrame()
+        pixels = np.array(pixels)
+        # print("BEFORE RESIZE: ", np.shape(pixels))
+        n, m = 600, 600  # We need to implement that to improve way to code instead of a fixed hardcoded numbers.
+        # pixels.resize(16, 16, 3)
+        # print("AFTER RESIZE: ", np.shape(pixels))
+        pixels = ndarray_to_list(pixels)
+        pixels_changed = get_rgb(pixels, n, m)
+        dotPatch.draw()
+        message.draw()
+        win.flip()  # make the drawn things visible
+        try:
+            if "data" not in message_to_feagi:
+                message_to_feagi["data"] = dict()
+            if "sensory_data" not in message_to_feagi["data"]:
+                message_to_feagi["data"]["sensory_data"] = dict()
+            message_to_feagi["data"]["sensory_data"]['camera'] = pixels_changed['vision']
+        except Exception as e:
+            pass
+        message_to_feagi['timestamp'] = datetime.now()
+        message_to_feagi['counter'] = msg_counter
+        sleep(network_settings['feagi_burst_speed'])
+        feagi_ipu_channel.send(message_to_feagi)
+        message_to_feagi.clear()
+    win.close()
+    core.quit()
