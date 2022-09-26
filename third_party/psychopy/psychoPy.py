92f5bf3cb35edcd1ee37d715270953debe387d32
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-09-26 14:59:12 -0400

    added a mini-game for FEAGI

--- third_party/psychopy/psychoPy.py
+++ third_party/psychopy/psychoPy.py
@@ -5,6 +5,7 @@
 Demo of dot kinematogram
 """
 
+import random
 import requests
 import numpy as np
 import retina as retina
@@ -20,6 +21,43 @@ def ndarray_to_list(array):
     array = array.flatten()
     new_list = (array.tolist())
     return new_list
+
+
+def chroma_keyer(frame, size, name_id):
+    vision_dict = dict()
+    frame_row_count = size[0]  # width
+    frame_col_count = size[1]  # height
+
+    x_vision = 0  # row counter
+    y_vision = 0  # col counter
+    z_vision = 0  # RGB counter
+
+    previous_frame = {}
+    frame_len = frame_row_count * frame_col_count * 3  # hardcoded. Needs to update this section.
+    try:
+        if frame_len == frame_row_count * frame_col_count * 3:  # check to ensure frame length matches the
+            # resolution setting
+            for index in range(frame_len):
+                if frame[index] != 128:
+                    dict_key = str(y_vision) + '-' + str(abs((frame_row_count - 1) - x_vision)) + '-' + str(
+                        0)
+                    vision_dict[dict_key] = frame[index]  # save the value for the changed index to the dict
+                z_vision += 1
+                if z_vision == 3:
+                    z_vision = 0
+                    y_vision += 1
+                    if y_vision == frame_col_count:
+                        y_vision = 0
+                        x_vision += 1
+
+    except Exception as e:
+        print("Error: Raw data frame does not match frame resolution")
+        print("Error due to this: ", e)
+
+    if len(vision_dict) > 3500:
+        return {'camera': {name_id: {}}}
+    else:
+        return {'camera': {name_id: vision_dict}}
 
 
 def get_rgb(frame, size, previous_frame_data, name_id):
@@ -124,6 +162,9 @@ if __name__ == "__main__":
     Y = 0
     flag = False
     mouse_flag = False
+    redPill.pos = misc.pol2cart(redTheta, radius=0.6)
+    rgb = dict()
+    rgb['camera'] = dict()
 
     while not event.getKeys():
         message_from_feagi = feagi_opu_channel.receive()
@@ -132,49 +173,36 @@ if __name__ == "__main__":
 
         # Handle the wheel(s):
         # dY is the normal mouse wheel, but some have a dX as well
-        wheel_dX, wheel_dY = myMouse.getWheelRel()
-        grating.setOri(wheel_dY * 5, '+')
-
-        # get rid of other, unprocessed events
-        event.clearEvents()
-        redTheta += 0.2
-        blueTheta -= -0.4
-
-        redPill.pos = misc.pol2cart(redTheta, radius=0.6)
-        bluePill.pos = misc.pol2cart(blueTheta, radius=0.8)
-
-        # redPill.draw()
-        # bluePill.draw()
+        # wheel_dX, wheel_dY = myMouse.getWheelRel()
+        # grating.setOri(wheel_dY * 5, '+')
+        redPill.draw()
 
         # Do the drawing
         fixSpot.draw()
-        pixels = win._getFrame()
-        pixels = np.array(pixels)
+        pixels = np.array(win._getFrame())
         win.flip()
         retina_data = retina.frame_split(pixels)
         opu_data = FEAGI.opu_processor(message_from_feagi)
-        rgb = dict()
-        rgb['camera'] = dict()
-        if previous_data_frame == {}:
-            for i in retina_data:
-                previous_name = str(i) + "_prev"
-                previous_data_frame[previous_name] = {}
+        # if previous_data_frame == {}:
+        #     for i in retina_data:
+        #         # previous_name = str(i) + "_prev"
+        #         previous_data_frame[previous_name] = {}
         for i in retina_data:
             name = i
             if 'prev' not in i:
                 data = ndarray_to_list(retina_data[i])
                 if 'C' in i:
                     previous_name = str(i) + "_prev"
-                    rgb_data, previous_data_frame[previous_name] = get_rgb(data,
-                                                                           capabilities['vision'][
-                                                                               'central_vision_compression'],
-                                                                           previous_data_frame[previous_name], name)
+                    rgb_data = chroma_keyer(data,
+                                            capabilities['vision'][
+                                                'central_vision_compression'],
+                                            name)
                 else:
-                    previous_name = str(i) + "_prev"
-                    rgb_data, previous_data_frame[previous_name] = get_rgb(data,
-                                                                           capabilities['vision'][
-                                                                               'peripheral_vision_compression'],
-                                                                           previous_data_frame[previous_name], name)
+                    # previous_name = str(i) + "_prev"
+                    rgb_data = chroma_keyer(data,
+                                            capabilities['vision'][
+                                                'peripheral_vision_compression'],
+                                            name)
                 for a in rgb_data['camera']:
                     rgb['camera'][a] = rgb_data['camera'][a]
         try:
@@ -188,18 +216,17 @@ if __name__ == "__main__":
         if opu_data is not None:
             if 'motor' in opu_data:
                 if opu_data['motor']:
-                    print("FEAGI DATA: ", opu_data['motor'])
                     for i in opu_data['motor']:
                         if i // 2 == 0:
                             if i % 2 == 0:
-                                X += opu_data['motor'][i] / 1000
+                                X += opu_data['motor'][i] / 100
                             else:
-                                X -= opu_data['motor'][i] / 1000
+                                X -= opu_data['motor'][i] / 100
                         if i // 2 == 1:
                             if i % 2 == 0:
-                                Y += opu_data['motor'][i] / 1000
+                                Y += opu_data['motor'][i] / 100
                             else:
-                                Y -= opu_data['motor'][i] / 1000
+                                Y -= opu_data['motor'][i] / 100
                 if opu_data['misc']:
                     for i in opu_data['misc']:
                         if i == 0:
@@ -210,34 +237,48 @@ if __name__ == "__main__":
                             mouse3 = 1
                 fixSpot.setPos([X, Y])
 
-        print("mouse1: ", mouse1, " mouse2: ", mouse2, " mouse3: ", mouse3)
-        if mouse1:
-            print(fixSpot.pos)
-            for i in range(10):
-                for y in range(10):
-                    print("*" * 5)
-        if mouse2:
-            for i in range(10):
-                for y in range(10):
-                    print("@" * 5)
-        if mouse3:
-            for i in range(10):
-                for y in range(10):
-                    print("z" * 5)
+        # Psychopy game start here
+        # if mouse1:
+        #     print(fixSpot.pos)
+        #     for i in range(10):
+        #         for y in range(10):
+        #             print("*" * 5)
+        # if mouse2:
+        #     for i in range(10):
+        #         for y in range(10):
+        #             print("@" * 5)
+        # if mouse3:
+        #     for i in range(10):
+        #         for y in range(10):
+        #             print("z" * 5)
+        if mouse1 == 1:
+            if redPill.contains(fixSpot.pos):
+                rnd_number = random.randint(0, 2)
+                if rnd_number == 0:
+                    redPill = visual.Rect(win, size=[0.5, 0.5], fillColor='blue')
+                elif rnd_number == 1:
+                    redPill = visual.Circle(win, size=[0.5, 0.5], fillColor='red')
+                elif rnd_number == 2:
+                    redPill = visual.Polygon(win, size=[0.5, 0.5], fillColor='yellow')
+                redPill.pos = (random.uniform(0, 1), random.uniform(0, 1))
+
+        # Psychopy game ends
         message_to_feagi['timestamp'] = datetime.now()
         message_to_feagi['counter'] = msg_counter
-        msg_counter += 1
-        flag += 1
-        if flag == 10:
-            feagi_burst_speed = requests.get(api_address + stimulation_period_endpoint).json()
-            feagi_burst_counter = requests.get(api_address + burst_counter_endpoint).json()
-            flag = 0
-            if msg_counter < feagi_burst_counter:
-                feagi_opu_channel = FEAGI.sub_initializer(opu_address=opu_channel_address)
-                if feagi_burst_speed != network_settings['feagi_burst_speed']:
-                    network_settings['feagi_burst_speed'] = feagi_burst_speed
-        sleep(network_settings['feagi_burst_speed'])
+        # msg_counter += 1
+        # flag += 1
+        # if flag == 10:
+        #     feagi_burst_speed = requests.get(api_address + stimulation_period_endpoint).json()
+        #     feagi_burst_counter = requests.get(api_address + burst_counter_endpoint).json()
+        #     flag = 0
+        #     if msg_counter < feagi_burst_counter:
+        #         feagi_opu_channel = FEAGI.sub_initializer(opu_address=opu_channel_address)
+        #         if feagi_burst_speed != network_settings['feagi_burst_speed']:
+        #             network_settings['feagi_burst_speed'] = feagi_burst_speed
         feagi_ipu_channel.send(message_to_feagi)
+        # sleep(network_settings['feagi_burst_speed'])
         message_to_feagi.clear()
+        for i in rgb['camera']:
+            rgb['camera'][i].clear()
     win.close()
     core.quit()
