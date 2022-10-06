9ea36f39fc519de44495e04c64f8d8106590cbde
Kakcalu13 <65916520+Kakcalu13@users.noreply.github.com>
2022-10-06 09:26:04 -0400

    Feature godot red fix (#171)
    * remove cortical area of vision
    
    * testing meshinstance
    
    * increased max on websocket
    
    * latest project on godot
    
    * added multi-meshinstance
    
    * temporary test
    
    * push latest on godot project
    
    * replaced gridmap with multimeshinstance
    
    * eliminated freezing websocket
    
    * update to latest
    
    * reduced and eliminated massive cortical area such as vision
    
    * added cv for robot
    
    * updated latest

--- third_party/godot/src/bridge_godot_python.py
+++ third_party/godot/src/bridge_godot_python.py
@@ -92,7 +92,7 @@ def simulation_testing():
     for i in range(1000):
         x = random.randint(0, 64)
         y = random.randint(0, 64)
-        z = 3
+        z = random.randint(0, 64)
         array.append((x, y, z))
     return array
 
@@ -118,8 +118,7 @@ def godot_data(input):
     Simply clean the list and remove all unnecessary special characters and deliver with name, xyz only
     """
     data = ast.literal_eval(input)
-    dict_with_updated_name = {}
-    dict_with_updated_name["data"] = dict()
+    dict_with_updated_name = {"data": dict()}
     dict_with_updated_name["data"]["direct_stimulation"] = dict(dict())
     for key in data["data"]["direct_stimulation"]:
         Updated_name = name_to_id(key)
@@ -277,7 +276,7 @@ def feagi_init(feagi_host, api_port):
 async def echo(websocket):
     while True:
         try:
-            if len(zmq_queue) > 5:
+            if len(zmq_queue) > 2: # This will eliminate any stack up queue
                 stored_value = zmq_queue[len(zmq_queue)-1]
                 zmq_queue.clear()
                 zmq_queue[0] = stored_value
@@ -294,7 +293,8 @@ async def echo(websocket):
 
 
 async def websocket_main():
-    async with websockets.serve(echo, "0.0.0.0", configuration.network_settings['godot_websocket_port'], max_size=None):
+    async with websockets.serve(echo, "0.0.0.0", configuration.network_settings['godot_websocket_port'], max_size=None,
+                                max_queue=None, write_limit=None, compression=None):
         await asyncio.Future()
 
 
@@ -359,7 +359,7 @@ if __name__ == "__main__":
                 except Exception as e:
                     print("Error during genome reset:\n", e)
             one_frame = feagi_breakdown(one_frame)
-            # one_frame = simulation_testing()
+            #one_frame = simulation_testing()
             if burst_second > network_settings['burst_duration_threshold']:
                 zmq_queue.append(one_frame)
         if ws_queue:
