f1a48645b77017453c59a6b51bfc0e578e3a441d
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-10-09 15:59:52 -0400

    fixed the name

--- src/inf/initialize.py
+++ src/inf/initialize.py
@@ -383,7 +383,7 @@ def init_io_channels():
             runtime_data.router_address_gazebo = "tcp://" + runtime_data.parameters['Sockets']['gazebo_host_name'] + ':' + runtime_data.parameters['Sockets'][
                 'feagi_inbound_port_gazebo']
         if runtime_data.parameters['Sockets']['feagi_inbound_port_embodiment']:
-            runtime_data.router_address_gazebo = "tcp://" + runtime_data.parameters['Sockets']['embodiment_host_name'] \
+            runtime_data.router_address_embodiment = "tcp://" + runtime_data.parameters['Sockets']['embodiment_host_name'] \
                                                  + ':' + runtime_data.parameters['Sockets']['feagi_inbound_port_embodiment']
 
         print("Router addresses has been set")
