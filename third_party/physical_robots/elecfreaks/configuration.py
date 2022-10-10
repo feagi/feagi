920e5659230308117d5704fba03f8e6a5ad65d93
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-10-10 08:45:56 -0400

    reverted the app name

--- third_party/physical_robots/elecfreaks/configuration.py
+++ third_party/physical_robots/elecfreaks/configuration.py
@@ -22,7 +22,7 @@ network_settings = {
     "feagi_host": "127.0.0.1",
     "feagi_api_port": "8000",
     "feagi_outbound_port": "30000",
-    "feagi_inbound_port_embodiment": "30003",
+    "feagi_inbound_port_gazebo": "30002",
     'TTL': 2,
     'last_message': 0,
     "microbit_mac_address": "xx.xx.xx.xx.xx.xx",
