97aa8fc102aa6325525c8c5a03ad3d0f97e200aa
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-10-21 11:33:53 -0400

    added configuration for arm

---
+++ third_party/physical_robots/mycobot/configuration.py
@@ -0,0 +1,39 @@
+"""
+Copyright 2016-2022 The FEAGI Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================
+"""
+# !/usr/bin/env python3
+
+app_name = 'gazebo'
+
+network_settings = {
+    "feagi_host": "127.0.0.1",
+    "feagi_api_port": "8000",
+    'TTL': 2,
+    'last_message': 0,
+}
+
+capabilities = {
+    "servo": {
+        "type": "opu",
+        "disabled": True,
+        "refresh_rate": 1,
+        "cortical_mapping": "o__ser",
+        'count': 2,
+        'topic_identifier': '/S'
+    }
+}
+
+message_to_feagi = {"data": {}}
