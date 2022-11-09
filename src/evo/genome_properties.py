b8445efecfda95bb745e35b85ea70509fbf71d0c
Mohammad Nadji <26581495+m-nadji-tehrani@users.noreply.github.com>
2022-11-09 14:56:55 -0500

    Genome Validation (#177)

---
+++ src/evo/genome_properties.py
@@ -0,0 +1,23 @@
+genome_properties = {
+    "structure": {
+        "segment_guide": "________-______-__-______-_",
+        "segment_count": 5,
+        "segment_seperator": "-"
+    },
+    "position": {
+        "gene": [0, 0, 26],
+        "expression_parameters": [1, 0, 7],
+        "cortical_id": [2, 9, 14],
+        "gene_classifier": [3, 16, 17],
+        "encoding_id": [4, 19, 24],
+        "value_type": [5, 26, 26],
+    },
+    "value": {
+        "gene_classifier": [
+            "nx", "cx"
+        ],
+        "value_type": [
+            "b", "i", "f", "d", "t"
+        ]
+    }
+}
