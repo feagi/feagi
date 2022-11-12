f177390fdfd6bcf5a0b2199d35fb1fde39f8bcfc
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-11-12 14:28:31 -0500

    added individual scripts

---
+++ third_party/godot/godot_source/menu_assets/add_cortical_button.gd
@@ -0,0 +1,26 @@
+extends Button
+
+
+func _ready():
+	visible = false
+
+
+func _on_Button_pressed():
+	pass # Replace with function body.
+
+
+func _on_add_cortical_button_pressed():
+	if $cortical_menu.visible:
+		$cortical_menu.visible = false
+	else:
+		$cortical_menu.visible = true
+#
+#	if $name.visible:
+#		$name.visible = false
+#		$name_string.visible = false
+#	else:
+#		$name.visible = true
+#		$name_string.visible = true
+
+func _on_LineEdit_text_changed(new_text):
+	print("ENTERED")
---
+++ third_party/godot/godot_source/menu_assets/name_string.gd
@@ -0,0 +1,4 @@
+extends LineEdit
+
+func _ready():
+	visible = true
