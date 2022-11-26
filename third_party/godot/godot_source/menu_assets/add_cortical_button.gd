3be1f5e5cccdd7a6aeafc7ea4386bb08cb61537c
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-11-26 17:32:36 -0500

    remove massive comments

--- third_party/godot/godot_source/menu_assets/add_cortical_button.gd
+++ third_party/godot/godot_source/menu_assets/add_cortical_button.gd
@@ -4,27 +4,6 @@ extends Button
 func _ready():
 	visible = false
 
-
-#func _on_Button_pressed():
-#	if $cortical_menu.visible:
-#		$cortical_menu.visible = false
-#	else:
-#		$cortical_menu.visible = true
-
-
-#func _on_add_cortical_button_pressed():
-#	if $cortical_menu.visible:
-#		$cortical_menu.visible = false
-#	else:
-#		$cortical_menu.visible = true
-#
-#	if $name.visible:
-#		$name.visible = false
-#		$name_string.visible = false
-#	else:
-#		$name.visible = true
-#		$name_string.visible = true
-
 func _on_LineEdit_text_changed(new_text):
 	new_text = new_text
 	pass
