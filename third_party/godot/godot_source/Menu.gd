2a3cdb7115a90eab81cfbc5a2a3af51dac6b9fee
Kakcalu13 <kevin.a.araujo@gmail.com>
2022-11-26 17:31:56 -0500

    remove unnecessary button

--- third_party/godot/godot_source/Menu.gd
+++ third_party/godot/godot_source/Menu.gd
@@ -4,26 +4,25 @@ var pressed = false
 
 func _ready():
 	visible = true
-	
+
 func check_esc():
 	if Input.is_action_just_pressed("esc"):
 		if visible:
 			visible = false
 		else:
 			visible = true
-			
+
 func _on_Button_pressed():
 	if $menu_background.visible:
 		$menu_background.visible = false
 		$add_cortical_button.visible = false
-		$move_cortical.visible = false
 		$add_cortical_button/cortical_menu.visible = false
-		$move_cortical/cortical_menu.visible = false
+
 		
 	else:
 		$add_cortical_button.visible = true
 		$menu_background.visible = true
-		$move_cortical.visible = true
+
 
 func _process(_delta):
 	check_esc()
@@ -38,20 +37,7 @@ func _on_Add_it_pressed():
 func _on_add_cortical_button_pressed():
 	if pressed != true:
 		pressed = true
-		$move_cortical.visible = false
 		$add_cortical_button/cortical_menu.visible = true
 	else:
 		pressed = false
-		$move_cortical.visible = true
 		$add_cortical_button/cortical_menu.visible = false
-
-
-func _on_move_cortical_pressed():
-	if pressed != true:
-		pressed = true
-		$add_cortical_button.visible = false
-		$move_cortical/cortical_menu.visible = true
-	else:
-		pressed = false
-		$add_cortical_button.visible = true
-		$move_cortical/cortical_menu.visible = false
