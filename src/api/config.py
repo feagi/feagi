7b2af690d6ffbc0d603e29a9e0f4ace6aa79d197
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2023-01-09 16:36:32 -0500

    API configuration cleanup

--- src/api/config.py
+++ src/api/config.py
@@ -3,13 +3,6 @@ import os
 from inf import runtime_data
 from pydantic import BaseSettings
 
-k8_server = os.getenv("ip_server")
-if not k8_server:
-    k8_server = "placeholder"
-k8_server_api = "http://" + k8_server + ":" + "8000"
-k8_server_gazebo = "http://" + k8_server + ":" + "6080"
-k8_server_gadot = "http://" + k8_server + ":" + "6081"
-
 
 class Settings(BaseSettings):
     description: str = """FEAGI REST API will help you integrate FEAGI into other applications and provides a 
@@ -22,7 +15,7 @@ class Settings(BaseSettings):
     contact: dict = {
         "name": "FEAGI Community",
         "url": "http://feagi.org",
-        "email": "info@neuraville.com",
+        "email": "info@feagi.org",
     }
     license_info: dict = {
         "name": "Apache 2.0",
@@ -31,10 +24,7 @@ class Settings(BaseSettings):
     origins: list = ["http://localhost:6080",
                      "http://localhost:6081",
                      "http://localhost:3000",
-                     "http://localhost:8000",
-                     k8_server_api,
-                     k8_server_gazebo,
-                     k8_server_gadot]
+                     "http://localhost:8000"]
 
 
 settings = Settings()
