0aef402dcf6f69bbe1ca8b38e4ff80b18c6cb5dc
Kakcalu13 <kevin.a.araujo@gmail.com>
2023-01-27 09:15:29 -0500

    restructure godot file

--- third_party/godot/feagi_initalize.sh
+++ third_party/godot/feagi_initalize.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#FLAG=true
-#FILE=./csv_data.gdc
-#rm ../html/*
-#cp ../html_backup/* ../html/
-#rm /root/godot_source/csv_data.gdc
-#echo "% pre-bridge launch"
-
 python3 bridge_godot_python.py &
 echo "% post-bridge launch"
 
@@ -14,33 +7,9 @@ cd ../html
 live-server &
 
 cd ../godot_source/
-./Godot_v3.4.4-stable_linux_headless.64 --export "HTML5"
+../Godot_v3.4.4-stable_linux_headless.64 --export "HTML5"
 
 while true
   do
     :
   done
-
-#http-server -c-1 &
-
-#while true; do
-#  while [ ! -f csv_data.gdc ]; do
-#    if [ -f reset.txt ]; then
-#      if $FLAG; then
-#        FLAG=false
-#        rm ../html/*
-#        cp ../html_backup/* ../html/
-#      fi
-#    fi
-#  done
-#  echo "\n\n\n ### ### ### ### ### ### ### ### New CSV has been detected \n\n\n"
-
-#  rm ../html/*
-#  cp ../html_backup/* ../html/
-# commented above as it's already handled in bridge
-#  ./Godot_v3.4.4-stable_linux_headless.64 --export "HTML5"
-#  rm ./csv_data.gdc
-#  rm ./reset.txt
-#  FLAG=true
-
-#done
