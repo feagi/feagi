113b8ec67d5ca40a84745f198a48132dd06acd4e
Kakcalu13 <kevin.a.araujo@gmail.com>
2023-02-07 09:49:42 -0500

    added to prevent arrows control to move the camera while typing

--- third_party/godot/godot_source/leak.gd
+++ third_party/godot/godot_source/leak.gd
@@ -16,3 +16,13 @@ func _on_text_changed(_new_text):
 
 func _on_text_entered(_new_text):
 	release_focus()
+	
+func _input(event):
+	if event.is_action_pressed("ui_up") and self.has_focus():
+		Godot_list.Node_2D_control = true
+	if event.is_action_pressed("ui_down") and self.has_focus():
+		Godot_list.Node_2D_control = true
+	if event.is_action_pressed("ui_left") and self.has_focus():
+		Godot_list.Node_2D_control = true
+	if event.is_action_pressed("ui_right") and self.has_focus():
+		Godot_list.Node_2D_control = true
