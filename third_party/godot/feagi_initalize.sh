bea5f6b7b4c5195a48d05267ef3eadb8577a5a38
kakcalu13 <kevin.a.araujo@gmail.com>
2023-02-21 16:12:46 -0500

    improve feagi_initalize

--- third_party/godot/feagi_initalize.sh
+++ third_party/godot/feagi_initalize.sh
@@ -13,10 +13,21 @@ api_port="$API_PORT"
 websocket_ip="$GODOT_WEBSOCKET"
 websocket_port="$WEBSOCKET_PORT"
 
-sed -i "s/var websocket_ip_address = \"127\.0\.0\.1\"/var websocket_ip_address = \"$websocket_ip\"/g" network_configuration.gd
-sed -i "s/var api_port_address = \"8000\"/var api_port_address = \"$api_port\"/g" network_configuration.gd
-sed -i "s/var api_ip_address = \"127\.0\.0\.1\"/var api_ip_address = \"$api_ip\"/g" network_configuration.gd
-sed -i "s/var websocket_port_address = \"9050\"/var websocket_port_address = \"$websocket_port\"/g" network_configuration.gd
+if [[ -n "$websocket_ip" ]]; then
+  sed -i "s/var websocket_ip_address = \"127\.0\.0\.1\"/var websocket_ip_address = \"$websocket_ip\"/g" network_configuration.gd
+fi
+
+if [[ -n "$api_port" ]]; then
+  sed -i "s/var api_port_address = \"8000\"/var api_port_address = \"$api_port\"/g" network_configuration.gd
+fi
+
+if [[ -n "$api_ip" ]]; then
+  sed -i "s/var api_ip_address = \"127\.0\.0\.1\"/var api_ip_address = \"$api_ip\"/g" network_configuration.gd
+fi
+
+if [[ -n "$websocket_port" ]]; then
+  sed -i "s/var websocket_port_address = \"9050\"/var websocket_port_address = \"$websocket_port\"/g" network_configuration.gd
+fi
 
 
 ./Godot_v3.4.4-stable_linux_headless.64 --export "HTML5"
