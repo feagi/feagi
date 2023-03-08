6bfa51bd2d36293c41ec849cd5e99644c0423f3d
kakcalu13 <kevin.a.araujo@gmail.com>
2023-03-08 10:25:14 -0500

    move the morphology usuage from here to feagi gd

--- third_party/godot/godot_source/rule_properties.gd
+++ third_party/godot/godot_source/rule_properties.gd
@@ -1,16 +1,19 @@
 extends ColorRect
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
-
-
 
 func _on_mapping_rule_options_item_selected(index):
 	var rule_name = $mapping_rule_options.get_item_text(index)
 	if rule_name != " ":
 		if "+" in rule_name:
 			rule_name = rule_name.replace("+", "%2B")
+		if "[" in rule_name:
+			rule_name = rule_name.replace("[", "%5B")
+		if "]" in rule_name:
+			rule_name = rule_name.replace("]", "%5D")
+		if ", " in rule_name:
+			rule_name = rule_name.replace(", ", "%2C%20")
 		var combine_url = 'http://' + network_setting.api_ip_address + ':' + network_setting.api_port_address + '/v1/feagi/genome/morphology?morphology_name=' + rule_name
 		$get_morphology.request(combine_url)
 		combine_url = 'http://' + network_setting.api_ip_address + ':' + network_setting.api_port_address + '/v1/feagi/genome/morphology_usage?morphology_name=' + rule_name
@@ -22,16 +25,3 @@ func _process(_delta):
 			$rules.visible = false
 		else:
 			$rules.visible = true
-
-func _on_get_morphology_usuage_request_completed(_result, _response_code, _headers, body):
-	var json = JSON.parse(body.get_string_from_utf8())
-	var api_data = json.result
-	var new_name = ""
-	for i in api_data:
-		if new_name == "":
-			new_name = str(i).replace(",", " > ") + "\n"
-		else:
-			new_name = new_name + str(i).replace(",", " > ") +"\n"
-	new_name = new_name.replace("[", "")
-	new_name = new_name.replace("]", "")
-	$rules/morphology_definition/associations_data.text = new_name
