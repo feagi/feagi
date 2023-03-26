7f973374c815ea2ff49649734f6b6f125d7dfb26
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2023-03-26 10:45:43 -0400

    API: Composite Morphology

--- src/api/api.py
+++ src/api/api.py
@@ -108,7 +108,7 @@ class BurstEngine(BaseModel):
 
 class MorphologyProperties(BaseModel):
     name: str
-    type: Literal['vectors', 'patterns', 'functions']
+    type: Literal['vectors', 'patterns', 'composite', 'functions']
     morphology: list
 
 
@@ -637,7 +637,7 @@ async def genome_neuron_morphology_types():
     Returns the properties of a neuron morphology.
     """
     try:
-        return {"vectors", "patterns", "functions"}
+        return {"vectors", "patterns", "composite", "functions"}
     except Exception as e:
         print("API Error:", e)
         return {"Request failed...", e}
