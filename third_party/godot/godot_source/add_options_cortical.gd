4fd23f2fc8fc7ecff39d567ed0f8062b805508d1
kakcalu13 <kevin.a.araujo@gmail.com>
2023-03-28 17:35:43 -0400

    update optionbuttons using this function

--- third_party/godot/godot_source/add_options_cortical.gd
+++ third_party/godot/godot_source/add_options_cortical.gd
@@ -14,11 +14,26 @@ func load_options():
 	
 func _on_load_options_cortical_name_request_completed(_result, _response_code, _headers, body):
 	clear()
+	get_parent().get_node("inside_mapping_menu/Control/Mapping_def").clear()
+	get_parent().get_parent().get_node("rule_properties/mapping_rule_options").clear()
+	get_parent().get_node("source_dropdown").clear()
 	var json = JSON.parse(body.get_string_from_utf8())
 	var api_data = json.result
-	add_item(" ")
-	for i in api_data:
-		add_item(i)
+	if api_data != null:
+		add_item(" ")
+		get_parent().get_node("inside_mapping_menu/Control/Mapping_def").add_item(" ")
+		get_parent().get_parent().get_node("rule_properties/mapping_rule_options").add_item(" ")
+		get_parent().get_node("source_dropdown").add_item(" ")
+		for i in api_data:
+			add_item(i)
+			get_parent().get_node("inside_mapping_menu/Control/Mapping_def").add_item(i)
+			get_parent().get_parent().get_node("rule_properties/mapping_rule_options").add_item(i)
+			get_parent().get_node("source_dropdown").add_item(i)
+			
+	get_parent().get_parent().get_parent().get_parent().get_parent().get_node("notification").visible = true
+	get_parent().get_parent().get_parent().get_parent().get_parent().get_node("notification/Label").text = str(_response_code)
+	get_parent().get_parent().get_parent().get_parent().get_parent().get_node("notification/TextEdit").text = str(api_data)
+	get_parent().get_parent().get_parent().get_parent().get_parent().get_node("notification/Label2").text = "_on_cortical_type_options_request_request_completed"
 		
 func loading_in_two_seconds():
 	yield(get_tree().create_timer(2), "timeout")
