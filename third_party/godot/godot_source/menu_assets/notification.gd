7490272d1ede8d4d9bc0bc75fe6ae243bfd6a871
kakcalu13 <kevin.a.araujo@gmail.com>
2023-04-04 17:43:25 -0400

    added red/grey on notification

--- third_party/godot/godot_source/menu_assets/notification.gd
+++ third_party/godot/godot_source/menu_assets/notification.gd
@@ -4,9 +4,13 @@ var flag = false
 var current = 0
 var setpoint = false
 var projectResolution: Vector2
+var pulse_transparent = 0.3
+var default_color: Color
+
 
 func _ready():
 	visible = false
+	default_color = color
 #	projectResolution=Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/width"))
 
 func _process(_delta):
@@ -17,10 +21,13 @@ func _process(_delta):
 				timer()
 			else:
 				$TextEdit.text = "FEAGI IS NOT STARTED NOR LOADED!!"
+				color = Color(0.545098, 0, 0, pulse_transparent)
 	if $Label.text == "200":
 		$Label.add_color_override("font_color", "#1f9239")
+		color = default_color
 	else:
 		$Label.add_color_override("font_color", "#ff0000")
+		color = Color(0.545098, 0, 0, pulse_transparent)
 #	print("rect size.x: ", rect_size.x, " and current: ", current, " and rect position: ", rect_position.x)
 #	if flag:
 #		if rect_position.x != 690:
