87e5b74c1ba987a67eee9811b4b4f703b77bca05
kakcalu13 <kevin.a.araujo@gmail.com>
2023-04-12 14:26:28 -0400

    added assert

--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -21,4 +21,20 @@ from inf import initialize, runtime_data
 
 def test_feagi_initialization():
     # ensure runtime_data parameters are updated upon initialization
-    print("Done")
+    assert initialize.init_infrastructure()
+    assert runtime_data.brain_run_id
+    assert runtime_data.parameters
+    assert runtime_data.working_directory
+    assert runtime_data.connectome_path
+
+    # confirm genome creation using static genome
+    assert runtime_data.parameters['Switches']['use_static_genome']
+    assert runtime_data.genome
+    assert runtime_data.genome_id
+
+    # suppress connectome figure generation during testing
+    runtime_data.parameters['Visualization']['connectome_visualizer'] = False
+
+    # ensure completion of brain development following neuroembryogenesis
+    neuroembryogenesis.develop_brain(reincarnation_mode=False)
+    assert runtime_data.brain
