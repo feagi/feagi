a5c00610c702843c259551a5285837f6096cd0d5
kakcalu13 <kevin.a.araujo@gmail.com>
2023-04-12 14:48:17 -0400

    test

--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -15,30 +15,10 @@
 # ==============================================================================
 """
 import pytest
-from evo import neuroembryogenesis
-from inf import initialize, runtime_data
 
 
 def test_feagi_initialization():
     """
-    ensure runtime_data parameters are updated upon initialization
+    Testing
     """
-    initialize.init_parameters()
-    initialize.init_infrastructure()
-    initialize.init_brain()
-    assert runtime_data.brain_run_id
-    assert runtime_data.parameters
-    assert runtime_data.working_directory
-    assert runtime_data.connectome_path
-
-    # confirm genome creation using static genome
-    assert runtime_data.parameters['Switches']['use_static_genome']
-    assert runtime_data.genome
-    assert runtime_data.genome_id
-
-    # suppress connectome figure generation during testing
-    runtime_data.parameters['Visualization']['connectome_visualizer'] = False
-
-    # ensure completion of brain development following neuroembryogenesis
-    neuroembryogenesis.develop_brain(reincarnation_mode=False)
-    assert runtime_data.brain
+    assert(print("Done."))
