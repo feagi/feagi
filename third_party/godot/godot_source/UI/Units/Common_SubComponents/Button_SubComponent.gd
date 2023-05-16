3905955a5ef0dce38983e36720b0144470e1b251
Amir-Rasteg <127231771+Amir-Rasteg@users.noreply.github.com>
2023-05-16 12:49:08 -0400

    Add Disclaimer

--- third_party/godot/godot_source/UI/Units/Common_SubComponents/Button_SubComponent.gd
+++ third_party/godot/godot_source/UI/Units/Common_SubComponents/Button_SubComponent.gd
@@ -1,6 +1,10 @@
 extends Button
 class_name Button_SubComponent
 
+# NOTE: This is DIFFERENT from the Button COmponent, this is for adding button to existing components
+# (IE, the dropdown), and not intended to be used as a component directly. Please see
+# Button_Component.gd for that instead!
+
 signal SizeChanged(selfReference)
 
 var Hsize: Vector2:
