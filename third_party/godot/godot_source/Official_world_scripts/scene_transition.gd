bbbb53ec5d4a61e2608ecd8dded58b01b6fa8249
kakcalu13 <kevin.a.araujo@gmail.com>
2023-05-18 18:06:16 -0400

    move to menu node and updated the name switch

--- third_party/godot/godot_source/Official_world_scripts/scene_transition.gd
+++ third_party/godot/godot_source/Official_world_scripts/scene_transition.gd
@@ -2,7 +2,6 @@ extends Node3D
 
 func ready():
 	$Button.position.y = $Menu/information_menu.position.y
-	$Button.position.x = $Menu/information_menu.size.x
 	
 
 func change_scene():
@@ -14,10 +13,12 @@ func change_scene():
 		BV.visible = false
 		$Menu.visible = false
 		CB.visible = true
+		$Button.text = "Brain Visualizer"
 	elif CB.visible:
 		BV.visible = true
 		$Menu.visible = true
 		CB.visible = false
+		$Button.text = "Circuit Builder"
 	$scene_transition/AnimationPlayer.play_backwards("dissolve", -1)
 
 func _on_button_pressed():
