1a9a5d84ac98fea35879d06d25c618f41b1783f2
kakcalu13 <kevin.a.araujo@gmail.com>
2023-05-23 15:20:57 -0400

    improve way to connect API and WS

--- third_party/godot/godot_source/Shared_components/network_configuration.gd
+++ third_party/godot/godot_source/Shared_components/network_configuration.gd
@@ -23,17 +23,44 @@ func _ready():
 		}
 		getIPAddress();
 		""")
+	var port_disabled = JavaScriptBridge.eval(""" 
+		function get_port() {
+			var url_string = window.location.href;
+			var url = new URL(url_string);
+			const searchParams = new URLSearchParams(url.search);
+			const ipAddress = searchParams.get("port_disabled");
+			return ipAddress;
+		}
+		get_port();
+		""")
+	var full_url = JavaScriptBridge.eval(""" 
+		function get_port() {
+			var url_string = window.location.href;
+			var url = new URL(url_string);
+			const searchParams = new URLSearchParams(url.search);
+			const ipAddress = searchParams.get("full_url");
+			return ipAddress;
+		}
+		get_port();
+		""")
 	if ip_result == "" or ip_result == null:
 		websocket_ip_address = "127.0.0.1"
 		api_ip_address = "127.0.0.1"
 	else:
 		websocket_ip_address = ip_result
 		api_ip_address = ip_result
-	print("javascript ip: ", ip_result)
+	print("javascript ip: ", ip_result, " port: ", port_disabled, " full: ", full_url)
 	socket.set_max_queued_packets(10000000)
 	socket.inbound_buffer_size = 10000000
-	socket.connect_to_url("ws://" + str(websocket_ip_address) + ":" + str(websocket_port_address))
-
+	if full_url != null:
+		print("connecting to: ", full_url)
+		socket.connect_to_url(full_url)
+	elif port_disabled == "true":
+		print("connecting to: ", "ws://" + str(websocket_ip_address))
+		socket.connect_to_url("ws://" + str(websocket_ip_address))
+	else:
+		print("connecting to: ", "ws://" + str(websocket_ip_address) + ":" + str(websocket_port_address))
+		socket.connect_to_url("ws://" + str(websocket_ip_address) + ":" + str(websocket_port_address))
 func _process(_delta):
 	socket.poll()
 	state = socket.get_ready_state()
