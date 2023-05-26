57fc3697bf9abacfcfd50ecb07cdbc43bb24edc8
Amir-Rasteg <127231771+Amir-Rasteg@users.noreply.github.com>
2023-05-26 12:41:10 -0400

    added fill subcomponent

--- third_party/godot/godot_source/UI/Units/Components/Fill/Fill_Component.gd
+++ third_party/godot/godot_source/UI/Units/Components/Fill/Fill_Component.gd
@@ -3,25 +3,46 @@ class_name Fill_Component
 
 # Defaults, see readme
 const DEF_USINGCUSTOMCOLOR = false
-const DEF_COLOR = Color(0.0,0.0,0.0,0.0)
+const DEF_COLOR = Vector3i(255, 255, 255)
+const COLOR_OFFSET = Vector3i(255, 255, 255)
 
 
 const ADDITIONAL_SETTABLE_PROPERTIES = {
 	"usingCustomColor": TYPE_BOOL,
-	"color": TYPE_VECTOR3I}
+	"color": TYPE_VECTOR3I,
+	"colorR": TYPE_INT,
+	"colorG": TYPE_INT,
+	"colorb": TYPE_INT}
 
 const TYPE: String = "Fill"
 
 var usingCustomColor: bool:
-	get: return _Panel.visible
+	get: return _usingCustomColor
 	set(v): 
-		_Panel.visible = v
-var color: Color:
-	get: return _Panel.customColor
-	set(v): _Panel.customColor
+		_usingCustomColor = v
+		if v:
+			color = color # funny property shenanigans
+		else:
+			_Panel.resetColor()
+var color: Vector3i:
+	get: return HelperFuncs.ColorToV3I(_Panel.customColor)
+	set(v): 
+		if !usingCustomColor: return
+		_Panel.customColor = HelperFuncs.V3IToColor(v)
+		_usingCustomColor = true
+var colorR: int:
+	get: return color.x
+	set(v): color = Vector3i(v, color.y, color.z)
+var colorG: int:
+	get: return color.y
+	set(v): color = Vector3i(color.x, v, color.z)
+var colorB: int:
+	get: return color.z
+	set(v): color = Vector3i(color.x, color.y, v)
+
 
 var _Panel: Fill_SubComponent
-
+var _usingCustomColor: bool
 
 # Activates the Counter
 func _Activation(settings: Dictionary):
@@ -35,5 +56,8 @@ func _Activation(settings: Dictionary):
 	
 	# Fill in Inits
 	usingCustomColor = HelperFuncs.GetIfCan(settings, 'usingCustomColor', DEF_USINGCUSTOMCOLOR)
-	color = HelperFuncs.GetIfCan(settings, 'customColor', DEF_COLOR)
-	
+	color = HelperFuncs.LoadMostDefaultv3_Color(settings, 'color', DEF_COLOR)
+
+
+func _GetData():
+	return color
--- third_party/godot/godot_source/UI/Units/Components/Fill/Fill_SubComponent.gd
+++ third_party/godot/godot_source/UI/Units/Components/Fill/Fill_SubComponent.gd
@@ -4,6 +4,8 @@ class_name Fill_SubComponent
 
 signal SizeChanged(selfReference)
 
+var _style: StyleBoxFlat = StyleBoxFlat.new()
+
 var Hsize: Vector2:
 	get: return size
 	set(v):
@@ -12,4 +14,10 @@ var Hsize: Vector2:
 
 var customColor: Color:
 	get: return modulate
-	set(v): modulate = v
+	set(v): 
+		_style.bg_color = v
+		add_theme_stylebox_override("panel", _style)
+		
+
+func resetColor() -> void:
+	remove_theme_stylebox_override("panel")
