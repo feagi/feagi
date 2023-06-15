186b2fcfb9b128c3eeefab389c5743724e449402
Amir-Rasteg <127231771+Amir-Rasteg@users.noreply.github.com>
2023-06-15 11:42:14 -0400

    Fix Button not sending any signals

--- third_party/godot/godot_source/UI/Newnit/Elements/Sub_Elements/Button_Sub.gd
+++ third_party/godot/godot_source/UI/Newnit/Elements/Sub_Elements/Button_Sub.gd
@@ -1,11 +1,17 @@
 extends Button
 class_name Button_Sub
 
-signal value_edited()
+signal value_edited(bool)
 
 var editable: bool:
 	get: return !disabled
 	set(v): disabled = !v
+
+func _ready():
+	pressed.connect(_PressProxy)
+
+func _PressProxy():
+	value_edited.emit(true) # we need some sort of value in here
 
 # built in vars
 # text: String
