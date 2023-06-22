f45a2f34f2e8042640124319021597d89799be9e
Amir-Rasteg <127231771+Amir-Rasteg@users.noreply.github.com>
2023-06-22 12:47:46 -0400

    Pushed sizing fixes

--- third_party/godot/godot_source/UI/Newnit/Elements/Elements/Element_TextBox.gd
+++ third_party/godot/godot_source/UI/Newnit/Elements/Elements/Element_TextBox.gd
@@ -6,12 +6,14 @@ const D_value = ""
 const D_expand_to_text_length = false
 const D_max_length = 50
 const D_placeholder_text = ""
+const D_fill_textBox = true
 
 const _specificSettableProps = {
 	"value": TYPE_STRING,
 	"editable": TYPE_BOOL,
 	"placeholder_text": TYPE_STRING,
 	"wrap_mode": TYPE_INT,
+	"fill_textBox": TYPE_BOOL
 }
 
 var value: String:
@@ -30,6 +32,15 @@ var wrap_mode: int:
 	get: return _TextEdit.wrap_mode
 	set(v): _TextEdit.wrap_mode
 
+var fill_textBox: bool:
+	set(v): 
+		if v:
+			if vertical: _TextEdit.size_flags_vertical = 3
+			else: _TextEdit.size_flags_horizontal = 3
+		else:
+			if vertical: _TextEdit.size_flags_vertical = 1
+			else: _TextEdit.size_flags_horizontal = 1
+
 var _TextEdit: TextEdit_Sub
 
 func _ActivationSecondary(settings: Dictionary) -> void:
@@ -39,11 +50,12 @@ func _ActivationSecondary(settings: Dictionary) -> void:
 	editable = HelperFuncs.GetIfCan(settings, "editable", D_editable)
 	placeholder_text = HelperFuncs.GetIfCan(settings, "placeholder_text", D_placeholder_text)
 	_TextEdit.text = HelperFuncs.GetIfCan(settings, "value", D_value)
+	fill_textBox = HelperFuncs.GetIfCan(settings, "fill_textBox", D_fill_textBox)
 	_runtimeSettableProperties.merge(_specificSettableProps)
 
 func _PopulateSubElements() -> Array:
 	# used during Activation Primary to add Counter
-	return ["field"]
+	return ["textbox"]
 
 func _getChildData() -> Dictionary:
 	return {
