c07ffd2deffee698dbbc8630c4128848c627791f
kakcalu13 <kevin.a.araujo@gmail.com>
2023-07-06 13:28:10 -0400

    remove godot source

--- third_party/godot/godot_source/animation_teaser.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-func _on_pressed():
-	if $".."/Core/GlobalUISystem/Brain_Visualizer/Node3D/Camera3D.current:
-		$".."/AnimationPlayer/Camera3D.make_current()
-		$".."/AnimationPlayer.play("animation")
-	elif $".."/AnimationPlayer/Camera3D.current:
-		$".."/Core/GlobalUISystem/Brain_Visualizer/Node3D/Camera3D.make_current()
