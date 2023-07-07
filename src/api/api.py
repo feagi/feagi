6c0cfc9c3af8c41cdfdd97271de456866d7bc437
pradeeppathak9 <pradeeppathak9@gmail.com>
2023-07-07 19:15:30 +0530

    Port fixing (#341)
    * Changes for port sent from Feagi to agent
    
    * feagi port fixes bugs resolved
    
    * feagi port fixes bugs resolved
    
    * feagi port fixes bugs resolved
    
    * Changes for Dynamic Feagi Registration
    
    * port-fixes
    
    * changed composer url to feagi_auth_url
    
    ---------
    
    Co-authored-by: PAT <pat@PATs-MacBook-Air.local>
    Co-authored-by: Ubuntu <ubuntu@pat-instance.us-central1-a.c.feagi-poc.internal>
    Co-authored-by: Mohammad Nadji <26581495+m-nadji-tehrani@users.noreply.github.com>

--- src/api/api.py
+++ src/api/api.py
@@ -1853,8 +1853,9 @@ def assign_available_port():
 
 
 @app.api_route("/v1/agent/register", methods=['POST'], tags=["Peripheral Nervous System"])
-async def agent_registration(request: Request, agent_type: str, agent_id: str, agent_ip: str,
-                             agent_data_port: int, response: Response):
+async def agent_registration(request: Request, agent_type: str, agent_id: str, agent_ip: str, agent_data_port: int,
+                             response: Response):
+
     try:
         if agent_id in runtime_data.agent_registry:
             agent_info = runtime_data.agent_registry[agent_id]
