03435a34209dad596528531712e81aa0fd9239c5
kakcalu13 <kevin.a.araujo@gmail.com>
2023-07-29 15:05:30 -0400

    added aptr range and default

--- peripherals/feagi_agent_video_capture/feagi_agent_video_capture/configuration.py
+++ peripherals/feagi_agent_video_capture/feagi_agent_video_capture/configuration.py
@@ -42,11 +42,13 @@ capabilities = {
         "deviation_threshold": 0.5,
         "retina_width_percent": 90,
         "retina_height_percent": 70,
-        "central_vision_compression": [64,64],
+        "central_vision_compression": [64, 64],
         "peripheral_vision_compression": [8, 8],
         "previous_data": {},
         "video_device_index": 0,
-        "video_loop": False
+        "video_loop": False,
+        "aperture_range": [0.7, 2],
+        "aperture_default": 2
     }
 }
 
