d8ce58745d68b9ec0a6bc392d44724e548423e44
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-23 11:12:59 -0400

    move to embodiments, middleware, simulators, and visualizers

--- third_party/ros/Micro-ROS_DEPRECATED/update.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~/ros2_ws/
-sudo rm -R ~/ros2_ws/install/ ~/ros2_ws/build/ ~/ros2_ws/log/
-colcon build --packages-select py_topic
--- third_party/ros/serial/sonar_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source /opt/ros/foxy/setup.bash
-mypath=`pwd`
-cd ~/ros2_ws/
-cp $mypath/ros_laser_scan.py $mypath/ros_teleop.py $mypath/HC_SR04_Foxy.py $mypath/py2arduino.py ~/ros2_ws/src/py_topic/py_topic/
-cp $mypath/setup.py ~/ros2_ws/src/py_topic/
-colcon build
-source ~/ros2_ws/install/setup.bash
-echo "completed"
