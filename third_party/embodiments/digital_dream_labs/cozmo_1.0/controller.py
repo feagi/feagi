87fdfad599cd6425d849935c25e50fd41237b0f9
kakcalu13 <kevin.a.araujo@gmail.com>
2023-09-25 17:09:03 -0400

    added main function

--- third_party/embodiments/digital_dream_labs/cozmo_1.0/controller.py
+++ third_party/embodiments/digital_dream_labs/cozmo_1.0/controller.py
@@ -206,139 +206,140 @@ def action(obtained_data, device_list, feagi_settings, head_angle, arms_angle):
                     face_selected.append(i)
     return arms_angle, head_angle
 
+if __name__ == '__main__':
+    # # FEAGI REACHABLE CHECKER # #
+    feagi_flag = False
+    print("retrying...")
+    print("Waiting on FEAGI...")
+    while not feagi_flag:
+        feagi_flag = FEAGI.is_FEAGI_reachable(
+            os.environ.get('FEAGI_HOST_INTERNAL', feagi_settings["feagi_host"]),
+            int(os.environ.get('FEAGI_OPU_PORT', "3000")))
+        sleep(2)
 
-# # FEAGI REACHABLE CHECKER # #
-feagi_flag = False
-print("retrying...")
-print("Waiting on FEAGI...")
-while not feagi_flag:
-    feagi_flag = FEAGI.is_FEAGI_reachable(
-        os.environ.get('FEAGI_HOST_INTERNAL', feagi_settings["feagi_host"]),
-        int(os.environ.get('FEAGI_OPU_PORT', "3000")))
-    sleep(2)
+    # # FEAGI REACHABLE CHECKER COMPLETED # #
 
-# # FEAGI REACHABLE CHECKER COMPLETED # #
+    # # # FEAGI registration # # # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # - - - - - - - - - - - - - - - - - - #
+    feagi_settings, runtime_data, api_address, feagi_ipu_channel, feagi_opu_channel = \
+        FEAGI.connect_to_feagi(feagi_settings,runtime_data, agent_settings, capabilities)
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    face_selected = deque()
+    threading.Thread(target=face_starter, daemon=True).start()
+    msg_counter = 0
+    rgb = {'camera': {}}
+    genome_tracker = 0
+    capabilities['camera']['current_select'] = []
+    get_size_for_aptr_cortical = api_address + '/v1/FEAGI/genome/cortical_area?cortical_area=o_aptr'
+    raw_aptr = requests.get(get_size_for_aptr_cortical).json()
+    aptr_cortical_size = pns.fetch_aptr_size(10, raw_aptr, None)
+    # Raise head.
+    cli = pycozmo.Client()
+    cli.start()
+    cli.connect()
+    cli.wait_for_robot()
+    print("max in rad: ", pycozmo.robot.MAX_HEAD_ANGLE.radians)  # 0.7766715171374767
+    print("min in rad: ", pycozmo.robot.MIN_HEAD_ANGLE.radians)  # -0.4363323129985824
+    max = pycozmo.robot.MAX_HEAD_ANGLE.radians - 0.1
+    min = pycozmo.robot.MIN_HEAD_ANGLE.radians + 0.1
+    max_lift = pycozmo.MAX_LIFT_HEIGHT.mm - 5
+    min_lift = pycozmo.MIN_LIFT_HEIGHT.mm + 5
+    angle_of_head = (pycozmo.robot.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians) / 2.0
+    angle_of_arms = 50  # TODO: How to obtain the arms encoders in real time
+    cli.set_head_angle(angle_of_head)  # move head
+    lwheel_speed = 0  # Speed in millimeters per second for the left wheel
+    rwheel_speed = 0  # Speed in millimeters per second for the right wheel
+    lwheel_acc = 0  # Acceleration in millimeters per second squared for the left wheel
+    rwheel_acc = 0  # Acceleration in millimeters per second squared for the right wheel
+    duration = 0  # Duration in seconds for how long to drive the wheels
 
-# # # FEAGI registration # # # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# - - - - - - - - - - - - - - - - - - #
-feagi_settings, runtime_data, api_address, feagi_ipu_channel, feagi_opu_channel = \
-    FEAGI.connect_to_feagi(feagi_settings,runtime_data, agent_settings, capabilities)
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-face_selected = deque()
-threading.Thread(target=face_starter, daemon=True).start()
-msg_counter = 0
-rgb = {'camera': {}}
-genome_tracker = 0
-capabilities['camera']['current_select'] = []
-get_size_for_aptr_cortical = api_address + '/v1/FEAGI/genome/cortical_area?cortical_area=o_aptr'
-raw_aptr = requests.get(get_size_for_aptr_cortical).json()
-aptr_cortical_size = pns.fetch_aptr_size(10, raw_aptr, None)
-# Raise head.
-cli = pycozmo.Client()
-cli.start()
-cli.connect()
-cli.wait_for_robot()
-print("max in rad: ", pycozmo.robot.MAX_HEAD_ANGLE.radians)  # 0.7766715171374767
-print("min in rad: ", pycozmo.robot.MIN_HEAD_ANGLE.radians)  # -0.4363323129985824
-max = pycozmo.robot.MAX_HEAD_ANGLE.radians - 0.1
-min = pycozmo.robot.MIN_HEAD_ANGLE.radians + 0.1
-max_lift = pycozmo.MAX_LIFT_HEIGHT.mm - 5
-min_lift = pycozmo.MIN_LIFT_HEIGHT.mm + 5
-angle_of_head = (pycozmo.robot.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians) / 2.0
-angle_of_arms = 50  # TODO: How to obtain the arms encoders in real time
-cli.set_head_angle(angle_of_head)  # move head
-lwheel_speed = 0  # Speed in millimeters per second for the left wheel
-rwheel_speed = 0  # Speed in millimeters per second for the right wheel
-lwheel_acc = 0  # Acceleration in millimeters per second squared for the left wheel
-rwheel_acc = 0  # Acceleration in millimeters per second squared for the right wheel
-duration = 0  # Duration in seconds for how long to drive the wheels
+    # vision capture
+    cli.enable_camera(enable=True, color=True)
 
-# vision capture
-cli.enable_camera(enable=True, color=True)
-
-cli.add_handler(pycozmo.event.EvtNewRawCameraImage, on_camera_image)
-cli.add_handler(pycozmo.protocol_encoder.RobotState, on_robot_state)
-time.sleep(2)
-# vision ends
-device_list = pns.generate_OPU_list(capabilities)  # get the OPU sensors
-while True:
-    try:
-        start = time.time()
-        message_from_feagi = pns.efferent_signaling(feagi_opu_channel)
-        if message_from_feagi is not None:
-            # Obtain the size of aptr
-            if aptr_cortical_size is None:
-                aptr_cortical_size = pns.check_aptr(raw_aptr)
-            # Update the aptr
-            capabilities = pns.fetch_aperture_data(message_from_feagi, capabilities,
-                                                   aptr_cortical_size)
-            # Update the ISO
-            capabilities = pns.fetch_iso_data(message_from_feagi, capabilities, aptr_cortical_size)
-            # OPU section STARTS
-            obtained_signals = pns.obtain_opu_data(device_list, message_from_feagi)
-            angle_of_arms, angle_of_head = action(obtained_signals, device_list, feagi_settings,
-                                                  angle_of_arms, angle_of_head)
-            # OPU section ENDS
-        new_rgb = rgb_array['current']
-        previous_data_frame, rgb['camera'], capabilities['camera']['current_select'] = \
-            pns.generate_rgb(new_rgb,
-                             capabilities['camera']['central_vision_allocation_percentage'][0],
-                             capabilities['camera']['central_vision_allocation_percentage'][1],
-                             capabilities['camera']["central_vision_resolution"],
-                             capabilities['camera']['peripheral_vision_resolution'],
-                             previous_data_frame,
-                             capabilities['camera']['current_select'],
-                             capabilities['camera']['iso_default'],
-                             capabilities['camera']["aperture_default"])
-        battery = robot['battery']
+    cli.add_handler(pycozmo.event.EvtNewRawCameraImage, on_camera_image)
+    cli.add_handler(pycozmo.protocol_encoder.RobotState, on_robot_state)
+    time.sleep(2)
+    # vision ends
+    device_list = pns.generate_OPU_list(capabilities)  # get the OPU sensors
+    while True:
         try:
-            ultrasonic_data = robot['ultrasonic'][0]
-            if ultrasonic_data:
-                formatted_ultrasonic_data = {
-                    'ultrasonic': {
-                        sensor: data for sensor, data in enumerate([ultrasonic_data])
+            start = time.time()
+            message_from_feagi = pns.efferent_signaling(feagi_opu_channel)
+            if message_from_feagi is not None:
+                # Obtain the size of aptr
+                if aptr_cortical_size is None:
+                    aptr_cortical_size = pns.check_aptr(raw_aptr)
+                # Update the aptr
+                capabilities = pns.fetch_aperture_data(message_from_feagi, capabilities,
+                                                       aptr_cortical_size)
+                # Update the ISO
+                capabilities = pns.fetch_iso_data(message_from_feagi, capabilities, aptr_cortical_size)
+                # OPU section STARTS
+                obtained_signals = pns.obtain_opu_data(device_list, message_from_feagi)
+                angle_of_arms, angle_of_head = action(obtained_signals, device_list, feagi_settings,
+                                                      angle_of_arms, angle_of_head)
+                # OPU section ENDS
+            new_rgb = rgb_array['current']
+            previous_data_frame, rgb['camera'], capabilities['camera']['current_select'] = \
+                pns.generate_rgb(new_rgb,
+                                 capabilities['camera']['central_vision_allocation_percentage'][0],
+                                 capabilities['camera']['central_vision_allocation_percentage'][1],
+                                 capabilities['camera']["central_vision_resolution"],
+                                 capabilities['camera']['peripheral_vision_resolution'],
+                                 previous_data_frame,
+                                 capabilities['camera']['current_select'],
+                                 capabilities['camera']['iso_default'],
+                                 capabilities['camera']["aperture_default"])
+            battery = robot['battery']
+            try:
+                ultrasonic_data = robot['ultrasonic'][0]
+                if ultrasonic_data:
+                    formatted_ultrasonic_data = {
+                        'ultrasonic': {
+                            sensor: data for sensor, data in enumerate([ultrasonic_data])
+                        }
                     }
-                }
-            else:
-                formatted_ultrasonic_data = {}
-            message_to_feagi, battery = FEAGI.compose_message_to_feagi(
-                original_message={**formatted_ultrasonic_data}, battery=battery)
-        except Exception as ERROR:
-            print("ERROR IN ULTRASONIC: ", ERROR)
-            ultrasonic_data = 0
-        try:
-            if "data" not in message_to_feagi:
-                message_to_feagi["data"] = {}
-            if "sensory_data" not in message_to_feagi["data"]:
-                message_to_feagi["data"]["sensory_data"] = {}
-            message_to_feagi["data"]["sensory_data"]['camera'] = rgb['camera']
+                else:
+                    formatted_ultrasonic_data = {}
+                message_to_feagi, battery = FEAGI.compose_message_to_feagi(
+                    original_message={**formatted_ultrasonic_data}, battery=battery)
+            except Exception as ERROR:
+                print("ERROR IN ULTRASONIC: ", ERROR)
+                ultrasonic_data = 0
+            try:
+                if "data" not in message_to_feagi:
+                    message_to_feagi["data"] = {}
+                if "sensory_data" not in message_to_feagi["data"]:
+                    message_to_feagi["data"]["sensory_data"] = {}
+                message_to_feagi["data"]["sensory_data"]['camera'] = rgb['camera']
+            except Exception as e:
+                print("error: ", e)
+            # Add accelerator section
+            try:
+                runtime_data['accelerator']['0'] = robot['accelerator'][0]
+                runtime_data['accelerator']['1'] = robot['accelerator'][1]
+                runtime_data['accelerator']['2'] = robot['accelerator'][2]
+                if "data" not in message_to_feagi:
+                    message_to_feagi["data"] = {}
+                if "sensory_data" not in message_to_feagi["data"]:
+                    message_to_feagi["data"]["sensory_data"] = {}
+                message_to_feagi["data"]["sensory_data"]['accelerator'] = runtime_data['accelerator']
+            except Exception as ERROR:
+                message_to_feagi["data"]["sensory_data"]['accelerator'] = {}
+            # End accelerator section
+
+            message_to_feagi['timestamp'] = datetime.now()
+            message_to_feagi['counter'] = msg_counter
+            if message_from_feagi is not None:
+                feagi_settings['feagi_burst_speed'] = message_from_feagi['burst_frequency']
+            sleep(feagi_settings['feagi_burst_speed'])
+
+            pns.afferent_signaling(message_to_feagi, feagi_ipu_channel, agent_settings)
+            message_to_feagi.clear()
+            for i in rgb['camera']:
+                rgb['camera'][i].clear()
         except Exception as e:
-            print("error: ", e)
-        # Add accelerator section
-        try:
-            runtime_data['accelerator']['0'] = robot['accelerator'][0]
-            runtime_data['accelerator']['1'] = robot['accelerator'][1]
-            runtime_data['accelerator']['2'] = robot['accelerator'][2]
-            if "data" not in message_to_feagi:
-                message_to_feagi["data"] = {}
-            if "sensory_data" not in message_to_feagi["data"]:
-                message_to_feagi["data"]["sensory_data"] = {}
-            message_to_feagi["data"]["sensory_data"]['accelerator'] = runtime_data['accelerator']
-        except Exception as ERROR:
-            message_to_feagi["data"]["sensory_data"]['accelerator'] = {}
-        # End accelerator section
+            print("ERROR: ", e)
+            traceback.print_exc()
+            break
 
-        message_to_feagi['timestamp'] = datetime.now()
-        message_to_feagi['counter'] = msg_counter
-        if message_from_feagi is not None:
-            feagi_settings['feagi_burst_speed'] = message_from_feagi['burst_frequency']
-        sleep(feagi_settings['feagi_burst_speed'])
-
-        pns.afferent_signaling(message_to_feagi, feagi_ipu_channel, agent_settings)
-        message_to_feagi.clear()
-        for i in rgb['camera']:
-            rgb['camera'][i].clear()
-    except Exception as e:
-        print("ERROR: ", e)
-        traceback.print_exc()
-        break
