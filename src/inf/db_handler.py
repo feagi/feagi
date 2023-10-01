4404a1c4b3314c4c70e90a5a5c8ee708417b5560
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2023-09-30 22:29:07 -0400

    Reset game stat API

--- src/inf/db_handler.py
+++ src/inf/db_handler.py
@@ -18,10 +18,15 @@ import time
 import logging
 import random
 import traceback
+from datetime import datetime, timedelta
 
 from pymongo.errors import ServerSelectionTimeoutError
 from pymongo import MongoClient, DESCENDING, ASCENDING
 from inf import runtime_data, settings
+
+import influxdb_client
+from influxdb_client import InfluxDBClient, Point, WriteOptions, Bucket
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 
 logger = logging.getLogger(__name__)
@@ -176,10 +181,6 @@ class MongoManagement:
 
 class InfluxManagement:
     def __init__(self):
-        import influxdb_client
-        from influxdb_client import InfluxDBClient, Point
-        from influxdb_client.client.write_api import SYNCHRONOUS
-
         self.db_params = runtime_data.parameters['Database']
 
         if runtime_data.parameters:
@@ -188,6 +189,16 @@ class InfluxManagement:
             self.game_stats_bucket = self.db_params["influxdb_game_stats_bucket"]
             self.org = self.db_params["influxdb_organization"]
             self.token = self.db_params["influxdb_token"]
+
+            # self.write_options = WriteOptions(
+            #     batch_size=1000,  # Write every 1000 points
+            #     flush_interval=10_000,  # Flush every 10 seconds
+            #     jitter_interval=2_000,  # Jitter interval of 2 seconds
+            #     retry_interval=5_000,  # Retry every 5 seconds
+            #     max_retries=5,  # Maximum of 5 retries
+            #     max_retry_delay=30_000,  # Maximum retry delay of 30 seconds
+            #     exponential_base=2  # Exponential base of 2 for backoff strategy
+            # )
 
             # todo: db address needs to be def from a config file instead
             print('Running in container: ', runtime_data.running_in_container)
@@ -245,6 +256,36 @@ class InfluxManagement:
         # except Exception as e:
         #     print(settings.Bcolors.RED +
         #           "ERROR: Cannot connect to << InfluxDb >> Database \n ::: %s" % str(repr(e)) + settings.Bcolors.ENDC)
+
+    def create_new_bucket(self, bucket_name):
+        try:
+            # Create a new bucket with the same name
+            print("Creating a new bucket called ", bucket_name)
+
+            self.client.buckets_api().create_bucket(bucket_name=bucket_name, org_id=self.org)
+            print("@#@#@#@#@#@   @#@#@#@#@#    @# @#@#@ #@# @#  @# @# @ #@#")
+        except Exception as e:
+            print("Exception:", e, traceback.print_exc())
+
+    def delete_bucket_content(self, bucket_name):
+        # Perform the delete operation
+        try:
+            # Calculate the start date as 30 days ago from now
+            start_date = datetime.utcnow() - timedelta(days=30)
+            # Format it in the RFC3339Nano format
+            start_str = start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+            # Get the current time in the RFC3339Nano format
+            stop_str = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+            predicate = "_measurement=\"game_result\""
+            self.client.delete_api().delete(start=start_str, stop=stop_str,
+                                            bucket=bucket_name, org=self.org,
+                                            predicate=predicate)
+            print("Data deleted successfully.")
+            print("- " * 100)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def insert_neuron_activity(self, connectome_path, src_cortical_area, src_neuron_id,
                                dst_voxel_x, dst_voxel_y, dst_voxel_z, membrane_potential, timestamp=None):
@@ -430,9 +471,17 @@ class InfluxManagement:
 
     def drop_game_activity(self):
         # Get the list of buckets
-        bucket = self.client.buckets_api().find_bucket_by_name(bucket_name="data")
-        print("bucket:", bucket)
-        self.client.buckets_api().delete_bucket(bucket=bucket["id"])
+        print("@@@ @   @ @ @ @  @ @\n" * 20)
+        self.delete_bucket_content(bucket_name=self.game_stats_bucket)
+        # self.create_new_bucket(bucket_name=self.game_stats_bucket)
+
+        # # Create a new bucket with the same name
+        # new_bucket = self.client.buckets_api().create_bucket(bucket_name=bucket.name, org_id=self.org)
+        # # new_bucket = self.client.buckets_api().create_bucket(Bucket=bucket)
+        # self.client.buckets_api().create_bucket(bucket=new_bucket)
+        #
+        # # Get the Write API object
+        # self.write_client = self.client.write_api()
 
     def test_influxdb(self):
         try:
