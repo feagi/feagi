2f94e61dd52dd937e4fe71b5ce089c4bb80cc3db
kakcalu13 <kevin.a.araujo@gmail.com>
2023-11-27 17:26:12 -0500

    push latest

--- peripherals/feagi_agent_core/feagi_agent/retina.py
+++ peripherals/feagi_agent_core/feagi_agent/retina.py
@@ -53,3 +53,46 @@ def vision_frame_capture(device, RGB_flag=True):
         return frame, datetime.now()
     else:
         return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), datetime.now()
+
+
+def vision_region_coordinates(frame_width, frame_height, x1, x2, y1, y2):
+    prime_x1 = int(frame_width * (x1 / 100))
+    prime_x2 = int((frame_width - prime_x1) * (x2 / 100))
+    prime_x3 = int(frame_width - (prime_x1 + prime_x2))
+    prime_y1 = int(frame_height * (y1 / 100))
+    prime_y2 = int((frame_height - prime_y1) * (y2 / 100))
+    prime_y3 = int(frame_height - (prime_x1 + prime_x2))
+
+    vision = dict()
+    vision['TL'] = [prime_x1, 0, prime_y1, 0]
+    vision['TM'] = [0, prime_x2, prime_y1, 0]
+    vision['TR'] = [prime_x3, prime_x2, prime_y1, 0]
+    vision['ML'] = [prime_x1, 0, prime_y2, 0]
+    vision['C'] = [0, prime_x2, prime_y2, 0]
+    vision['MR'] = [prime_x3, prime_x2, prime_y2, 0]
+    vision['LL'] = [prime_x1, prime_y3, 0]
+    vision['LM'] = [0, prime_x2, prime_y3, 0]
+    vision['LR'] = [prime_x2, prime_x3, prime_y3, 0]
+    return vision
+
+
+def split_vision_regions(vision, frame):
+    test = dict()
+    test['TL'] = frame[0:vision['TL'][0], 0:(vision['TL'][2] + vision['TL'][3])]
+    test['TM'] = frame[vision['TL'][0]:vision['TM'][1], 0:vision['TM'][2]+vision['TM'][3]]
+    return test
+
+cam = get_device_of_vision(2)
+while True:
+    pixels, time = vision_frame_capture(cam)
+    vision_dict = vision_region_coordinates(1000, 600, 10, 20, 40, 60)
+    data = split_vision_regions(vision_dict, pixels)
+    print(data)
+    for i in data:
+        cv2.imshow(i, data[i])
+    cv2.imshow("full", pixels)
+    # # for i in vision_dict:
+    # #     print(vision_dict)
+    #     # cv2.imshow(i, vision_dict[i])
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
