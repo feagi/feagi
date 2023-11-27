8a8a56ca7937796480eddbb751536e617bceddb4
kakcalu13 <kevin.a.araujo@gmail.com>
2023-11-27 13:01:58 -0500

    added docstring

--- peripherals/feagi_agent_core/feagi_agent/retina.py
+++ peripherals/feagi_agent_core/feagi_agent/retina.py
@@ -21,10 +21,33 @@ import traceback
 
 
 def get_device_of_vision(device):
+    """
+    Obtain the camera source and bind it using the provided address.
+
+    Args:
+    - device: The path to the file, video, or webcam. Webcam should be an integer number.
+
+    Returns:
+    - An address corresponding to the webcam source, enabling its use across different files.
+    """
     return cv2.VideoCapture(device)
 
 
 def vision_frame_capture(device, RGB_flag=True):
+    """
+    Capture frames from the specified `device`, which represents the camera source.
+
+    Args:
+    - device: The camera device obtained using the `get_device_of_vision()` function.
+    - RGB_flag: A boolean indicating whether to retrieve data in RGB format (default: True).
+      If set to False, the function returns grayscale data.
+
+    Returns:
+    - An nd.array representing the captured frame data. For RGB, it contains three dimensions;
+      for grayscale, it displays a single dimension.
+      Example format: [[x, y, z], [x, y, z]].
+    """
+
     check, frame = device.read()  # 0 is the default
     if RGB_flag:
         return frame
