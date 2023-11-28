56ee544e1136689990cb1092ee2ca35a41882173
kakcalu13 <kevin.a.araujo@gmail.com>
2023-11-28 10:09:28 -0500

    WIP, completed downsize

--- peripherals/feagi_agent_core/feagi_agent/retina.py
+++ peripherals/feagi_agent_core/feagi_agent/retina.py
@@ -18,6 +18,7 @@ limitations under the License.
 
 import cv2
 import traceback
+import requests
 from datetime import datetime
 
 
@@ -56,6 +57,28 @@ def vision_frame_capture(device, RGB_flag=True):
 
 
 def vision_region_coordinates(frame_width, frame_height, x1, x2, y1, y2):
+    """
+    Calculate coordinates for nine different regions within a frame based on given percentages.
+
+    This function computes the coordinates for nine regions within a frame, defined by x1, x2,
+    y1, and y2 percentages. These percentages indicate the position of a point within the frame's
+    width and height.
+
+    Inputs:
+    - frame_width: Integer, width of the frame.
+    - frame_height: Integer, height of the frame.
+    - x1, x2, y1, y2: integers representing percentages (0 to 100) along x-axis
+    and y-axis.
+                      For example, x1=50, y1=40 corresponds to 50% and 40%.
+
+    Output:
+    - region_coordinates: Dictionary containing coordinates for nine different regions:
+                          'TL', 'TM', 'TR', 'ML', '_C', 'MR', 'LL', 'LM', 'LR'.
+                          Each region has its respective coordinates within the frame.
+
+    Note: Make sure that x1, x2, y1, and y2 are valid percentage values within the range of 0 to
+    100.
+    """
     x1_prime = int(frame_width * (x1 / 100))
     x2_prime = int((frame_width * (x2 / 100) + x1_prime))
     y1_prime = int(frame_height * (y1 / 100))
@@ -75,24 +98,85 @@ def vision_region_coordinates(frame_width, frame_height, x1, x2, y1, y2):
 
 
 def split_vision_regions(coordinates, raw_frame_data):
+    """
+    Split a frame into separate regions based on provided coordinates.
+
+    This function takes the output coordinates from the 'vision_region_coordinates()' function
+    and the raw frame data, then splits the frame into nine distinct regions according to those
+    coordinates.
+
+    Inputs:
+    - coordinates: Dictionary containing the coordinates for nine regions, usually obtained
+                   from the 'vision_region_coordinates()' function.
+    - raw_frame_data: The original frame data or image used for splitting into regions.
+
+    Output:
+    - Display: Visual representation or display of all nine regions independently within the frame.
+    """
+
     frame_segments = dict()
     for region in coordinates:
         frame_segments[region] = raw_frame_data[coordinates[region][1]:coordinates[region][3],
-                                                coordinates[region][0]:coordinates[region][2]]
+                                 coordinates[region][0]:coordinates[region][2]]
     return frame_segments
 
 
-cam = get_device_of_vision(0)
+def downsize_regions(frame, resize, RGB_flag=True):
+    """
+    Downsize regions within a frame using specified width and height for compression.
+
+    This function utilizes the resize parameter to compress regions within a frame obtained from
+     FEAGI's API.
+    The frame should be represented as a NumPy ndarray.
+
+    Inputs:
+    - frame: NumPy ndarray representing the image/frame data.
+    - resize: Tuple containing width and height values for compression.
+              Example: (8, 8), (64, 64), (64, 32)
+
+    Output:
+    - compressed_dict: Dictionary containing compressed data for nine regions.
+                       Each region will be represented within the compressed_dict.
+
+    Make sure that the 'frame' input is a valid NumPy ndarray and the 'resize' parameter contains
+    appropriate width and height values for compression.
+    """
+    if RGB_flag:
+        compressed_dict = cv2.resize(frame, resize, interpolation=cv2.INTER_AREA)
+    else:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        compressed_dict = cv2.resize(frame, resize, interpolation=cv2.INTER_AREA)
+    return compressed_dict
+
+
+cam = get_device_of_vision(2)
+url = 'http://127.0.0.1:8000/v1/feagi/genome/cortical_area/geometry'
+response = requests.get(url)
+data = response.json()
+items = ["_C", "LL", "LM", "LR", "MR", "ML", "TR", "TL", "TM"]
+resize_list = {}
+for i in data:
+    for x in items:
+        if x in i:
+            dimension_array = data[i]["dimensions"][0], data[i]["dimensions"][1]
+            resize_list[x] = dimension_array
 while True:
     raw_frame, time = vision_frame_capture(cam)
     region_coordinates = vision_region_coordinates(frame_width=raw_frame.shape[1],
                                                    frame_height=raw_frame.shape[0],
                                                    x1=25, x2=50,
                                                    y1=25, y2=50)
-    segmented_frame_data = split_vision_regions(coordinates=region_coordinates, raw_frame_data=raw_frame)
-    for segment in segmented_frame_data:
-        cv2.imshow(segment, segmented_frame_data[segment])
-    cv2.imshow("full", raw_frame)
+    segmented_frame_data = split_vision_regions(coordinates=region_coordinates,
+                                                raw_frame_data=raw_frame)
+    compressed_data = dict()
+    for i in segmented_frame_data:
+        if "_C" in i:
+            compressed_data[i] = downsize_regions(segmented_frame_data[i], resize_list[i])
+        else:
+            compressed_data[i] = downsize_regions(segmented_frame_data[i], resize_list[i], False)
+    for segment in compressed_data:
+        cv2.imshow(segment, compressed_data[segment])
+    # cv2.imshow("full", raw_frame)
     # # for i in vision_dict:
     # #     print(vision_dict)
     #     # cv2.imshow(i, vision_dict[i])
