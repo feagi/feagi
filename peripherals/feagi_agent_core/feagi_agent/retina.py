84251a30d372b1853f2c0188db87b5f7d4ae7e04
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2023-11-27 23:02:30 -0500

    Fixed flipped image Cleaned up variable names

--- peripherals/feagi_agent_core/feagi_agent/retina.py
+++ peripherals/feagi_agent_core/feagi_agent/retina.py
@@ -56,45 +56,43 @@ def vision_frame_capture(device, RGB_flag=True):
 
 
 def vision_region_coordinates(frame_width, frame_height, x1, x2, y1, y2):
-    prime_x1 = int(frame_width * (x1 / 100))
-    prime_x2 = int((frame_width * (x2 / 100) + prime_x1))
-    prime_y1 = int(frame_height * (y1 / 100))
-    prime_y2 = int((frame_height * y2 / 100) + prime_y1)
+    x1_prime = int(frame_width * (x1 / 100))
+    x2_prime = int((frame_width * (x2 / 100) + x1_prime))
+    y1_prime = int(frame_height * (y1 / 100))
+    y2_prime = int((frame_height * y2 / 100) + y1_prime)
+
+    region_coordinates = dict()
+    region_coordinates['TL'] = [0, 0, x1_prime, y1_prime]
+    region_coordinates['TM'] = [x1_prime, 0, x2_prime, y1_prime]
+    region_coordinates['TR'] = [x2_prime, 0, frame_width, y1_prime]
+    region_coordinates['ML'] = [0, y1_prime, x1_prime, y2_prime]
+    region_coordinates['_C'] = [x1_prime, y1_prime, x2_prime, y2_prime]
+    region_coordinates['MR'] = [x2_prime, y1_prime, frame_width, y2_prime]
+    region_coordinates['LL'] = [0, y2_prime, x1_prime, frame_height]
+    region_coordinates['LM'] = [x1_prime, y2_prime, x2_prime, frame_height]
+    region_coordinates['LR'] = [x2_prime, y2_prime, frame_width, frame_height]
+    return region_coordinates
 
 
-    vision = dict()
-    vision['TL'] = [0, 0, prime_x1, prime_y1]
-    vision['TM'] = [prime_x1, 0, prime_x2, prime_y1]
-    vision['TR'] = [prime_x2, 0, frame_width, prime_y1]
-    vision['ML'] = [0, prime_y1, prime_x1, prime_y2]
-    vision['C'] = [prime_x1, prime_y1, prime_x2, prime_y2]
-    vision['MR'] = [prime_x2, prime_y1, frame_width, prime_y2]
-    vision['LL'] = [0, prime_y2, prime_x1, frame_height]
-    vision['LM'] = [prime_x1, prime_y2, prime_x2, frame_height]
-    vision['LR'] = [prime_x2, prime_y2, frame_width, frame_height]
-    return vision
-
-
-def split_vision_regions(vision, frame):
-    test = dict()
-    # area = "TM"
-    print(vision)
-    for area in vision:
-        print("NAME: ", area)
-        test[area] = frame[vision[area][0]:vision[area][2], vision[area][1]:vision[area][3]]
-    return test
+def split_vision_regions(coordinates, raw_frame_data):
+    frame_segments = dict()
+    for region in coordinates:
+        frame_segments[region] = raw_frame_data[coordinates[region][1]:coordinates[region][3],
+                                                coordinates[region][0]:coordinates[region][2]]
+    return frame_segments
 
 
 cam = get_device_of_vision(0)
 while True:
-    pixels, time = vision_frame_capture(cam)
-    print("shape: ", pixels.shape)
-    vision_dict = vision_region_coordinates(pixels.shape[0], pixels.shape[1], 10, 20, 20, 40)
-    data = split_vision_regions(vision_dict, pixels)
-    for i in data:
-        print("AREA: ", i)
-        cv2.imshow(i, data[i])
-    cv2.imshow("full", pixels)
+    raw_frame, time = vision_frame_capture(cam)
+    region_coordinates = vision_region_coordinates(frame_width=raw_frame.shape[1],
+                                                   frame_height=raw_frame.shape[0],
+                                                   x1=10, x2=10,
+                                                   y1=20, y2=40)
+    segmented_frame_data = split_vision_regions(coordinates=region_coordinates, raw_frame_data=raw_frame)
+    for segment in segmented_frame_data:
+        cv2.imshow(segment, segmented_frame_data[segment])
+    cv2.imshow("full", raw_frame)
     # # for i in vision_dict:
     # #     print(vision_dict)
     #     # cv2.imshow(i, vision_dict[i])
