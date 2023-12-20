ce943a50cd043fc42aec21d94c2e0c1a487c618e
kakcalu13 <kevin.a.araujo@gmail.com>
2023-12-20 00:48:59 -0500

    added serial features into actuators

--- peripherals/feagi_agent_core/feagi_agent/actuators.py
+++ peripherals/feagi_agent_core/feagi_agent/actuators.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from feagi_agent import feagi_interface as feagi
+import serial
+import json
 
 
 def obtain_opu_data(device_list, message_from_feagi):
@@ -12,3 +14,19 @@ def obtain_opu_data(device_list, message_from_feagi):
                     opu_signal_dict[i] = {}
                 opu_signal_dict[i][x] = opu_data[i][x]
     return opu_signal_dict
+
+def initialization_port (port):
+    return serial.Serial(port,
+                         baudrate=9600,
+                         timeout=2.5,
+                         parity=serial.PARITY_NONE,
+                         bytesize=serial.EIGHTBITS,
+                         stopbits=serial.STOPBITS_ONE)
+
+
+def convert_dict_to_json(data):
+    data = json.dumps(data)
+    return data
+
+def send_serial(ser, data):
+    ser.write(data.encode('ascii'))
