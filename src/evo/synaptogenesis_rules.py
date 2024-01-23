7dd35bbe5f0f3f3e2164450c0ca60f70ca8c20f3
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2024-01-23 14:07:57 -0500

    Mem mapping fix

--- src/evo/synaptogenesis_rules.py
+++ src/evo/synaptogenesis_rules.py
@@ -149,7 +149,8 @@ def neighbor_finder(cortical_area_src, cortical_area_dst, src_neuron_id, morphol
                     # candidate_voxel_list.append([candidate, post_synaptic_current])
 
             elif neuron_morphology == "memory":
-                print("%$___" * 20)
+                print("%$___" * 2000)
+                print(cortical_area_src, cortical_area_dst)
                 syn_memory(src_cortical_area=cortical_area_src, dst_cortical_area=cortical_area_dst)
 
             candidate_list = None
@@ -413,5 +414,6 @@ def syn_projector(src_cortical_area, dst_cortical_area, src_neuron_id, src_subre
 
 
 def syn_memory(src_cortical_area, dst_cortical_area):
-    if dst_cortical_area in runtime_data.memory_register:
-        runtime_data.memory_register[dst_cortical_area].add(src_cortical_area)
+    if dst_cortical_area not in runtime_data.memory_register:
+        runtime_data.memory_register[dst_cortical_area] = set()
+    runtime_data.memory_register[dst_cortical_area].add(src_cortical_area)
