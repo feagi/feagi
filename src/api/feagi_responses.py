ee9f4b497112b6cad0137fefd25746983d944d98
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2024-02-12 15:27:47 -0500

    Structured error messages

---
+++ src/api/feagi_responses.py
@@ -0,0 +1,26 @@
+response_list = {
+    "CORTICAL_AREA_NOT_FOUND": {
+        "type": "error"
+    },
+    "CORTICAL_AREA_INVALID_ID_LENGTH": {
+        "type": "error"
+    },
+    "CORTICAL_AREA_UNDERGOING_TRANSFORMATION": {
+        "type": "error"
+    },
+    "CORTICAL_AREA_CREATION_FAILED": {
+        "type": "error"
+    },
+    "CORTICAL_AREA_DELETION_FAILED": {
+        "type": "error"
+    },
+    "CORTICAL_AREA_DELETION_PROHIBITED": {
+        "type": "error"
+    },
+    "CORTICAL_AREA_CREATED": {
+        "type": "error"
+    },
+    "CORTICAL_AREA_PROPERTIES_UPDATED": {
+        "type": "info"
+    }
+  }
