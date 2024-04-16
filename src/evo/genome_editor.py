6c3bd9e8314f5b625c9667f2d169392a33c7d50e
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2024-04-16 10:22:49 -0400

    Debug

--- src/evo/genome_editor.py
+++ src/evo/genome_editor.py
@@ -32,15 +32,6 @@ import json
 logger = logging.getLogger(__name__)
 
 
-class Sub:
-    def __init__(self, id, type):
-        self.id = id
-        self.type = type
-
-    def to_dict(self):
-        return {'id': self.id, 'type': self.type}
-
-
 def set_default(obj):
     if isinstance(obj, set):
         return list(obj)
