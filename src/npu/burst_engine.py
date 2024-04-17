2162f05ade6d30d48b686ad3c28ae0f5b4c55abc
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2024-04-17 11:38:04 -0400

    Connected agents

--- src/npu/burst_engine.py
+++ src/npu/burst_engine.py
@@ -598,6 +598,14 @@ def burst_manager():
                 for neuron in runtime_data.brain["___pwr"]:
                     runtime_data.fire_candidate_list["___pwr"].add(neuron)
 
+            # Reset connected agent list
+            runtime_data.connected_agents = {
+                "media_capture_ctrl": False,
+                "bluetooth_ctrl": False,
+                "godot_game_ctrl": False,
+                "zmq_to_ws_ctrl": False
+            }
+
             # Manage ZMQ communication from and to FEAGI
             pns_manager()
 
@@ -659,14 +667,6 @@ def burst_manager():
 
         # Burst stats
         burst_stats(burst_start_time)
-
-        # Clear connected agent list
-        runtime_data.connected_agents = {
-                "media_capture_ctrl": False,
-                "bluetooth_ctrl": False,
-                "godot_game_ctrl": False,
-                "zmq_to_ws_ctrl": False
-            }
 
         # Manage Threads
         # For performance reasons, running this function not on every single burst
