8ea505c6f3d55d7c83b414f6857041ad3a725735
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2024-05-16 13:22:50 -0400

    removal of IPU/OPU from suggested input/output

--- src/inf/initialize.py
+++ src/inf/initialize.py
@@ -173,10 +173,10 @@ def init_brain_regions():
         runtime_data.genome["brain_regions"]["root"]["areas"].append(cortical_area)
         runtime_data.cortical_area_region_association[cortical_area] = "root"
 
-        if runtime_data.genome["blueprint"][cortical_area]["group_id"] == "IPU":
-            runtime_data.genome["brain_regions"]["root"]["inputs"][cortical_area] = []
-        if runtime_data.genome["blueprint"][cortical_area]["group_id"] == "OPU":
-            runtime_data.genome["brain_regions"]["root"]["outputs"][cortical_area] = []
+        # if runtime_data.genome["blueprint"][cortical_area]["group_id"] == "IPU":
+        #     runtime_data.genome["brain_regions"]["root"]["inputs"][cortical_area] = []
+        # if runtime_data.genome["blueprint"][cortical_area]["group_id"] == "OPU":
+        #     runtime_data.genome["brain_regions"]["root"]["outputs"][cortical_area] = []
 
 
 def init_container_variables():
