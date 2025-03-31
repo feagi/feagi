fd8c28098e7c63b293396d541a5b3eeab9c86a8f
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2025-03-30 23:21:41 -0400

    dependency checker

--- feagi/utils/__init__.py
+++ feagi/utils/__init__.py
@@ -3,5 +3,6 @@
 This package contains various utility functions and classes used throughout FEAGI."""
 
 from feagi.utils.logger import setup_logger
+from feagi.utils.version_checker import check_dependencies, verify_dependencies
 
-__all__ = ["setup_logger"] 
+__all__ = ["setup_logger", "check_dependencies", "verify_dependencies"] 
