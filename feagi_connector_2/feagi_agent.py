094fc6f95397e5bc87e3080234a3a7a9de887d10
Amir Rasteg <amir.rasteg@protonmail.com>
2025-10-28 19:22:03 -0400

    example stuff

--- feagi_connector_2/feagi_agent.py
+++ feagi_connector_2/feagi_agent.py
@@ -3,7 +3,7 @@ from .cache.sensors_proxy import SensorsProxy
 from .cache.motors_proxy import MotorsProxy
 from feagi_connector_2.cache.sensors_cache_interface import SensorsCacheInterface
 from feagi_connector_2.cache.motors_cache_interface import MotorsCacheInterface
-from .premade_reflexes import PreMadeReflexes
+from .premade_feedbacks import PreMadeFeedBacks
 
 class FeagiAgent:
 
@@ -14,9 +14,9 @@ class FeagiAgent:
 
         self.brain_input_cache = SensorsCacheInterface(self._rust_agent)
         self.brain_output_cache = MotorsCacheInterface(self._rust_agent)
-        self.premade_reflexes = PreMadeReflexes(self._rust_agent)
+        self.premade_feedbacks = PreMadeFeedBacks(self._rust_agent)
 
-    def encode_sensor_data_and_send(self):
+    def send_brain_input_to_feagi(self):
         self.brain_input_cache.encode_cached_values_to_bytes()
         byte_data: bytes = self.brain_input_cache.copy_out_encoded_bytes()
 
