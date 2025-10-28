d8c26e39a9d46dde28de948779d292dfc6e9dc38
Amir Rasteg <amir.rasteg@protonmail.com>
2025-10-28 17:53:25 -0400

    moved to better location

--- feagi_connector_2/feagi_agent.py
+++ feagi_connector_2/feagi_agent.py
@@ -1,8 +1,8 @@
 import feagi_rust_py_libs as frpl
 from .cache.sensors_proxy import SensorsProxy
 from .cache.motors_proxy import MotorsProxy
-from .cache.devices.sensors_cache_interface import SensorsCacheInterface
-from .cache.devices.motors_cache_interface import MotorsCacheInterface
+from feagi_connector_2.cache.sensors_cache_interface import SensorsCacheInterface
+from feagi_connector_2.cache.motors_cache_interface import MotorsCacheInterface
 
 class FeagiAgent:
 
@@ -14,4 +14,3 @@ class FeagiAgent:
         self.brain_input_cache = SensorsCacheInterface(self._rust_agent)
         self.brain_output_cache = MotorsCacheInterface(self._rust_agent)
 
-        
